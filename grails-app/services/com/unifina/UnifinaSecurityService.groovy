d343f0c6261b1518e6300e5f844c9e3464f92551
djh <>
2013-09-24 13:51:06 +0000

    Domain object access checks complete

--- grails-app/services/com/unifina/UnifinaSecurityService.groovy
+++ grails-app/services/com/unifina/UnifinaSecurityService.groovy
@@ -1,12 +1,19 @@
 package com.unifina
 
+import org.apache.log4j.Logger;
+
 class UnifinaSecurityService {
 	
 	def springSecurityService
+	Logger log = Logger.getLogger(UnifinaSecurityService)
 	
 	boolean checkUser(instance) {
-		if (instance.hasProperty("user") && instance.user?.id!=null)
-			return instance.user.id == springSecurityService.getCurrentUser().id
+		if (instance.hasProperty("user") && instance.user?.id!=null) {
+			boolean result = instance.user.id == springSecurityService.getCurrentUser().id
+			if (!result)
+				log.warn("User $springSecurityService.currentUser.id tried to access $instance owned by user $instance.user.id!")
+			return result
+		}
 		else return true
 	}
 	
