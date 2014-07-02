cd18442d2e6f9a23343485170ffa43c99136b874
djh <>
2014-07-02 11:27:53 +0000

    AWS plugin merged to core plugin
    Repos reordered and made to use company Nexus server

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,36 +21,39 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 //    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+		// Fast local repos first
+		grailsHome()
+		mavenLocal()
 		
-        mavenLocal()
-//        mavenCentral()
+		// Unifina Nexus server
 		mavenRepo "http://192.168.10.21:8081/content/repositories/central/"
 		mavenRepo "http://192.168.10.21:8081/content/repositories/releases/"
 		mavenRepo "http://192.168.10.21:8081/content/repositories/snapshots/"
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+		
+		// Remote Grails repos
+        grailsPlugins()
+        grailsCentral()
+
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.21'
 		compile('log4j:log4j:1.2.16')
-//		compile('org.apache.commons:commons-math:2.2')
+
 		runtime('org.apache.commons:commons-math3:3.2')
 		compile('org.atmosphere:atmosphere-runtime:1.0.0.beta5')
+		
 		compile('org.apache.kafka:kafka_2.8.2:0.8.0') {
 			excludes "slf4j-simple"
-		}		
+		}
 		compile('com.unifina:kafka-client:0.0.6') {
 			excludes "slf4j-simple"
 		}
+		
 		compile('com.mashape.unirest:unirest-java:1.3.3')
-
+		
+		runtime('com.amazonaws:aws-java-sdk:1.7.5')
     }
 
     plugins {
