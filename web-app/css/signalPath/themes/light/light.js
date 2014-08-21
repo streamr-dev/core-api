f6fcb3d8cdd59a297567a94e88e1c4ad87032aca
Jaakko Manninen <jaakko.manninen@unifina.com>
2014-08-21 17:01:47 +0300

    adjust module styling and jsPlumb endpoint size

--- web-app/css/signalPath/themes/light/light.js
+++ web-app/css/signalPath/themes/light/light.js
@@ -38,11 +38,11 @@ SignalPath.defaultChartOptions = {
 // Any styles and colors defined here should be consistent with
 // colors in the CSS. Some browsers don't support SVG styling via CSS.
 
-jsPlumb.Defaults.EndpointStyle = { radius: 10, /*strokeStyle:'#fff', fillStyle: '#ccc' */};
+jsPlumb.Defaults.EndpointStyle = { radius: 9, /*strokeStyle:'#fff', fillStyle: '#ccc' */};
 //jsPlumb.Defaults.EndpointHoverStyle = { radius: 10, strokeStyle:'#fff', fillStyle:'#fff'};
 jsPlumb.Defaults.Connector = [ "Bezier", { curviness: 50 } ];
 jsPlumb.Defaults.PaintStyle = {
-		lineWidth:3,
+		lineWidth: 3,
 		strokeStyle: "#aaa" //('rgba(200,0,0,100)'
 };
 //jsPlumb.Defaults.Overlays = [["Arrow", {direction:-1, paintStyle: {cssClass:"arrow"}}]];
