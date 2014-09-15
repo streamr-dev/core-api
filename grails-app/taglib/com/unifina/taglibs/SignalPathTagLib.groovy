191fa9968519a1597605ec4355e3e4054c7cb882
Henri Pihkala <henri.pihkala@unifina.com>
2014-09-15 10:37:02 +0000

    CORE-134 #resolve Save dropdown menu moved to absolute positioning under body to fix overflow clipping issue

--- grails-app/taglib/com/unifina/taglibs/SignalPathTagLib.groovy
+++ grails-app/taglib/com/unifina/taglibs/SignalPathTagLib.groovy
@@ -308,22 +308,28 @@ class SignalPathTagLib {
 					})
 				})
 			})
+
+			
+			\$('#save-dropdown').on('show.bs.dropdown', function () {
+				var offset = \$('#save-dropdown-button').offset()
+				\$('#save-dropdown-menu').css("top", offset.top + \$("#save-dropdown-button").outerHeight())
+				\$('#save-dropdown-menu').css("left", offset.left)
+				\$('#save-dropdown-menu').show()
+			})
+			\$('#save-dropdown').on('hide.bs.dropdown', function () {
+				\$('#save-dropdown-menu').hide()
+			})
 		"""
 		out << str
 		writeScriptFooter(out)
 		
 		out << """
-			<div class="btn-group">
-				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+			<div id="save-dropdown" class="btn-group">
+				<button id="save-dropdown-button" type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
 					<i class="fa fa-save"></i>
 					Save
 					<span class="caret"></span>
 				</button>
-
-				<ul class="dropdown-menu" role="menu">
-					<li class="disabled"><a href="#" id="saveButton">Save</a></li>
-					<li><a href="#" id="saveAsButton">Save as..</a></li>
-				</ul>
 			</div>
 		"""
 
