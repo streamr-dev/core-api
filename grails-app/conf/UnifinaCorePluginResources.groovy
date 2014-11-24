970923f676f23ba2b6dab1e514c86899aada2b9a
Henri Pihkala <henri.pihkala@unifina.com>
2014-11-24 11:19:42 +0200

    Google Fonts link to https

--- grails-app/conf/UnifinaCorePluginResources.groovy
+++ grails-app/conf/UnifinaCorePluginResources.groovy
@@ -156,7 +156,7 @@ modules = {
 	}
 	'main-theme' {
 		dependsOn 'bootstrap'
-		resource url: "http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin", attrs: [type: "css"]
+		resource url: "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin", attrs: [type: "css"]
 		resource url:[dir:'css/compiled-less', file:'main.css', plugin: 'unifina-core']
 	}
 }
