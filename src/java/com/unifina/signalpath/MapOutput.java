b9b70af8dd876e2860daed01531c4c71c45ba09b
Henri Pihkala <henri.pihkala@streamr.com>
2015-04-07 22:37:15 +0300

    CORE-338 #resolve MapInput/MapOutput added

---
+++ src/java/com/unifina/signalpath/MapOutput.java
@@ -0,0 +1,11 @@
+package com.unifina.signalpath;
+
+import java.util.Map;
+
+public class MapOutput extends Output<Map> {
+
+	public MapOutput(AbstractSignalPathModule owner, String name) {
+		super(owner, name, "Map");
+	}
+
+}
