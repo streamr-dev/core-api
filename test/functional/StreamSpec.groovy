2f4093bbc2936565f49b96d5c084ff63fbbd60e1
Henri Pihkala <henri.pihkala@streamr.com>
2015-07-29 15:36:13 +0300

    StreamSpec is passing

--- test/functional/StreamSpec.groovy
+++ test/functional/StreamSpec.groovy
@@ -1,9 +1,10 @@
 import java.nio.file.Paths
 
-import spock.lang.*
 import core.LoginTester1Spec
 import core.mixins.StreamMixin
-import core.pages.*
+import core.pages.StreamListPage
+import core.pages.StreamShowPage
+
 
 @Mixin(StreamMixin)
 class StreamSpec extends LoginTester1Spec {
@@ -14,39 +15,38 @@ class StreamSpec extends LoginTester1Spec {
 		waitFor { at StreamShowPage }
 		File file = Paths.get(getClass().getResource("files/test-upload-file.csv").toURI()).toFile()
 		fileInput = file
-		waitFor { $(".history .control-label", text:"Range").displayed }
-		waitFor { $(".history div", text:contains("2015-02-23")).displayed }
-		waitFor { $(".history div", text:contains("2015-02-25")).displayed }
+		waitFor { 
+			historyDeleteButton.displayed
+			historyStartDate.text() == "2015-02-23"
+			historyEndDate.text() == "2015-02-25"
+		}
 	}
 	
 	void "removing data from a stream works"() {		
 		when: "only one day's feed files are removed"
 		deleteFeedFilesUpTo("2015-02-23")
 		then: "The stream has no data from that day anymore"
-		waitFor { !$(".history div", text:contains("2015-02-23")).displayed }
-		waitFor { $(".history div", text:contains("2015-02-24")).displayed }
+		waitFor { historyStartDate.text() == "2015-02-24" }
 
 		when: "rest of the feed files are deleted"
 		deleteAllFeedFiles()
 		then: "the stream has no data anymore"
-		$(".col-sm-12 .col-sm-6 p", text:contains("no history"))
+		waitFor { noHistoryMessage.displayed }
 
 		
 		when: "Clicked to go to the configure view and deleted the fields"
-		$(".btn.btn-sm", text:"Configure Fields").click()
-		waitFor { $("span.btn.btn-sm.delete").size() == 4 }
+		configureFieldsButton.click()
+		waitFor { $(".delete-field-button").size() == 4 }
 		deleteFields()
 		then: "There are no fields anymore"
-		waitFor { $("span.btn.btn-sm.delete").size() == 0 }
+		waitFor { $(".delete-field-button").size() == 0 }
 		
 		when: "Saved"
-		$(".btn.save", text:"Save").click()
+		$("button.save").click()
 		then: "Go to StreamShowPage, no configured fields"
 		waitFor { at StreamShowPage }
-		waitFor { $("div.alert", text:"The fields for this stream are not yet configured. Click the button above to configure them.").displayed }
+		waitFor { $("div.alert.alert-info").displayed }
 	}
-	
-	
 	
 }
 
