dce0104a19e5113bc7fa8b4ef7965193bed594de
Aapeli <aapeli.haanpuu@unifina.com>
2015-10-22 11:29:33 +0300

    added func test for the security of the profileEditPage

--- test/functional/LoginSpec.groovy
+++ test/functional/LoginSpec.groovy
@@ -26,45 +26,15 @@ class LoginSpec extends GebReportingSpec {
 		then: "should not go forward"
 		at LoginPage
 		$("p.login-failed-message").displayed
-	}
-	
-	def "login without remember me works"(){
+	}	
+
+	def "basic login works"(){
 		when: "logged in"
 		to LoginPage
 		username = "tester1@streamr.com"
 		password = "tester1TESTER1"
 		loginButton.click()
-		then: 
+		then:
 		at CanvasPage
 	}
-	
-	@Shared cookieName = "streamr_remember_me"
-	@Shared cookieValue = "initial"
-	
-	def "log in with checking remember me"(){
-		when: "logged in and clicked remember me"
-		to LoginPage
-		then: "the remember me checkbox is visible"
-		rememberMeCheckbox.displayed
-		when: "logged in and checked the remember me"
-		username = "tester1@streamr.com"
-		password = "tester1TESTER1"
-		rememberMeCheckbox.click()
-		loginButton.click()
-		waitFor {
-			at CanvasPage
-		}
-		cookieValue = driver.manage().getCookieNamed(cookieName).getValue()
-		then: "at canvaspage" 
-		at CanvasPage
-	}
-	
-	def "the browser should remember me"(){
-		Cookie cookie = new Cookie(cookieName, cookieValue)
-		driver.manage().addCookie(cookie)
-		expect: "should be able to go straight to the canvas page"
-		to CanvasPage
-	}
-	
-	
 }
