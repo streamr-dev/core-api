d2703f819a3588b8a7b8fa1c9d69b773868fed78
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 15:15:27 +0200

    Fix test description

--- test/unit/com/unifina/signalpath/statistics/VarianceSpec.groovy
+++ test/unit/com/unifina/signalpath/statistics/VarianceSpec.groovy
@@ -3,25 +3,25 @@ package com.unifina.signalpath.statistics
 import com.unifina.utils.testutils.ModuleTestHelper
 import spock.lang.Specification
 
-class SumOfSquaresSpec extends Specification {
+class VarianceSpec extends Specification {
 
-	SumOfSquares module
+	StandardDeviation module
 
 	def setup() {
-		module = new SumOfSquares()
+		module = new StandardDeviation()
 		module.init()
 	}
 
-	void "sumOfSquares gives the right answer"() {
+	void "standardDeviation gives the right answer"() {
 		when:
 		module.getInput("windowLength").receive(3)
 		module.getInput("minSamples").receive(2)
 
 		Map inputValues = [
-			in: [1, 1, 0, -1, 2, -5, 13].collect {it?.doubleValue()},
+			in: [1, 1, 0, -1, 13.32931668].collect {it?.doubleValue()},
 		]
 		Map outputValues = [
-			out: [null, 2, 2, 2, 5, 30, 198].collect {it?.doubleValue()}
+			out: [null, 0, 0.57735027, 1, 8].collect {it?.doubleValue()}
 		]
 
 		then:
