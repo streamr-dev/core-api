035293665e05baf52f14135a35ea9c7cabd2cd95
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 13:17:42 +0200

    Test for PearsonsCorrelation

---
+++ test/unit/com/unifina/signalpath/statistics/PearsonsCorrelationSpec.groovy
@@ -0,0 +1,33 @@
+package com.unifina.signalpath.statistics
+
+import com.unifina.utils.testutils.ModuleTestHelper
+import spock.lang.Specification
+
+class PearsonsCorrelationSpec extends Specification {
+
+	PearsonsCorrelation module
+
+	def setup() {
+		module = new PearsonsCorrelation()
+		module.init()
+	}
+
+	def cleanup() {
+
+	}
+
+	void "pearsonsCorrelation gives the right answer"() {
+		when:
+		module.getInput("windowLength").receive(3)
+		Map inputValues = [
+			inX: [1, 4, 0.5, 6, 3, 7, 10].collect {it?.doubleValue()},
+			inY: [2, 8, 1, 12, 9, 14, 21].collect {it?.doubleValue()},
+		]
+		Map outputValues = [
+			corr: [null, null, 1, 1, 0.95261293, 0.98624138, 0.98416045].collect {it?.doubleValue()}
+		]
+
+		then:
+		new ModuleTestHelper(module, inputValues, outputValues).test()
+	}
+}
