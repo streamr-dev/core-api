8a38056f646cd64ce408691256702fdfa64d9b83
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 12:40:06 +0200

    Test for Kurtosis

---
+++ test/unit/com/unifina/signalpath/statistics/KurtosisSpec.groovy
@@ -0,0 +1,27 @@
+package com.unifina.signalpath.statistics
+
+import com.unifina.utils.testutils.ModuleTestHelper
+import spock.lang.Specification
+
+class KurtosisSpec extends Specification {
+
+	Kurtosis module
+	
+    def setup() {
+		module = new Kurtosis()
+		module.init()
+    }
+	
+	void "kurtosis gives the right answer"() {
+		when:
+		Map inputValues = [
+			in: [1, 3, 1.5, 6, 7, -4, 0, 31].collect {it?.doubleValue()},
+		]
+		Map outputValues = [
+			out: [null, null, null, 0.97607072, -2.6135706, 0.34848284, -0.04397045, 5.74163759].collect {it?.doubleValue()}
+		]
+		
+		then:
+		new ModuleTestHelper(module, inputValues, outputValues).test()
+	}
+}
