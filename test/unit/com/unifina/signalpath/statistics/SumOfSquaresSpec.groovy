0586f5bae1d7c90a35944e3246491b850a4187ef
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 15:14:18 +0200

    Test for SumOfSquares

---
+++ test/unit/com/unifina/signalpath/statistics/SumOfSquaresSpec.groovy
@@ -0,0 +1,30 @@
+package com.unifina.signalpath.statistics
+
+import com.unifina.utils.testutils.ModuleTestHelper
+import spock.lang.Specification
+
+class SumOfSquaresSpec extends Specification {
+
+	SumOfSquares module
+
+	def setup() {
+		module = new SumOfSquares()
+		module.init()
+	}
+
+	void "standardDeviation gives the right answer"() {
+		when:
+		module.getInput("windowLength").receive(3)
+		module.getInput("minSamples").receive(2)
+
+		Map inputValues = [
+			in: [1, 1, 0, -1, 2, -5, 13].collect {it?.doubleValue()},
+		]
+		Map outputValues = [
+			out: [null, 2, 2, 2, 5, 30, 198].collect {it?.doubleValue()}
+		]
+
+		then:
+		new ModuleTestHelper(module, inputValues, outputValues).test()
+	}
+}
