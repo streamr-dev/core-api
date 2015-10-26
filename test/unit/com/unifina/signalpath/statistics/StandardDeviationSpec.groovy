c6cfb0d83b918633ddeb24366bf530dfb9d0628d
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 15:11:04 +0200

    Test for standard deviation

---
+++ test/unit/com/unifina/signalpath/statistics/StandardDeviationSpec.groovy
@@ -0,0 +1,30 @@
+package com.unifina.signalpath.statistics
+
+import com.unifina.utils.testutils.ModuleTestHelper
+import spock.lang.Specification
+
+class StandardDeviationSpec extends Specification {
+
+	StandardDeviation module
+
+	def setup() {
+		module = new StandardDeviation()
+		module.init()
+	}
+
+	void "standardDeviation gives the right answer"() {
+		when:
+		module.getInput("windowLength").receive(3)
+		module.getInput("minSamples").receive(2)
+
+		Map inputValues = [
+			in: [1, 1, 0, -1, 13.32931668].collect {it?.doubleValue()},
+		]
+		Map outputValues = [
+			out: [null, 0, 0.57735027, 1, 8].collect {it?.doubleValue()}
+		]
+
+		then:
+		new ModuleTestHelper(module, inputValues, outputValues).test()
+	}
+}
