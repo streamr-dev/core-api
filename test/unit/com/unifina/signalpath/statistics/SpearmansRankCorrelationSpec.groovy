2562363a05ae6f2965295553d5be3df347ad67ba
Eric Andrews <eric.andrews@streamr.com>
2015-10-26 14:59:37 +0200

    Test for SpearmansRankCorrelation

---
+++ test/unit/com/unifina/signalpath/statistics/SpearmansRankCorrelationSpec.groovy
@@ -0,0 +1,30 @@
+package com.unifina.signalpath.statistics
+
+import com.unifina.utils.testutils.ModuleTestHelper
+import spock.lang.Specification
+
+class SpearmansRankCorrelationSpec extends Specification {
+
+	SpearmansRankCorrelation module
+
+	def setup() {
+		module = new SpearmansRankCorrelation()
+		module.init()
+	}
+
+	void "spearmansRankCorrelation gives the right answer"() {
+		when:
+		module.getInput("windowLength").receive(3)
+
+		Map inputValues = [
+			inX: [9, 15, 4, -666, -12, 0.25, 1].collect {it?.doubleValue()},
+			inY: [-3, 2, 33, 0, 12, 100, -3].collect {it?.doubleValue()},
+		]
+		Map outputValues = [
+			corr: [null, null, -0.5, 0.5, 1, 1, -0.5].collect {it?.doubleValue()}
+		]
+
+		then:
+		new ModuleTestHelper(module, inputValues, outputValues).test()
+	}
+}
