1f891c2130153faad0da5df48c8f6b1e98d08786
Aapeli <aapeli.haanpuu@unifina.com>
2015-11-02 10:34:10 +0200

    test now checks the environment

--- test/functional/RegisterSpec.groovy
+++ test/functional/RegisterSpec.groovy
@@ -1,19 +1,27 @@
 
+import grails.util.Environment
 import core.pages.*
 import spock.lang.*
 import geb.spock.GebReportingSpec
 
 class RegisterSpec extends GebReportingSpec {
 	
-        def emailAddress = "testingemail@streamr.com"
+        
+        // Not a real email
+        def emailAddress = "testingemail2@streamr.com"
         def code = emailAddress.replaceAll("@", "_")
         // Just a random password
         def pwd = "Aymaw4HV"
         
-        def "requesting the invitation token goes correctly"() {
+        def setup() {
+            // The environment must be TEST so the SignUpCodeService creates a preisible invitation token
+            expect:
+            assert Environment.current == Environment.TEST
+        }
+        
+        def "the invitation token can be requested correctly"() {
             when: "requested to get the invitation"
                 to SignUpPage
-                // Not a real email
                 email = emailAddress
                 signUpButton.click()
             then: "the invitation is told to be sent"
