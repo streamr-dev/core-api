e9c3dd4797fff0f8dd24b8f9213e39a86a01e7ad
Eric Andrews <eric.andrews@streamr.com>
2015-11-26 16:25:34 +0200

    Refactor code

--- src/java/com/unifina/signalpath/custom/StoredEndpointFields.java
+++ src/java/com/unifina/signalpath/custom/StoredEndpointFields.java
@@ -1,7 +1,6 @@
 package com.unifina.signalpath.custom;
 
 import com.unifina.signalpath.Endpoint;
-import org.apache.log4j.Logger;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
@@ -13,49 +12,68 @@ import java.util.Map;
  * <code>AbstractJavaCodeWrapper</code> to serialize fields of <code>AbstractCustomModule</code>.
  */
 public class StoredEndpointFields implements Serializable {
+	private Map<String, Endpoint> endpointsByFieldName;
 
-	private static final Logger log = Logger.getLogger(StoredEndpointFields.class);
-	private Map<String, Endpoint> endpointsByFieldName = new HashMap<>();
+	private StoredEndpointFields(Map<String, Endpoint> endpointsByFieldName) {
+		this.endpointsByFieldName = endpointsByFieldName;
+	}
 
+	/**
+	 * Collect and clear fields of type  <code>Endpoint</code>.
+	 * @param instance whose values should be cleared and collected
+	 * @return the holder of stored values
+	 */
+	public static StoredEndpointFields clearAndCollect(Object instance) {
+		Map<String, Endpoint> endpointsByFieldName = new HashMap<>();
+
+		for (Field f : fields(instance)) {
+			if (isEndpoint(f)) {
+				endpointsByFieldName.put(f.getName(), popField(instance, f));
+			}
+		}
+		return new StoredEndpointFields(endpointsByFieldName);
+	}
+
+	/**
+	 * Restore values of fields of type <code>Endpoint</code> on given object.
+	 */
 	public void setValuesOn(Object instance) {
 		for (Field f : fields(instance)) {
-			try {
-				f.setAccessible(true);
-				if (Endpoint.class.isAssignableFrom(f.getType())) {
-					f.set(instance, endpointsByFieldName.get(f.getName()));
-				}
-			} catch (IllegalAccessException e) {
-				log.error("Could not set field: " + f + ", class: " + instance.getClass() + " due to exception: " + e);
-			} finally {
-				f.setAccessible(false);
+			if (isEndpoint(f)) {
+				restoreField(instance, f, endpointsByFieldName.get(f.getName()));
 			}
 		}
 	}
 
-	public static StoredEndpointFields clearAndCollect(Object instance) {
-		StoredEndpointFields ef = new StoredEndpointFields();
-		for (Field f : fields(instance)) {
-			ef.fetchAndClearField(instance, f);
+	private static Endpoint popField(Object instance, Field f) {
+		f.setAccessible(true);
+		try {
+			Endpoint endpoint = (Endpoint) f.get(instance);
+			f.set(instance, null);
+			return endpoint;
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Failed to pop field " + f + "for class " + instance.getClass(), e);
+		} finally {
+			f.setAccessible(false);
 		}
-		return ef;
 	}
 
-	private void fetchAndClearField(Object instance, Field f) {
+	private static void restoreField(Object instance, Field f, Object value) {
+		f.setAccessible(true);
 		try {
-            f.setAccessible(true); // avoid java.lang.IllegalAccessException and requires privileges
-            Object obj = f.get(instance);
-            if (obj instanceof Endpoint) {
-                endpointsByFieldName.put(f.getName(), (Endpoint) obj);
-                f.set(instance, null);
-            }
-        } catch (IllegalAccessException e) {
-            log.error("Could not get field: " + f + ", class: " + instance.getClass() + " due to exception: " + e);
-        } finally {
-            f.setAccessible(false); // Set the field back to non-accessible
-        }
+			f.set(instance, value);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Failed to set field " + f + "for class " + instance.getClass(), e);
+		} finally {
+			f.setAccessible(false);
+		}
 	}
 
 	private static Field[] fields(Object instance) {
 		return instance.getClass().getDeclaredFields();
+	}
+
+	private static boolean isEndpoint(Field f) {
+		return Endpoint.class.isAssignableFrom(f.getType());
 	}
 }
