a25ab570c3d4a3f57f653a12a1ffd4b0952444dc
Eric Andrews <eric.andrews@streamr.com>
2016-01-13 16:21:07 +0200

    Fix CanvasApiControllerSpec

--- test/unit/com/unifina/controller/signalpath/CanvasApiControllerSpec.groovy
+++ test/unit/com/unifina/controller/signalpath/CanvasApiControllerSpec.groovy
@@ -23,13 +23,13 @@ class CanvasApiControllerSpec extends Specification {
 	SavedSignalPath ssp2
 	SavedSignalPath ssp3
 	SavedSignalPath ssp4
-	
+
 	void setup() {
 		// Populate db
-		
-		SecUser me = new SecUser(id:1, apiKey: "myApiKey").save(validate:false)
-		SecUser other = new SecUser(id:2, apiKey: "otherApiKey").save(validate:false)
-		
+
+		SecUser me = new SecUser(id: 1, apiKey: "myApiKey").save(validate: false)
+		SecUser other = new SecUser(id: 2, apiKey: "otherApiKey").save(validate: false)
+
 		ssp1 = new SavedSignalPath(
 			user: me,
 			name: "mine",
@@ -42,7 +42,7 @@ class CanvasApiControllerSpec extends Specification {
 			user: other,
 			name: "not mine",
 			json: '{name: "not mine", modules: []}',
-			type:SavedSignalPath.TYPE_USER_SIGNAL_PATH,
+			type: SavedSignalPath.TYPE_USER_SIGNAL_PATH,
 			hasExports: false
 		).save(validate: true, failOnError: true)
 
@@ -50,7 +50,7 @@ class CanvasApiControllerSpec extends Specification {
 			user: other,
 			name: "not mine but example",
 			json: '{name: "not mine but example", modules: []}',
-			type:SavedSignalPath.TYPE_EXAMPLE_SIGNAL_PATH,
+			type: SavedSignalPath.TYPE_EXAMPLE_SIGNAL_PATH,
 			hasExports: false
 		).save(validate: true, failOnError: true)
 
@@ -58,14 +58,13 @@ class CanvasApiControllerSpec extends Specification {
 			user: me,
 			name: "my example",
 			json: '{name: "not mine but example", modules: []}',
-			type:SavedSignalPath.TYPE_EXAMPLE_SIGNAL_PATH,
+			type: SavedSignalPath.TYPE_EXAMPLE_SIGNAL_PATH,
 			hasExports: false
 		).save(validate: true, failOnError: true)
-		
-		assert SecUser.count()==2
-		assert SavedSignalPath.count()==4
-		
-		
+
+		assert SecUser.count() == 2
+		assert SavedSignalPath.count() == 4
+
 		// Mock services or use real ones
 
 		/*
@@ -83,131 +82,131 @@ class CanvasApiControllerSpec extends Specification {
 
 	void "can list all my SignalPaths"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			request.method = "GET"
-			request.requestURI = "/api/v1/canvases/"
-			withFilters([action: "index"]) {
-				controller.index()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		request.method = "GET"
+		request.requestURI = "/api/v1/canvases/"
+		withFilters([action: "index"]) {
+			controller.index()
+		}
 		then:
-			response.status == 200
-			response.json.size() == 2
-			response.json.collect { it.name } == ["mine", "my example"]
+		response.status == 200
+		response.json.size() == 2
+		response.json.collect { it.name } == ["mine", "my example"]
 
 	}
-	
+
 	void "must be able to load my own SignalPath"() {
 		when:
-			params.id = "1"
-			request.addHeader("Authorization", "Token myApiKey")
-			request.method = "GET"
-			webRequest.actionName = "load"
-			request.requestURI = "/api/v1/canvases/load"
-			withFilters([action: "load"]) {
-				controller.load()
-			}
+		params.id = "1"
+		request.addHeader("Authorization", "Token myApiKey")
+		request.method = "GET"
+		webRequest.actionName = "load"
+		request.requestURI = "/api/v1/canvases/load"
+		withFilters([action: "load"]) {
+			controller.load()
+		}
 		then:
-			response.status == 200
-			response.json.uuid.size() == 22
-			response.json.name == "mine"
-			response.json.modules == []
-			!response.json.hasExports
+		response.status == 200
+		response.json.uuid.size() == 22
+		response.json.name == "mine"
+		response.json.modules == []
+		!response.json.hasExports
 	}
-	
+
 	void "must be able to save a new SignalPath"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.json = [
-			    name: "brand new SavedSignalPath",
-				modules: [],
-			]
-			request.method = "POST"
-			webRequest.actionName = "save"
-			request.requestURI = "/api/v1/canvases/save"
-			withFilters([action: "save"]) {
-				controller.save()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.json = [
+			name   : "brand new SavedSignalPath",
+			modules: [],
+		]
+		request.method = "POST"
+		webRequest.actionName = "save"
+		request.requestURI = "/api/v1/canvases/save"
+		withFilters([action: "save"]) {
+			controller.save()
+		}
 		then:
-			response.status == 200
-			response.json.uuid.size() > 10
+		response.status == 200
+		response.json.uuid.size() > 10
 	}
-	
+
 	void "must not be able to load others' SignalPath"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.id = "2"
-			request.method = "GET"
-			webRequest.actionName = "load"
-			request.requestURI = "/api/v1/canvases/load"
-			withFilters([action: "load"]) {
-				controller.load()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.id = "2"
+		request.method = "GET"
+		webRequest.actionName = "load"
+		request.requestURI = "/api/v1/canvases/load"
+		withFilters([action: "load"]) {
+			controller.load()
+		}
 		then:
-			response.status == 403
-			response.json.code == "FORBIDDEN"
+		response.status == 403
+		response.json.code == "FORBIDDEN"
 	}
-	
+
 	void "must be able to load example, even if it's not mine"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.id = "3"
-			request.method = "GET"
-			webRequest.actionName = "load"
-			request.requestURI = "/api/v1/canvases/load"
-			withFilters([action: "load"]) {
-				controller.load()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.id = "3"
+		request.method = "GET"
+		webRequest.actionName = "load"
+		request.requestURI = "/api/v1/canvases/load"
+		withFilters([action: "load"]) {
+			controller.load()
+		}
 		then:
-			response.status == 200
-			response.json.name == "not mine but example"
+		response.status == 200
+		response.json.name == "not mine but example"
 	}
-	
+
 	void "my own example must have saveData"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.id = "4"
-			request.method = "GET"
-			webRequest.actionName = "load"
-			request.requestURI = "/api/v1/canvases/load"
-			withFilters([action: "load"]) {
-				controller.load()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.id = "4"
+		request.method = "GET"
+		webRequest.actionName = "load"
+		request.requestURI = "/api/v1/canvases/load"
+		withFilters([action: "load"]) {
+			controller.load()
+		}
 		then:
-			response.status == 200
-			response.json.name == "my example"
+		response.status == 200
+		response.json.name == "my example"
 	}
-	
+
 	void "must be able to overwrite my own SignalPath"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.id = "1"
-			params.json = [
-				name: "updated, new name",
-				modules: [],
-			]
-			request.method = "POST"
-			webRequest.actionName = "save"
-			request.requestURI = "/api/v1/canvases/save"
-			withFilters([action: "save"]) {
-				controller.save()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.id = "1"
+		params.json = [
+			name   : "updated, new name",
+			modules: [],
+		]
+		request.method = "POST"
+		webRequest.actionName = "save"
+		request.requestURI = "/api/v1/canvases/save"
+		withFilters([action: "save"]) {
+			controller.save()
+		}
 		then:
-			SavedSignalPath.get(1).name == "updated, new name"
+		SavedSignalPath.get(1).name == "updated, new name"
 	}
-	
+
 	void "must not be able overwrite others' SignalPath"() {
 		when:
-			request.addHeader("Authorization", "Token myApiKey")
-			params.id = "3"
-			params.name = "new name"
-			params.json = ssp1.json
-			request.method = "POST"
-			withFilters([action: "save"]) {
-				controller.save()
-			}
+		request.addHeader("Authorization", "Token myApiKey")
+		params.id = "3"
+		params.name = "new name"
+		params.json = ssp1.json
+		request.method = "POST"
+		withFilters([action: "save"]) {
+			controller.save()
+		}
 		then:
-			response.status == 403
-			response.json.code == "FORBIDDEN"
+		response.status == 403
+		response.json.code == "FORBIDDEN"
 	}
 
 }
