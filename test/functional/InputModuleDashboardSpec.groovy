fd212d35204d6481d3f522e215299aec49ec1bf7
Aapeli <aapeli.haanpuu@unifina.com>
2016-01-21 12:41:38 +0200

    (should be) working specs, updated styles

--- test/functional/InputModuleDashboardSpec.groovy
+++ test/functional/InputModuleDashboardSpec.groovy
@@ -29,37 +29,58 @@ class InputModuleDashboardSpec extends LoginTester1Spec {
 			waitFor { startButton.displayed }
 		}
 
-		startButton.click()
+		dropDownButton.click()
+		clearAndStartButton.click()
+		waitForConfirmation()
+		acceptConfirmation()
 		waitFor { stopButton.displayed }
 
 		createDashboard(dashboardName)
+
+		addDashboardItem(liveCanvasName, "Table")
+
+		save()
+
+		waitFor {
+			!$(".ui-pnotify").displayed
+		}
 
 		navbar.navSettingsLink.click()
 		navbar.navLogoutLink.click()
 	}
 
 	def setup() {
+		openDashboard()
+	}
+
+	def openDashboard() {
 		to DashboardListPage
-		$(".table .td", text: dashboardName).click()
+		$(".clickable-table a span", text: dashboardName).click()
 		waitFor {
 			at DashboardShowPage
 		}
-		addDashboardItem(liveCanvasName, "Table")
-		saveButton.click()
+		if ($("body.editing").size() == 0) {
+			$("#main-menu-toggle").click()
+			waitFor {
+				saveButton.displayed
+			}
+		}
+	}
+
+	def cleanup() {
+		save()
 	}
 
 	def cleanupSpec() {
 		// Delete the dashboard
 		super.login()
-		waitFor { at CanvasPage }
 		deleteDashboard(dashboardName)
-		save()
 	}
 
-	void "the buttonModule works"() {
+	void "the button works"() {
 		def button
 		when: "Button added"
-		addModule(liveCanvasName, "Button")
+		addDashboardItem(liveCanvasName, "Button")
 		button = findDashboardItem("Button").find("button.button-module-button")
 		then: "The name of the button is buttonTest"
 		button.text() == "buttonTest"
@@ -67,6 +88,60 @@ class InputModuleDashboardSpec extends LoginTester1Spec {
 		when: "Button clicked"
 		button.click()
 		then: "The table gets the message"
-		$("streamr-table table.event-table-module-content tbody tr").size() == 1
+		waitFor {
+			$("table.event-table-module-content tbody tr").size() == 1
+			$("table.event-table-module-content tbody tr td", text: "10.0").displayed
+		}
+	}
+
+	void "the textField works"() {
+		def textField
+		def sendBtn
+		when: "TextField added"
+		addDashboardItem(liveCanvasName, "TextField")
+		textField = findDashboardItem("TextField").find("textarea")
+		sendBtn = findDashboardItem("TextField").find(".btn.send-btn")
+		then: "The text in the textField is textFieldTest"
+		// Geb's own .text() didn't work for some reason
+		js.exec("return \$('streamr-text-field textarea').val()") == "textFieldTest"
+
+		when: "Text changed and sendButton clicked"
+		textField << "2"
+		sendBtn.click()
+		then: "The table gets the message"
+		waitFor {
+			$("table.event-table-module-content tbody tr").size() == 2
+			$("table.event-table-module-content tbody tr td", text: "textFieldTest2").displayed
+		}
+	}
+
+	void "the switcher works"() {
+		def switcher
+		addDashboardItem(liveCanvasName, "Switcher")
+		switcher = findDashboardItem("Switcher").find("div.switcher div.switcher-inner")
+
+		when: "Switcher clicked"
+		switcher.click()
+		then: "The table gets the message"
+		waitFor {
+			$("table.event-table-module-content tbody tr").size() == 3
+			$("table.event-table-module-content tbody tr td", text: "1.0").displayed
+		}
+
+		when: "Logged out and reloaded"
+		save()
+		waitFor {
+			!$(".ui-pnotify").displayed
+		}
+
+		navbar.navSettingsLink.click()
+		navbar.navLogoutLink.click()
+		super.login()
+		openDashboard()
+
+		then: "Switcher remembers its state"
+		waitFor {
+			$(".switcher.checked").size() == 1
+		}
 	}
 }
