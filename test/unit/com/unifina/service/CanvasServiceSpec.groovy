c31d241994733fe6cc0f066778de0cdb637b61b5
Eric Andrews <eric.andrews@streamr.com>
2016-01-21 12:23:44 +0200

    Tests for CanvasService

--- test/unit/com/unifina/service/CanvasServiceSpec.groovy
+++ test/unit/com/unifina/service/CanvasServiceSpec.groovy
@@ -1,8 +1,9 @@
 package com.unifina.service
 
+import com.unifina.api.SaveCanvasCommand
+import com.unifina.api.ValidationException
 import com.unifina.domain.security.SecUser
 import com.unifina.domain.signalpath.Canvas
-import grails.converters.JSON
 import grails.plugin.springsecurity.SpringSecurityService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
@@ -14,11 +15,12 @@ class CanvasServiceSpec extends Specification {
 
 	SecUser me
 	SecUser someoneElse
+	Canvas myFirstCanvas
 
 	def setup() {
 		me = new SecUser(username: "me@me.com", apiKey: "myKey").save(validate: false)
 
-		new Canvas(
+		myFirstCanvas = new Canvas(
 			name: "my_canvas_1",
 			user: me,
 			json: "{}",
@@ -79,24 +81,78 @@ class CanvasServiceSpec extends Specification {
 		).save(failOnError: true)
 	}
 
-	def "findAllBy by default lists all of current user's Canvases"() {
+	def "#findAllBy, by default, lists all Canvases of current user"() {
 		expect:
 		service.findAllBy(me, null, null, null)*.name == (1..6).collect { "my_canvas_" + it }
 	}
 
-	def "findAllBy can filter by name"() {
+	def "#findAllBy can filter by name"() {
 		expect:
 		service.findAllBy(me, "my_canvas_4", null, null)*.name == ["my_canvas_4"]
 	}
 
-	def "findAllBy can filter by adhoc"() {
+	def "#findAllBy can filter by adhoc"() {
 		expect:
 		service.findAllBy(me, null, false, null)*.name == ["my_canvas_3", "my_canvas_6"]
 		service.findAllBy(me, null, true, null)*.name == ["my_canvas_4", "my_canvas_5"]
 	}
 
-	def "findAllBy can filter by state"() {
+	def "#findAllBy can filter by state"() {
 		expect:
 		service.findAllBy(me, null, null, Canvas.State.RUNNING)*.name == ["my_canvas_3", "my_canvas_4"]
 	}
+
+	def "createNew throws error when given null command object"() {
+		when:
+		service.createNew(null, me)
+
+		then:
+		thrown(NullPointerException)
+	}
+
+	def "createNew throws error when given incomplete command object"() {
+		when:
+		service.createNew(new SaveCanvasCommand(), me)
+
+		then:
+		thrown(ValidationException)
+	}
+
+	def "createNew creates a new Canvas"() {
+		def command = new SaveCanvasCommand(name: "my_new_canvas", modules: [])
+
+		when:
+		def newId = service.createNew(command, me).id
+		Canvas c = Canvas.findById(newId)
+
+		then:
+		c.id != null
+		c.user == me
+		c.name == "my_new_canvas"
+		c.json == '{"name":"my_new_canvas","modules":[],"settings":{},"hasExports":false}'
+		c.state == Canvas.State.STOPPED
+		!c.hasExports
+		!c.example
+
+		c.runner == null
+		c.server == null
+		c.requestUrl == null
+		!c.shared
+		!c.adhoc
+		c.serialized == null
+		c.serializationTime == null
+	}
+
+	def "updateExisting updates existing Canvas"() {
+		def command = new SaveCanvasCommand(name: "my_updated_canvas", modules: [], settings: ["a" : "b"])
+
+		when:
+		service.updateExisting(myFirstCanvas, command, me)
+		Canvas c = Canvas.findById(myFirstCanvas.id)
+
+		then:
+		c.name == "my_updated_canvas"
+		c.json == '{"name":"my_updated_canvas","modules":[],"settings":{"a":"b"},"hasExports":false}'
+	}
+
 }
