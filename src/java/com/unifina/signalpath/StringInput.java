203aa4d27586edb685488c3b565bc0e4c30fba74
Aapeli <aapeli.haanpuu@unifina.com>
2016-01-22 16:03:50 +0200

    abstraction for the primitiveModules

--- src/java/com/unifina/signalpath/StringInput.java
+++ src/java/com/unifina/signalpath/StringInput.java
@@ -1,56 +1,18 @@
 package com.unifina.signalpath;
 
-
-import java.util.Map;
-
-public class StringInput extends Input<String> {
-
-	public boolean canHaveInitialValue = true;
-	public String initialValue = null;
+public class StringInput extends PrimitiveInput<String> {
 
 	public StringInput(AbstractSignalPathModule owner, String name) {
 		super(owner, name, "String");
 	}
 
 	@Override
-	protected void doClear() {
-		if (initialValue!=null) {
-			value = initialValue;
-		}
-		else super.doClear();
-	}
-
-	public String getInitialValue() {
-		return initialValue;
-	}
-
-	public void setInitialValue(String initialValue) {
-		this.initialValue = initialValue;
-
-		if (initialValue!=null) {
-			boolean wasPending = owner.isSendPending();
-			this.receive(initialValue);
-			// Initial value must not change send pending state
-			owner.setSendPending(wasPending);
-		}
-	}
-
-	public Map<String,Object> getConfiguration() {
-		Map<String,Object> config = super.getConfiguration();
-
-		config.put("canHaveInitialValue", canHaveInitialValue);
-
-		if (canHaveInitialValue && initialValue!=null && !initialValue.equals(""))
-			config.put("initialValue", initialValue);
-
-		return config;
+	protected boolean validateInitialValue(String initialValue) {
+		return !initialValue.equals("");
 	}
 
 	@Override
-	public void setConfiguration(Map<String,Object> config) {
-		super.setConfiguration(config);
-
-		if (config.containsKey("initialValue"))
-			setInitialValue(config.get("initialValue")==null || config.get("initialValue").equals("") ? null : (String)config.get("initialValue"));
+	protected String parseInitialValue(String initialValue) {
+		return initialValue;
 	}
 }
