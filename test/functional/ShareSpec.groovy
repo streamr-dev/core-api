9647364ed49b4a16e406f5a4ff5a191e04997e0f
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-02-22 19:13:42 +0200

    CORE-481: Added Dashboard circuit and share-button hiding tests
    warning: it's not very DRY...

--- test/functional/ShareSpec.groovy
+++ test/functional/ShareSpec.groovy
@@ -2,6 +2,8 @@ import core.LoginTester1Spec
 import core.mixins.LoginMixin
 import core.pages.CanvasListPage
 import core.pages.CanvasPage
+import core.pages.DashboardListPage
+import core.pages.DashboardShowPage
 import core.pages.LoginPage
 import core.pages.StreamListPage
 import core.pages.StreamShowPage
@@ -171,8 +173,8 @@ class ShareSpec extends GebReportingSpec {
 	}
 
 	void "sharePopup can grant and revoke Canvas permissions"() {
-		loginTester1()
 		def getCanvasRow = { $("a.tr").findAll { it.text().startsWith("ShareSpec") }.first() }
+		loginTester1()
 
 		when:
 		to CanvasListPage
@@ -308,7 +310,293 @@ class ShareSpec extends GebReportingSpec {
 		!$(".ui-pnotify")
 	}
 
-	void "read permission allows opening but doesn't show share buttons"() {
+	void "sharePopup can grant and revoke Dashboard permissions"() {
+		def getDashboardRow = { $("a.tr").findAll { it.text().startsWith("ShareSpec") }.first() }
 		loginTester1()
+
+		when:
+		to DashboardListPage
+		then:
+		!$(".bootbox.modal")
+		waitFor { at DashboardListPage }
+		waitFor { getDashboardRow().displayed }
+
+		when: "open 'ShareSpec' Dashboard"
+		getDashboardRow().find("button").click()
+		then:
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 0
+
+		when: "add invalid email"
+		$(".new-user-field") << "foobar" << Keys.ENTER
+		then: "enter adds a permission row"
+		waitFor { $(".ui-pnotify").find(".alert-danger") }
+		$(".access-row").size() == 0
+		$(".new-user-field").value() == "foobar"
+
+		when:
+		$(".new-user-field") << Keys.ESCAPE
+		then: "esc clears the email"
+		waitFor { !$(".new-user-field").value() }
+
+		when:
+		$(".new-user-field") << "tester2@streamr.com"
+		$(".new-user-button").click()
+		then:
+		waitFor { $(".access-row") }
+		$(".access-row").size() == 1
+		!$(".new-user-field").value()
+
+		when:
+		$(".new-user-field") << Keys.ESCAPE
+		then: "esc closes the popup discarding changes"
+		waitFor { !$(".bootbox.modal") }
+
+		// ADD PERMISSION
+
+		when: "re-open"
+		getDashboardRow().find("button").click()
+		then: "access row from last time was discarded"
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 0
+
+		when:
+		$(".new-user-field") << "tester2@streamr.com" << Keys.ENTER
+		then:
+		waitFor { $(".access-row") }
+		$(".access-row").size() == 1
+		!$(".new-user-field").value()
+
+		when:
+		$(".new-user-field") << Keys.ENTER
+		then: "enter saves changes when user field is empty"
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when: "re-open once more"
+		closePnotify()	// the second pnotify would cover the share button
+		getDashboardRow().find("button").click()
+		then: "check that the saved row is still there"
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 1
+
+		when:
+		$("button", text: "Save").click()
+		then: "no changes, so no message displayed"
+		waitFor { !$(".bootbox.modal") }
+		!$(".ui-pnotify")
+
+		// REMOVE PERMISSION
+
+		when: "Move to Dashboard editor page, revoke permission"
+		getDashboardRow().click()
+		then:
+		waitFor { at DashboardShowPage }
+		waitFor { shareButton.displayed }
+
+		when:
+		shareButton.click()
+		then: "check that the saved row is still there"
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 1
+
+		when: "there should be only one row and only one delete-button..."
+		$(".user-delete-button").click()
+		then: "it's gone!"
+		waitFor { $(".access-row").size() == 0 }
+
+		when: "discard changes"
+		$("button", text: "Cancel").click()
+		then: "...so no notification"
+		waitFor { !$(".bootbox.modal") }
+		!$(".ui-pnotify")
+
+		when: "re-open"
+		shareButton.click()
+		then: "check that row hasn't been deleted"
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 1
+
+		when: "delete again"
+		$(".user-delete-button").click()
+		then: "it's gone!"
+		waitFor { $(".access-row").size() == 0 }
+
+		when: "...this time for reals"
+		$(".new-user-field") << Keys.ENTER
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when: "re-open"
+		closePnotify()
+		shareButton.click()
+		then: "...to double-check it's gone"
+		waitFor { $(".bootbox.modal") }
+		waitFor { $(".new-user-field").displayed }
+		$(".access-row").size() == 0
+
+		when: "save"
+		$(".new-user-field") << Keys.ENTER
+		then: "...but no changes, so no message displayed"
+		waitFor { !$(".bootbox.modal") }
+		!$(".ui-pnotify")
+	}
+
+	void "read permission allows opening but doesn't show share buttons"() {
+		def getStreamRow = { $("a.tr").findAll { it.text().trim().startsWith("ShareSpec") }.first() }
+		def getCanvasRow = { $("a.tr").findAll { it.text().startsWith("ShareSpec") }.first() }
+		def getDashboardRow = { $("a.tr").findAll { it.text().trim().startsWith("ShareSpec") }.first() }
+
+		loginTester1()
+
+		when: "give tester2 read permission to stream"
+		to StreamListPage
+		getStreamRow().find("button").click()
+		waitFor { $(".new-user-field").displayed }
+		$(".new-user-field") << "tester2@streamr.com" << Keys.ENTER
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		waitFor { $(".access-row") }
+		$(".access-row").size() == 1
+
+		when: "save stream read right"
+		$(".new-user-field") << Keys.ENTER
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when: "give tester2 read permission to canvas"
+		to CanvasListPage
+		getCanvasRow().find("button").click()
+		waitFor { $(".new-user-field").displayed }
+		$(".new-user-field") << "tester2@streamr.com" << Keys.ENTER
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		waitFor { $(".access-row") }
+		$(".access-row").size() == 1
+
+		when: "save canvas read right"
+		$(".new-user-field") << Keys.ENTER
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when: "give tester2 read permission to dashboard"
+		to DashboardListPage
+		getDashboardRow().find("button").click()
+		waitFor { $(".new-user-field").displayed }
+		$(".new-user-field") << "tester2@streamr.com" << Keys.ENTER
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		waitFor { $(".access-row") }
+		$(".access-row").size() == 1
+
+		when: "save dashboard read right"
+		$(".new-user-field") << Keys.ENTER
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when: "challenger appears"
+		closePnotify()
+		logout()
+		loginTester2()
+		to StreamListPage
+		then: "no share button in list"
+		!getStreamRow().find("button")
+
+		when: "open edit view"
+		getStreamRow().click()
+		then:
+		waitFor { at StreamShowPage }
+
+		when: "open menu"
+		streamMenuButton.click()
+		then: "no shareButton in menu!"
+		waitFor { deleteStreamButton.displayed }
+		!$("#share-button")
+
+		when: "check canvas"
+		to CanvasListPage
+		then: "no share button in list"
+		!getCanvasRow().find("button")
+
+		when:
+		getCanvasRow().click()
+		then: "only read rights given"
+		waitFor { at CanvasPage }
+		shareButton.disabled
+
+		when: "check dashboard"
+		to DashboardListPage
+		then:
+		getDashboardRow().click()
+
+		when:
+		getDashboardRow().click()
+		then: "only read rights given"
+		waitFor { at DashboardShowPage }
+		!$("#share-button")
+
+		// REMOVE ACCESS
+		logout()
+		loginTester1()
+
+		when:
+		to StreamListPage
+		getStreamRow().find("button").click()
+		waitFor { $(".user-delete-button").displayed }
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		$(".access-row").size() == 1
+
+		when: "there should be only one delete-button..."
+		$(".user-delete-button").click()
+		then: "gone!"
+		waitFor { $(".access-row").size() == 0 }
+
+		when:
+		$("button", text: "Save").click()
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when:
+		to CanvasListPage
+		getCanvasRow().find("button").click()
+		waitFor { $(".user-delete-button").displayed }
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		$(".access-row").size() == 1
+
+		when: "there should be only one delete-button..."
+		$(".user-delete-button").click()
+		then: "gone!"
+		waitFor { $(".access-row").size() == 0 }
+
+		when:
+		$("button", text: "Save").click()
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
+
+		when:
+		to DashboardListPage
+		getDashboardRow().find("button").click()
+		waitFor { $(".user-delete-button").displayed }
+		then: "got the access-row; also it's the only one so we're not mixing things up"
+		$(".access-row").size() == 1
+
+		when: "there should be only one delete-button..."
+		$(".user-delete-button").click()
+		then: "gone!"
+		waitFor { $(".access-row").size() == 0 }
+
+		when:
+		$("button", text: "Save").click()
+		then:
+		waitFor { $(".ui-pnotify").find(".alert-success") }
+		waitFor { !$(".bootbox.modal") }
 	}
 }
