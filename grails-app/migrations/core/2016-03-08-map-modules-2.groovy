a3b03863c58be8bc763ccd941b15d31260dccbc1
Eric Andrews <eric.andrews@streamr.com>
2016-03-09 16:07:13 +0200

    New map modules migrations

--- grails-app/migrations/core/2016-03-08-map-modules-2.groovy
+++ grails-app/migrations/core/2016-03-08-map-modules-2.groovy
@@ -1,3 +1,48 @@
 package core
 databaseChangeLog = {
+
+	changeSet(author: "eric", id: "serialized-feed-to-blob-1") {
+		def startingOffset = 224
+
+		def modules = [
+			[
+				name: "ContainsValue",
+			],
+			[
+				name: "GetFromMap",
+			],
+			[
+				name: "HeadMap",
+			],
+			[
+				name: "KeysToList",
+			],
+			[
+				name: "PutToMap",
+			],
+			[
+				name: "SortMap",
+			],
+			[
+				name: "TailMap",
+			],
+			[
+				name: "ValuesToList",
+			],
+		]
+
+		modules.eachWithIndex { module, i ->
+			insert(tableName: "module") {
+				column(name: "id", valueNumeric: 224 + i)
+				column(name: "version", valueNumeric: 0)
+				column(name: "category_id", valueNumeric: 51)
+				column(name: "implementing_class", value: "com.unifina.signalpath.map.${module.name}")
+				column(name: "name", value: module.name)
+				column(name: "js_module", value: "GenericModule")
+				column(name: "type", value: "module")
+				column(name: "module_package_id", valueNumeric: 1)
+				column(name: "json_help", value: module.jsonHelp)
+			}
+		}
+	}
 }
