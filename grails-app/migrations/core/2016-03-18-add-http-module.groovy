9c5e35d7474a4c8503c690e9060fe04d7cd2c83c
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-03-22 13:52:05 +0200

    CORE-536: Added ID to module in migration file
    also updated help

--- grails-app/migrations/core/2016-03-18-add-http-module.groovy
+++ grails-app/migrations/core/2016-03-18-add-http-module.groovy
@@ -2,7 +2,7 @@ package core
 databaseChangeLog = {
 	changeSet(author: "jtakalai", id: "2016031801409-1") {
 		insert(tableName: "module") {
-			//column(name: "id", valueNumeric: autoincremented?)
+			column(name: "id", valueNumeric: 225)
 			column(name: "version", valueNumeric: 0)
 			column(name: "category_id", valueNumeric: 3)
 			column(name: "implementing_class", value: "com.unifina.signalpath.remote.Http")
@@ -11,14 +11,19 @@ databaseChangeLog = {
 			column(name: "type", value: "module")
 			column(name: "module_package_id", valueNumeric: 1)
 			column(name: "json_help", value: '{' +
-				'"params":{"verb":"HTTP verb (GET, POST)","URL":"URL to send the request to"},' +
+				'"params":{"verb":"HTTP verb (e.g. GET, POST)","URL":"URL to send the request to"},' +
 				'"paramNames":["verb","URL"],' +
 				'"inputs":{"trigger":"Send request when input arrives"},' +
 				'"inputNames":["trigger"],' +
 				'"outputs":{"error":"Description of what went wrong"},' +
 				'"outputNames":["error"],' +
-				'"helpText":"<p>HTTP Request module sends input values as HTTP request to given URL and outputs the response.</p>"' +
-			'}')
+				'"helpText":"' +
+					'<p>HTTP Request module sends input values as HTTP request to given URL and outputs the response.</p>' +
+					'<p>For GET and DELETE requests, the input values are added to URL parameters:<br />' +
+					'<i>http://url?key1=value1&amp;key2=value2&amp;...</i></p>' +
+					'<p>For other requests, the input values are sent in the body as JSON object:<br />' +
+					'<i>{&quot;key1&quot;: &quot;value1&quot;, &quot;key2&quot;: &quot;value2&quot;, ...}</i></p>' +
+			'"}')
 		}
 	}
 }
