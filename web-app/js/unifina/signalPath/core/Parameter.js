1a66a0417b9e71e4415c589c9795a3eff6f75292
Henri Pihkala <henri.pihkala@unifina.com>
2016-03-28 17:21:42 +0300

    CORE-574: MapParameter works

--- web-app/js/unifina/signalPath/core/Parameter.js
+++ web-app/js/unifina/signalPath/core/Parameter.js
@@ -1,104 +1,5 @@
 (function() {
 
-	var KeyValuePair = Backbone.Model.extend({
-		defaults: {
-			key: '',
-			value: ''
-		}
-	});
-
-	var KeyValuePairList = Backbone.Collection.extend({
-		model: KeyValuePair,
-
-		fromJSON: function(map) {
-			var _this = this
-			Object.keys(map).forEach(function(key) {
-				_this.add(new KeyValuePair({
-					key: key,
-					value: map[key]
-				}))
-			})
-		},
-
-		toJSON: function() {
-			var map = {}
-			this.models.forEach(function(model) {
-				map[model.get('key')] = model.get('value')
-			})
-			return map
-		}
-	});
-
-	// TODO: how to tab from key to value?
-	var KeyValuePairView = Backbone.View.extend({
-		tagName: 'tr',
-		template: '<td><input type="text" class="form-control input-sm key" name="key" value="{{key}}" placeholder="Key"></td><td><input type="text" class="form-control input-sm value" name="value" value="{{value}}" placeholder="Value"></td><td><i class="fa fa-trash-o delete"></i></td>',
-		events: {
-			'click .delete': 'remove',
-			'change input.key': 'update',
-			'change input.value': 'update',
-		},
-		initialize: function() {
-			this.listenTo(this.model, 'change', this.render)
-			this.listenTo(this.model, 'remove', this.unrender)
-		},
-		render: function() {
-			this.$el.html(Mustache.render(this.template, this.model.attributes))
-			return this
-		},
-		update: function() {
-			this.model.set("key", $(this.el).find("input.key").val())
-			this.model.set("value", $(this.el).find("input.value").val())
-		},
-		unrender: function(){
-			$(this.el).remove();
-		},
-		remove: function(){
-			this.model.collection.remove(this.model);
-		}
-	});
-	Mustache.parse(KeyValuePairView.template)
-
-	var KeyValuePairEditor = Backbone.View.extend({
-		template: "<table class='table table-striped table-condensed'><thead><tr><th>Key</th><th>Value</th><th><button class='btn btn-default btn-sm add'><i class='fa fa-plus'></i></button></th></tr></thead><tbody></tbody></table>",
-		events: {
-			'click .add': 'add'
-		},
-		initialize: function() {
-			this.listenTo(this.collection, 'add', this.append)
-			this.render();
-		},
-		render: function() {
-			var _this = this
-			this.$el.html(Mustache.render(this.template))
-			this.collection.models.forEach(function(model) {
-				_this.append(model);
-			});
-
-			return this
-		},
-		add: function() {
-			var item = new KeyValuePair();
-			this.collection.add(item);
-		},
-		append: function(model) {
-			var view = new KeyValuePairView({
-				model: model
-			});
-			this.$el.find('table tbody').append(view.render().el);
-		},
-		clear: function() {
-			this.collection.remove(this.collection.toArray())
-		},
-		disable: function() {
-			this.$el.hide()
-		},
-		enable: function() {
-			this.$el.show()
-		}
-	});
-	Mustache.parse(KeyValuePairEditor.template)
-
 	SignalPath.ParamRenderers = {
 		"default": {
 			create: function(module,data) {
@@ -280,6 +181,15 @@
 					collection: collection
 				})
 				div.data("editor", editor)
+
+				// collection add and remove will resize the component, so module needs to be redrawn
+				collection.on('add', function() {
+					module.redraw()
+				})
+				collection.on('remove', function() {
+					module.redraw()
+				})
+
 				return div
 			},
 			getValue: function(module, data, input) {
