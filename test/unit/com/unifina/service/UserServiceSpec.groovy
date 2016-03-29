763aa242928ff3e045ede4d12a5bee8807380686
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-03-29 18:19:22 +0300

    CORE-476: Reformat, suggested in pull request feedback
    See https://github.com/streamr-dev/unifina-core/pull/80#discussion_r56536318

--- test/unit/com/unifina/service/UserServiceSpec.groovy
+++ test/unit/com/unifina/service/UserServiceSpec.groovy
@@ -20,123 +20,123 @@ import spock.lang.Specification
 @Mock([Feed, SecUser, SecRole, ModulePackage, SecUserSecRole, Module, Permission])
 class UserServiceSpec extends Specification {
 
-    void createData(){
-        // A feed created with minimum fields required
-        Feed feed = new Feed()
-        feed.id = new Long(7)
-        feed.name = "testFeed"
-        feed.eventRecipientClass = ""
-        feed.keyProviderClass = ""
-        feed.messageSourceClass = ""
-        feed.module = new Module()
-        feed.parserClass = ""
-        feed.timezone = "Europe/Minsk"
+	void createData() {
+		// A feed created with minimum fields required
+		Feed feed = new Feed()
+		feed.id = new Long(7)
+		feed.name = "testFeed"
+		feed.eventRecipientClass = ""
+		feed.keyProviderClass = ""
+		feed.messageSourceClass = ""
+		feed.module = new Module()
+		feed.parserClass = ""
+		feed.timezone = "Europe/Minsk"
 		feed.streamListenerClass = NoOpStreamListener.name
 		feed.streamPageTemplate = ""
-        feed.save(failOnError: true)
+		feed.save(failOnError: true)
 
-        // A modulePackage created with minimum fields required
-        def modulePackage = new ModulePackage()
-        modulePackage.id = new Long(1)
-        modulePackage.name = "test"
-        modulePackage.user = new SecUser()
-        modulePackage.save()
+		// A modulePackage created with minimum fields required
+		def modulePackage = new ModulePackage()
+		modulePackage.id = new Long(1)
+		modulePackage.name = "test"
+		modulePackage.user = new SecUser()
+		modulePackage.save()
 
-        def modulePackage2 = new ModulePackage()
-        modulePackage2.id = new Long(2)
-        modulePackage2.name = "test2"
-        modulePackage2.user = new SecUser()
-        modulePackage2.save()
+		def modulePackage2 = new ModulePackage()
+		modulePackage2.id = new Long(2)
+		modulePackage2.name = "test2"
+		modulePackage2.user = new SecUser()
+		modulePackage2.save()
 
-        // The roles created
-        ["ROLE_USER","ROLE_LIVE","ROLE_ADMIN"].each {
-            def role = new SecRole()
-            role.authority = it
-            role.save()
-        }
-    }
+		// The roles created
+		["ROLE_USER", "ROLE_LIVE", "ROLE_ADMIN"].each {
+			def role = new SecRole()
+			role.authority = it
+			role.save()
+		}
+	}
 
 	def permissionService
 
-    def setup() {
-        defineBeans {
-            passwordEncoder(PlaintextPasswordEncoder)
-            springSecurityService(SpringSecurityService)
+	def setup() {
+		defineBeans {
+			passwordEncoder(PlaintextPasswordEncoder)
+			springSecurityService(SpringSecurityService)
 			permissionService(PermissionService)
-        }
-        // Do some wiring that should be done automatically but for some reason is not (in unit tests)
-        grailsApplication.mainContext.getBean("springSecurityService").grailsApplication = grailsApplication
-        grailsApplication.mainContext.getBean("springSecurityService").passwordEncoder = grailsApplication.mainContext.getBean("passwordEncoder")
+		}
+		// Do some wiring that should be done automatically but for some reason is not (in unit tests)
+		grailsApplication.mainContext.getBean("springSecurityService").grailsApplication = grailsApplication
+		grailsApplication.mainContext.getBean("springSecurityService").passwordEncoder = grailsApplication.mainContext.getBean("passwordEncoder")
 		permissionService = mainContext.getBean(PermissionService)
 		permissionService.grailsApplication = grailsApplication
-    }
+	}
 
-    def "the user is created when called"() {
-        when:
-        createData()
-        service.createUser([username: "test@test.com", name:"test", password: "test", timezone:"Europe/Minsk", enabled:true, accountLocked:false, passwordExpired:false])
+	def "the user is created when called"() {
+		when:
+		createData()
+		service.createUser([username: "test@test.com", name: "test", password: "test", timezone: "Europe/Minsk", enabled: true, accountLocked: false, passwordExpired: false])
 
-        then:
-        SecUser.count() == 1
-    }
+		then:
+		SecUser.count() == 1
+	}
 
-    def "if no roles, feeds or modulePackages are given, it should use the default ones"() {
-        when:
-        createData()
-        SecUser user = service.createUser([username: "test@test.com", name:"test", password: "test", timezone:"Europe/Minsk", enabled:true, accountLocked:false, passwordExpired:false])
+	def "if no roles, feeds or modulePackages are given, it should use the default ones"() {
+		when:
+		createData()
+		SecUser user = service.createUser([username: "test@test.com", name: "test", password: "test", timezone: "Europe/Minsk", enabled: true, accountLocked: false, passwordExpired: false])
 
-        then:
-        user.getAuthorities().size() == 2
-        user.getAuthorities().toArray()[0].authority == "ROLE_USER"
-        user.getAuthorities().toArray()[1].authority == "ROLE_LIVE"
+		then:
+		user.getAuthorities().size() == 2
+		user.getAuthorities().toArray()[0].authority == "ROLE_USER"
+		user.getAuthorities().toArray()[1].authority == "ROLE_LIVE"
 
 		permissionService.getAll(ModulePackage, user).size() == 1
 		permissionService.getAll(ModulePackage, user)[0].id == 1
 
 		permissionService.getAll(Feed, user).size() == 1
 		permissionService.getAll(Feed, user)[0].id == 7
-    }
+	}
 
-    def "if the roles, feeds and modulePackages are given, it should use them"() {
-        when:
-        createData()
-        SecUser user = service.createUser([
-                username       : "test@test.com",
-                name           : "test",
-                password       : "test",
-                timezone       : "Europe/Minsk",
-                enabled        : true,
-                accountLocked  : false,
-                passwordExpired: false
-        ],
-                SecRole.findAllByAuthorityInList(["ROLE_USER"]),
-                new ArrayList<Feed>(),
-                ModulePackage.findAllByIdInList([new Long(1), new Long(2)])
-        )
+	def "if the roles, feeds and modulePackages are given, it should use them"() {
+		when:
+		createData()
+		SecUser user = service.createUser([
+			username       : "test@test.com",
+			name           : "test",
+			password       : "test",
+			timezone       : "Europe/Minsk",
+			enabled        : true,
+			accountLocked  : false,
+			passwordExpired: false
+		],
+			SecRole.findAllByAuthorityInList(["ROLE_USER"]),
+			new ArrayList<Feed>(),
+			ModulePackage.findAllByIdInList([new Long(1), new Long(2)])
+		)
 
-        then:
-        user.getAuthorities().size() == 1
-        user.getAuthorities().toArray()[0].authority == "ROLE_USER"
+		then:
+		user.getAuthorities().size() == 1
+		user.getAuthorities().toArray()[0].authority == "ROLE_USER"
 
 		permissionService.getAll(Feed, user).size() == 0
 
 		permissionService.getAll(ModulePackage, user).size() == 2
 		permissionService.getAll(ModulePackage, user)[0].id == 1
 		permissionService.getAll(ModulePackage, user)[1].id == 2
-    }
+	}
 
-    def "it should fail if the default roles, feeds of modulePackages are not found"() {
-        when:
-        // The data has not been created
-        SecUser user = service.createUser([username: "test@test.com", name:"test", password: "test", timezone:"Europe/Minsk", enabled:true, accountLocked:false, passwordExpired:false])
+	def "it should fail if the default roles, feeds of modulePackages are not found"() {
+		when:
+		// The data has not been created
+		SecUser user = service.createUser([username: "test@test.com", name: "test", password: "test", timezone: "Europe/Minsk", enabled: true, accountLocked: false, passwordExpired: false])
 
-        then:
-        thrown RuntimeException
-    }
+		then:
+		thrown RuntimeException
+	}
 
 	void "looking up a user based on correct api key"() {
 		when:
-		new SecUser(username: "me", password: "foo", apiKey: "apiKey", apiSecret: "apiSecret").save(validate:false)
+		new SecUser(username: "me", password: "foo", apiKey: "apiKey", apiSecret: "apiSecret").save(validate: false)
 		def user = service.getUserByApiKey("apiKey")
 
 		then:
@@ -145,7 +145,7 @@ class UserServiceSpec extends Specification {
 
 	void "looking up a user with incorrect api key"() {
 		when:
-		new SecUser(username: "me", password: "foo", apiKey: "apiKey", apiSecret: "apiSecret").save(validate:false)
+		new SecUser(username: "me", password: "foo", apiKey: "apiKey", apiSecret: "apiSecret").save(validate: false)
 		def user = service.getUserByApiKey("wrong api key")
 
 		then:
@@ -159,10 +159,10 @@ class UserServiceSpec extends Specification {
 		when: "given list of fieldErrors"
 		List<FieldError> errorList = new ArrayList<>()
 		errorList.add(new FieldError(
-				this.getClass().name, 'password', 'rejectedPassword', false, null, ['null', 'null', 'rejectedPassword'].toArray(), null
+			this.getClass().name, 'password', 'rejectedPassword', false, null, ['null', 'null', 'rejectedPassword'].toArray(), null
 		))
 		errorList.add(new FieldError(
-				this.getClass().name, 'username', 'rejectedUsername', false, null, ['null', 'null', 'rejectedUsername'].toArray(), null
+			this.getClass().name, 'username', 'rejectedUsername', false, null, ['null', 'null', 'rejectedUsername'].toArray(), null
 		))
 		checkedErrors = service.checkErrors(errorList)
 
