aceae14c784976dc143f5515bad55fff9f7a3ec3
Eric Andrews <eric.andrews@streamr.com>
2016-04-07 19:01:30 +0300

    MapModule: custom pop-up label for marker

--- src/java/com/unifina/signalpath/charts/MapModule.java
+++ src/java/com/unifina/signalpath/charts/MapModule.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashMap;
 public class MapModule extends ModuleWithUI {
 
 	Input<Object> id = new Input<>(this, "id", "Object");
+	Input<Object> label = new Input<>(this, "label", "Object");
 	TimeSeriesInput latitude = new TimeSeriesInput(this, "latitude");
 	TimeSeriesInput longitude = new TimeSeriesInput(this, "longitude");
 	ColorParameter color = new ColorParameter(this, "traceColor", new StreamrColor(233, 87, 15));
@@ -15,6 +16,7 @@ public class MapModule extends ModuleWithUI {
 	boolean drawTrace = false;
 	boolean autoZoom = true;
 	int traceRadius = 2;
+	boolean customMarkerLabel = false;
 
 	@Override
 	public void init() {
@@ -31,19 +33,24 @@ public class MapModule extends ModuleWithUI {
 		longitude.canBeFeedback = false;
 		id.setDrivingInput(true);
 		id.canBeFeedback = false;
+		label.setDrivingInput(false);
+		label.canBeFeedback = false;
 	}
-	
+
 	@Override
 	public void sendOutput() {
-		if (globals.getUiChannel()!=null) {
-			globals.getUiChannel().push(new MapPoint(id.getValue(), latitude.getValue(), longitude.getValue(), color.getValue()), uiChannelId);
-		}
+		MapPoint mapPoint = new MapPoint(
+			id.getValue(),
+			customMarkerLabel ? label.getValue() : id.getValue(),
+			latitude.getValue(),
+			longitude.getValue(),
+			color.getValue()
+		);
+		pushToUiChannel(mapPoint);
 	}
 
 	@Override
-	public void clearState() {
-
-	}
+	public void clearState() {}
 
 	@Override
 	public String getWebcomponentName() {
@@ -63,6 +70,7 @@ public class MapModule extends ModuleWithUI {
 		options.addIfMissing(new ModuleOption("drawTrace", drawTrace, ModuleOption.OPTION_BOOLEAN));
 		options.addIfMissing(new ModuleOption("autoZoom", autoZoom, ModuleOption.OPTION_BOOLEAN));
 		options.addIfMissing(new ModuleOption("traceRadius", traceRadius, ModuleOption.OPTION_INTEGER));
+		options.addIfMissing(new ModuleOption("customMarkerLabel", customMarkerLabel, ModuleOption.OPTION_BOOLEAN));
 
 		return config;
 	}
@@ -72,26 +80,38 @@ public class MapModule extends ModuleWithUI {
 		super.onConfiguration(config);
 		ModuleOptions options = ModuleOptions.get(config);
 
-		if (options.containsKey("drawTrace"))
+		if (options.containsKey("drawTrace")) {
 			drawTrace = options.getOption("drawTrace").getBoolean();
+		}
 
-		if(options.containsKey("autoZoom"))
+		if (options.containsKey("autoZoom")) {
 			autoZoom = options.getOption("autoZoom").getBoolean();
+		}
 
-		if(options.containsKey("traceRadius"))
+		if (options.containsKey("traceRadius")) {
 			traceRadius = options.getOption("traceRadius").getInt();
+		}
+
+		if (options.containsKey("customMarkerLabel")) {
+			customMarkerLabel = options.getOption("customMarkerLabel").getBoolean();
+		}
 
 		if (drawTrace) {
 			addInput(color);
 		}
+
+		if (customMarkerLabel) {
+			addInput(label);
+		}
 	}
 
-	class MapPoint extends LinkedHashMap<String,Object> {
-		public MapPoint(Object id, Double latitude, Double longitude, StreamrColor color) {
+	private static class MapPoint extends LinkedHashMap<String,Object> {
+		private MapPoint(Object id, Object label, Double latitude, Double longitude, StreamrColor color) {
 			super();
 			if (!(id instanceof Double || id instanceof String)) {
 				id = id.toString();
 			}
+			put("label", label);
 			put("t", "p");
 			put("lat", latitude);
 			put("lng", longitude);
