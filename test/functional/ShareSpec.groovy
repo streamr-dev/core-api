688fcd46ed331353ccea899ce029635c33288202
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-04-22 14:14:22 +0300

    ShareSpec fix: added robustness
    old good text input problem: perhaps (on Jenkins) this time no characters got through before Keys.ENTER, so different action was executed (probably closing the dialog, instead of alerting). After this, there doesn't seem to be any "un-force-fed" sharing dialog text inputs.

--- test/functional/ShareSpec.groovy
+++ test/functional/ShareSpec.groovy
@@ -86,6 +86,7 @@ class ShareSpec extends GebReportingSpec {
 			def len = $input.getAttribute("value").length()
 			len >= text.length() ?: ($input << text.substring(len))
 		}
+		return $input
 	}
 
 	void "sharePopup can grant and revoke Stream permissions"() {
@@ -105,7 +106,7 @@ class ShareSpec extends GebReportingSpec {
 		$(".access-row").size() == 0
 
 		when: "add invalid email"
-		$(".new-user-field") << "foobar" << Keys.ENTER
+		forceFeedTextInput(".new-user-field", "foobar") << Keys.ENTER
 		then: "enter adds a permission row"
 		waitFor { $(".ui-pnotify .alert-danger") }
 		$(".access-row").size() == 0
@@ -262,7 +263,7 @@ class ShareSpec extends GebReportingSpec {
 		$(".access-row").size() == 0
 
 		when: "add invalid email"
-		$(".new-user-field") << "foobar" << Keys.ENTER
+		forceFeedTextInput(".new-user-field", "foobar") << Keys.ENTER
 		then: "enter adds a permission row"
 		waitFor { $(".ui-pnotify .alert-danger") }
 		$(".access-row").size() == 0
@@ -404,7 +405,7 @@ class ShareSpec extends GebReportingSpec {
 		$(".access-row").size() == 0
 
 		when: "add invalid email"
-		$(".new-user-field") << "foobar" << Keys.ENTER
+		forceFeedTextInput(".new-user-field", "foobar") << Keys.ENTER
 		then: "enter adds a permission row"
 		waitFor { $(".ui-pnotify .alert-danger") }
 		$(".access-row").size() == 0
