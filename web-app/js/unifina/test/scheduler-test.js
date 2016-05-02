b875e81db78194a7d550effdf267a6f691db1b8f
Aapeli <aapeli.haanpuu@unifina.com>
2016-05-02 10:22:23 +0300

    one tiny fix of some version stuff

--- web-app/js/unifina/test/scheduler-test.js
+++ web-app/js/unifina/test/scheduler-test.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var fs = require('fs')
 var jsdom = require("jsdom")
 
-var window = jsdom.jsdom().parentWindow
+var window = jsdom.jsdom().defaultView
 var $ = require('jquery')(window);
 var _ = require('underscore')
 var Backbone = require('../../backbone/backbone')
