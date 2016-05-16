95744f69d7d0767787afcd1604430e4c8a3f2e66
Eric Andrews <eric.andrews@streamr.com>
2016-05-16 18:12:02 +0300

    Rewrite VariadicInputOutputPairSpec

--- test/unit/com/unifina/signalpath/variadic/VariadicInputOutputPairSpec.groovy
+++ test/unit/com/unifina/signalpath/variadic/VariadicInputOutputPairSpec.groovy
@@ -9,42 +9,52 @@ class VariadicInputOutputPairSpec extends Specification {
 		new InputInstantiator.SimpleObject(), new OutputInstantiator.SimpleObject())
 
 	def setup() {
-		inputOutputPairs.onConfiguration([
-			inputNames: ["endpoint-a", "endpoint-b"],
-			outputNames: ["endpoint-c", "endpoint-d"]
-		])
+		inputOutputPairs.addInput("sisääntulo-1")
+		inputOutputPairs.addInput("sisääntulo-2")
+		inputOutputPairs.addInput("sisääntulo-3")
+		inputOutputPairs.addInput("sisääntulo-4")
+
+		inputOutputPairs.addOutput("ulostulo-1")
+		inputOutputPairs.addOutput("ulostulo-2")
+		inputOutputPairs.addOutput("ulostulo-3")
+		inputOutputPairs.addOutput("ulostulo-4")
+
+		inputOutputPairs.onConfiguration(null)
 	}
 
-	def "getConfiguration() provides expected module options and configurations"() {
-		def config = [:]
-
-		when:
-		inputOutputPairs.getConfiguration(config)
-
-		then:
-		config.keySet() == ["inputNames", "outputNames", "variadicInput", "variadicOutput"] as Set
-
-		and:
-		config.inputNames.size() == 2
-		config.outputNames.size() == 2
-
-		and:
-		config.variadicInput == true
-		config.variadicOutput == true
-	}
-
-	def "sendValuesToOutputs() sends values to outputs"() {
-		when:
-		inputOutputPairs.sendValuesToOutputs([666, "hello world"])
-		then:
-		module.findOutputByDisplayName("out1").value == 666
-		module.findOutputByDisplayName("out2").value == "hello world"
-	}
-
-	def "getInputValues() returns values of inputs"() {
-		module.findInputByDisplayName("in1").receive("hello")
-		module.findInputByDisplayName("in2").receive("world")
+	def "adds pairing details to endpoints' variadic configs"() {
 		expect:
-		inputOutputPairs.inputValues == ["hello", "world"]
+		module.inputs*.getConfiguration()*.variadic == [
+			[isLast: false, index: 1, linkedOutput: "ulostulo-1"],
+			[isLast: false, index: 2, linkedOutput: "ulostulo-2"],
+			[isLast: false, index: 3, linkedOutput: "ulostulo-3"],
+			[isLast: true,  index: 4, linkedOutput: "ulostulo-4"]
+		]
+
+		and:
+		module.outputs*.getConfiguration()*.variadic == [
+			[isLast: false, disableGrow: true, index: 1],
+			[isLast: false, disableGrow: true, index: 2],
+			[isLast: false, disableGrow: true, index: 3],
+			[isLast: true,  disableGrow: true, index: 4]
+		]
+	}
+
+	def "can send values to outputs"() {
+		when:
+		inputOutputPairs.sendValuesToOutputs([666, "hello", "world"])
+		then:
+		module.outputs*.value == [666, "hello", "world", null] // last element is placeholder
+	}
+
+	def "can read values from from inputs"() {
+		module.inputs[0].receive(666)
+		module.inputs[1].receive("hello")
+		module.inputs[2].receive("world")
+
+		when:
+		List values = inputOutputPairs.getInputValues()
+		then:
+		values == [666, "hello", "world"]
 	}
 }
