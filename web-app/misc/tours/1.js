393c882378a130c462dd85c4be8317f5da42c00e
Eric Andrews <eric.andrews@streamr.com>
2016-05-24 16:11:52 +0300

    Improve tour #2 ux

--- web-app/misc/tours/1.js
+++ web-app/misc/tours/1.js
@@ -19,7 +19,7 @@
             "<br/><br/>Click Next when you are ready to begin!", '#navHelpLink',
             { placement: 'left', animated: true })
 
-        .step("Let's start by adding the Public transport demo data stream as was done in the 1st tutorial. <br/><br/>"+
+        .step("Let's start by adding the Public transport demo data stream (as in the 1st tutorial). <br/><br/>"+
             'Search for <b>Public transport demo</b>, and add it to the canvas by selecting it in the results. Do this now.',
             '#search',
             function() {
@@ -36,7 +36,7 @@
         )
 
         .step("For the duration of this tutorial, we'll be focusing on a single, specific tram. To achieve this goal, " +
-            "we need to filter the stream's incoming data appropriately.",
+            "we'll need to filter the stream's incoming data appropriately.",
             '.tourStream1'
         )
 
@@ -68,12 +68,15 @@
             }
         )
 
-        .step("Our goal is to filter data related to a single tram (specifically, vehicle RHKL00112) from all tram data being pushed out by <b>Stream</b>. To do so, we will need to pass forward only those events whose <b>veh</b> equals <b>RHKL00112</b>.",
+        .step("Our goal is to filter data related to a single tram (specifically, vehicle RHKL00112) from all tram " +
+            "data being pushed out by <b>Stream</b>. To do so, we will need to pass forward only those events whose " +
+            "<b>veh</b> equals <b>RHKL00112</b>.",
             '.tourStream1'
         )
 
         .step("Let's make it happen!<br/><br/>"+
-            "Start dragging from the highlighted circle near the output <code>veh</code> on the Stream, and drop on the second input (called <code>text</code>) on the TextEquals module.",
+            "Start dragging from the highlighted circle near the output <code>veh</code> on the Stream, and drop on " +
+            "the second input (called <code>text</code>) on the TextEquals module.",
             '.tourStream1',
             { placement: 'bottom' },
             tour.highlightOutputUntilDraggingStarts("tourStream1.veh")
@@ -106,15 +109,19 @@
             tour.waitForInput(".tourTextEquals1 .parameterInput", "RHKL00112")
         )
 
-        .step("Then connect <code>lat</code>, <code>long</code>, and <code>spd</code> of Stream to <code>in1</code>, <code>in2</code>, and <code>in3</code> of module Filter",
-            '.tourStream1',
-            function(cb) {
-                tour.waitForConnections([
-                    ['tourStream1.lat', 'tourFilter1.in1'],
-                    ['tourStream1.long', 'tourFilter1.in2'],
-                    ['tourStream1.spd', 'tourFilter1.in3']
-                ])(cb)
-            }
+        .step("Then connect <code>lat</code> of Stream to <code>in1</code> of Filter.",
+            '.tourStream1 .endpoint.output:eq(2)',
+            tour.waitForConnection(['tourStream1.lat', 'tourFilter1.in1'])
+        )
+
+        .step("Then connect <code>long</code> of Stream to <code>in2</code> of Filter.",
+            '.tourStream1 .endpoint.output:eq(3)',
+            tour.waitForConnection(['tourStream1.long', 'tourFilter1.in2'])
+        )
+
+        .step("Then connect <code>spd</code> of Stream to <code>in3</code> of Filter.",
+            '.tourStream1 .endpoint.output:eq(4)',
+            tour.waitForConnection(['tourStream1.spd', 'tourFilter1.in3'])
         )
 
         .step("Let's add a <code>Table</code> to see affirm that data is flowing in and to see how it looks like",
