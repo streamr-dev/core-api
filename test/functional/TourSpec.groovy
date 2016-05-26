6c4937798d958fcc2da4f1e971484bbc05b332e6
Eric Andrews <eric.andrews@streamr.com>
2016-05-26 16:57:22 +0300

    Functional test for Tour 1

--- test/functional/TourSpec.groovy
+++ test/functional/TourSpec.groovy
@@ -127,4 +127,138 @@ class TourSpec extends LoginTester1Spec {
 		}
 	}
 
+	def "tour 1"() {
+		startTourFromHelpMenu(1)
+
+		expect:
+		waitFor {
+			getTourBubble().displayed
+			getTourBubble().find('.hopscotch-next').displayed
+		}
+		repeatNextStep()
+
+		advance { searchAndClickContains("public trans") }
+
+		advance { selectCategoryInModuleBrowser("Utils") }
+
+		advance { dragAndDropModule("Filter", 500, -400) }
+
+		advance {
+			searchForModule("Text")
+			clickSearchResult("TextEquals")
+		}
+
+		moveModuleBy("TextEquals", 250, 50, 0, true)
+
+		advance {
+			// Start dragging
+			def ep = getJSPlumbEndpoint(findOutput("Stream", "veh"))
+			interact {
+				clickAndHold(ep)
+				moveByOffset(50, 0)
+			}
+		}
+
+		advance {
+			// Drop on endpoint
+			def ep = getJSPlumbEndpoint(findInput("TextEquals", "text"))
+			interact {
+				moveToElement(ep)
+				release()
+			}
+		}
+
+		advance {
+			// Start dragging
+			def ep = getJSPlumbEndpoint(findOutput("TextEquals", "equals?"))
+			interact {
+				clickAndHold(ep)
+				moveByOffset(50, 0)
+			}
+		}
+
+		advance {
+			// Drop on endpoint
+			def ep = getJSPlumbEndpoint(findInput("Filter", "pass"))
+			interact {
+				moveToElement(ep)
+				release()
+			}
+		}
+
+		advance {
+			setParameterValueForModule("TextEquals", "search", "RHKL00112")
+		}
+
+		advance {
+			connectEndpoints(findOutput("Stream", "lat"), findInputByDisplayName("Filter", "in1"))
+		}
+
+		advance {
+			connectEndpoints(findOutput("Stream", "long"), findInputByDisplayName("Filter", "in2"))
+		}
+
+		advance {
+			connectEndpoints(findOutput("Stream", "spd"), findInputByDisplayName("Filter", "in3"))
+		}
+
+		advance {
+			searchAndClick("Table")
+		}
+
+		moveModuleBy("Table", 600, 150)
+
+		advance {
+			connectEndpoints(findOutputByDisplayName("Filter", "out1"), findInputByDisplayName("Table", "in1"))
+			connectEndpoints(findOutputByDisplayName("Filter", "out2"), findInputByDisplayName("Table", "in2"))
+			connectEndpoints(findOutputByDisplayName("Filter", "out3"), findInputByDisplayName("Table", "in3"))
+		}
+
+		advance {
+			runHistoricalButton.click()
+		}
+
+		waitFor(30) {
+			$(".event-table-module-content tbody tr").size() >= 20
+		}
+
+		advance {
+			runHistoricalButton.click()
+		}
+
+		sleep(1000)
+
+		advance {
+			closeModule("Table")
+		}
+
+		advance {
+			searchAndClick("Chart")
+		}
+
+		moveModuleBy("Chart", 600, 150)
+
+		advance {
+			connectEndpoints(findOutputByDisplayName("Filter", "out3"), findInputByDisplayName("Chart", "in1"))
+		}
+
+		advance {
+			runHistoricalButton.click()
+		}
+
+		sleep(1000)
+
+		waitFor {
+			!$(".chartDrawArea div").isEmpty() // test that anything is added to Chart module's body
+		}
+
+		advance {
+			runHistoricalButton.click()
+		}
+
+		waitFor {
+			atEndOfTour()
+		}
+	}
+
 }
