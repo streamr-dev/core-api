3c1c8565c00cd58a8be76e6661e5bf3be1e6a7f0
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-05-27 18:26:09 +0300

    CORE-621 code review fixes: flushing should now be much thread-safer
    when user is thrown away, it's double checked for just-added metrics; when counter is zeroed, same thing.
    
    There's a sync on "stats" now that could be done without, but it's not on the "critical path" (normal increment). Without sync, following scenario is possible:
    * threads 1...N add new user to stats (line 45)
    * thread N+1 removes user (line 91), checks counters, finds nothing, and finishes flush()
    
    Now there's N threads incrementing counters that have been checked to be zero earlier and forgotten. This is of course not going to common, since deleting user happens only after one full reporting interval without increments.

--- grails-app/services/com/unifina/service/MetricsService.groovy
+++ grails-app/services/com/unifina/service/MetricsService.groovy
@@ -10,6 +10,8 @@ import groovy.transform.Synchronized
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.InitializingBean
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.atomic.AtomicLong
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
@@ -30,45 +32,39 @@ class MetricsService implements InitializingBean, DisposableBean {
 	int reportingPeriodEvents = DEFAULT_REPORTING_PERIOD_EVENTS
 	int reportingPeriodSeconds = DEFAULT_REPORTING_PERIOD_SECONDS
 
-	private Map<String, Map<Long, AtomicLong>> stats = [:]
-	// See Java 8 solution for increment
-	//private ConcurrentMap<String, ConcurrentMap<Long, AtomicLong>> stats = new ConcurrentHashMap<>()
+	private ConcurrentMap<Long, ConcurrentMap<String, AtomicLong>> stats = new ConcurrentHashMap<>()
 
 	private Timer flushTimer
 
-	private final incrementInitLock = new Object[0]
-	def increment(String metric, SecUser user, long count=1) {
-		if (disabled || !metric || !user?.id || count < 1) { return }
-		long userId = user.id
+	def increment(String metric, long userId, long count=1) {
+		if (disabled || !metric || count < 1) { return }
 
-		def m = stats[metric]
-		if (!m) {
-			synchronized (incrementInitLock) {
-				if (!stats[metric]) {
-					stats[metric] = [:]
-				}
+		Map<String, AtomicLong> userStats = stats[userId]
+		if (!userStats) {
+			synchronized (stats) {
+				stats.putIfAbsent(userId, new ConcurrentHashMap<String, AtomicLong>())
+				userStats = stats[userId]
 			}
-			m = stats[metric]
 		}
 
-		AtomicLong counter = m[userId]
+		AtomicLong counter = userStats[metric]
 		if (!counter) {
-			synchronized (incrementInitLock) {
-				if (!m[userId]) {
-					m[userId] = new AtomicLong()
-				}
-			}
-			counter = m[userId]
+			userStats.putIfAbsent(metric, new AtomicLong())
+			counter = userStats[metric]
 		}
 
-		// Java 8 solution for the above, no need for manual locking
-		//def m = stats.computeIfAbsent(metric, k -> new ConcurrentHashMap<Long, AtomicLong>()) ?: stats.get(metric)
-		//AtomicLong counter = m.computeIfAbsent(userId, k -> new AtomicLong()) ?: m.get(userId)
+		// Java 8 solution for the above
+		//def userStats = stats.computeIfAbsent(userId, k -> new ConcurrentHashMap<String, AtomicLong>())
+		//AtomicLong counter = userStats.computeIfAbsent(metric, k -> new AtomicLong())
 
 		long newCount = counter.addAndGet(count)
 		if (newCount > reportingPeriodEvents) {
 			report(metric, userId)
 		}
+	}
+	def increment(String metric, SecUser user, long count=1) {
+		if (!user?.id) { return }
+		increment(metric, user.id, count)
 	}
 	def increment(String metric, Stream stream, long count=1) {
 		increment(metric, stream?.user, count)
@@ -79,12 +75,31 @@ class MetricsService implements InitializingBean, DisposableBean {
 		// if locked, bail; it's ok, someone else is flushing all metrics already
 		if (flushLock.tryLock()) {
 			try {
-				stats.each { String metric, Map<Long, AtomicLong> mStats ->
-					mStats.each { Long userId, AtomicLong counter ->
-						report(metric, userId as long)
+				List<Long> usersToRemove = new ArrayList<>()
+				stats.each { Long userId, Map<String, AtomicLong> userStats ->
+					long reportedValueSum = 0L
+					userStats.each { String metric, AtomicLong counter ->
+						reportedValueSum += report(metric, userId)
+					}
+					if (reportedValueSum == 0L) {
+						usersToRemove.add(userId)
 					}
 				}
-				stats.clear()
+
+				// forget users that didn't have any measured activity in the last reporting interval
+				usersToRemove.each { Long userId ->
+					Map<String, AtomicLong> reportedStats
+					synchronized (stats) {
+						reportedStats = stats.remove(userId)
+					}
+					// double-check that stats were not incremented while reporting (or rather not reporting)
+					reportedStats.each { String metric, AtomicLong counter ->
+						long value = counter.get()
+						if (value > 0L) {
+							increment(metric, userId, value)	// restore the counter
+						}
+					}
+				}
 			} finally {
 				flushLock.unlock()
 			}
@@ -92,15 +107,21 @@ class MetricsService implements InitializingBean, DisposableBean {
 	}
 
 	@Synchronized
-	private def report(String metric, long userId) {
-		kafkaService.sendMessage(kafkaTopic, "", [
-			metric: metric,
-			user: userId,
-			value: stats[metric][userId].get()
-		]);
-
-		// zero it for now, clean up all metrics after flush
-		stats[metric][userId].set(0L)
+	private long report(String metric, long userId) {
+		AtomicLong counter = stats[userId]?.get(metric)
+		long value = counter?.get() ?: 0L
+		if (value > 0L) {
+			kafkaService.sendMessage(kafkaTopic, "", [
+				metric: metric,
+				user  : userId,
+				value : value
+			]);
+			long justAdded = counter.getAndSet(0L) - value
+			if (justAdded > 0L) {
+				counter.addAndGet(justAdded)
+			}
+		}
+		return value
 	}
 
 	@Synchronized
