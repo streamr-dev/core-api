6af54423f23cd1f1a087c469e8cbeb5cc2e81370
Eric Andrews <harbu88@hotmail.com>
2016-06-14 14:40:24 +0300

    CORE-699 FilterMap module (#161)

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -32,4 +32,5 @@ databaseChangeLog = {
 	include file: 'core/2016-06-08-new-variadic-modules.groovy'
 	include file: 'core/2016-06-10-constant-list.groovy'
 	include file: 'core/2016-06-10-new-moving-average-module.groovy'
+	include file: 'core/2016-06-13-filter-map-module.groovy'
 }
