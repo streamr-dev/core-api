21005d250ea3038fbcccbbc47e91ee39a3fc33ef
Henri Pihkala <henri.pihkala@unifina.com>
2016-08-08 21:36:58 +0300

    drop unique constraint on SignupInvite

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -38,4 +38,5 @@ databaseChangeLog = {
 	include file: 'core/2016-06-14-collect-from-maps-module.groovy'
 	include file: 'core/2016-04-06-tours.groovy'
 	include file: 'core/2016-06-15-new-event-table-module.groovy'
+	include file: 'core/2016-08-08-drop-unique-constraint-on-signupinvite.groovy'
 }
