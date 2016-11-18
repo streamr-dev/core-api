fe57e5aa290fb42605d78c1dda334c2e459a775d
Eric Andrews <harbu88@hotmail.com>
2016-11-18 16:06:21 +0200

    CORE-780 Random modules (#180)
    - RandomNumber for generating samples from a uniform distribution.
    - RandomNumberGaussian for generating samples from a Gaussian distrubtion.
    - RandomString for generating fixed-length equiprobable character strings
    - ShuffleList for re-ordering the items of a list
    
    All four random modules use seeds for predictable randomness in subsequent
    runs.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -48,6 +48,7 @@ databaseChangeLog = {
 	include file: 'core/2016-08-19-foreach-module-js.groovy'
 	include file: 'core/2016-08-29-useful-list-modules.groovy'
 	include file: 'core/2016-08-30-test-fixtures-foreach-subcanvas.groovy'
+	include file: 'core/2016-03-21-random-modules.groovy'
 	include file: 'core/2016-09-07-time-of-event-module.groovy'
 	include file: 'core/2016-09-12-format-number-module.groovy'
 }
