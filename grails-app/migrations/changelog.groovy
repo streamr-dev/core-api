127ffd4857a7232a5b624f747dcaac0f4e378c9c
Eric Andrews <harbu88@hotmail.com>
2016-11-18 15:55:17 +0200

    FormatNumber: formatting numbers to string (#194)

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -48,4 +48,5 @@ databaseChangeLog = {
 	include file: 'core/2016-08-19-foreach-module-js.groovy'
 	include file: 'core/2016-08-29-useful-list-modules.groovy'
 	include file: 'core/2016-08-30-test-fixtures-foreach-subcanvas.groovy'
+	include file: 'core/2016-09-12-format-number-module.groovy'
 }
