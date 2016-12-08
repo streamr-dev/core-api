c5bc517dc0fae0ef009e08bf35745f40f1e1ce20
Eric Andrews <eric.andrews@streamr.com>
2016-12-08 18:06:42 +0200

    Fix indentation

--- test/unit/com/unifina/signalpath/utils/MovingWindowSpec.groovy
+++ test/unit/com/unifina/signalpath/utils/MovingWindowSpec.groovy
@@ -5,19 +5,19 @@ import com.unifina.utils.testutils.ModuleTestHelper
 import spock.lang.Specification
 
 class MovingWindowSpec extends Specification {
-	
+
 	MovingWindow module
-	
-    def setup() {
+
+	def setup() {
 		module = new MovingWindow()
 		module.init()
 		module.configure(module.getConfiguration())
-    }
+	}
 
 	void "movingWindow works correctly"() {
 		module.getInput("windowLength").receive(2)
 		module.getInput("minSamples").receive(1)
-		
+
 		when:
 		Map inputValues = [
 			in: [1,2,3,4,5,6]
@@ -25,15 +25,15 @@ class MovingWindowSpec extends Specification {
 		Map outputValues = [
 			list: [[1], [1,2], [2,3], [3,4], [4,5], [5,6]]
 		]
-		
+
 		then:
 		new ModuleTestHelper.Builder(module, inputValues, outputValues).test()
 	}
-	
+
 	void "movingWindow produces no output before minSamples samples is reached"() {
 		module.getInput("windowLength").receive(3)
 		module.getInput("minSamples").receive(3)
-		
+
 		when:
 		Map inputValues = [
 			in: [1,2,3,4]
@@ -41,9 +41,9 @@ class MovingWindowSpec extends Specification {
 		Map outputValues = [
 			list: [null, null, [1, 2, 3], [2, 3, 4]]
 		]
-		
+
 		then:
 		new ModuleTestHelper.Builder(module, inputValues, outputValues).test()
 	}
-	
+
 }
