82b87546832d614052bff3428ffc853a1c2e0b9a
Juuso Takalainen <juuso.takalainen@streamr.com>
2016-12-21 12:47:07 +0200

    Fixed ModuleBrowserSpec
    also added same checks for other tests needing to use table of contents

--- test/functional/ModuleBrowserSpec.groovy
+++ test/functional/ModuleBrowserSpec.groovy
@@ -1,9 +1,15 @@
 import core.LoginTester1Spec
 import core.pages.ModuleBrowserPage
+import org.openqa.selenium.Dimension
 
 class ModuleBrowserSpec extends LoginTester1Spec {
 
 	void "module browser can be opened via help menu"() {
+		// necessary to make sure table of contents column is shown
+		if (driver.manage().window().size.width < 1000) {
+			driver.manage().window().size = new Dimension(1000, 800)
+		}
+
 		when:
 		$("#navHelpLink").click()
 		then:
