230c437f054cc46d7847f4b8ae21fe1195b4fb97
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-02-08 18:38:32 +0200

    ETH-6: Added neat help text to EthereumCall module

--- grails-app/migrations/core/2017-01-20-ethereum-call-module.groovy
+++ grails-app/migrations/core/2017-01-20-ethereum-call-module.groovy
@@ -10,7 +10,21 @@ databaseChangeLog = {
 			column(name: "js_module", value: "GenericModule")
 			column(name: "type", value: "module")
 			column(name: "module_package_id", valueNumeric: 1)
-			column(name: "json_help", value: null)
+			column(name: "json_help", value: '{' +
+				'"params":{},' +
+				'"paramNames":[],' +
+				'"inputs":{"contract":"Ethereum contract", "trigger":"Send call (for functions that have no inputs)", "ether":"ETH to send with the function call (for <i>payable</i> functions)"},' +
+				'"inputNames":["contract", "trigger", "ether"],' +
+				'"outputs":{"errors":"List of error messages"},' +
+				'"outputNames":["errors"],' +
+				'"helpText":' +
+					'"<p>Call Ethereum smart contract.</p>' +
+					'<p>First, connect Ethereum contract into <strong>contract</strong>&nbsp;input. You can write your own using SolidityModule, or pick a template such as PayByUse.</p>' +
+					'<p>Second, choose the <strong>function</strong> you want to call from the dropdown. There are two kinds of functions calls:</p>' +
+					'<ul><li>constant function calls that return results directly, and</li>' +
+						'<li>transactions that return values through events that the function call invokes.</li></ul>' +
+					'<p>The contract must be deployed before this module can activate.</p>"' +
+				'}')
 		}
 	}
 
