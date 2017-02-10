3e4038ff5aa510d6f482b6ebf902ba9b1d94919c
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-02-10 16:56:34 +0200

    update paybyuse code: added kill function
    renaming mostly due trying to make second constructor argument unitPrice_ETH. Unfortunately solidity can't handle floats/decimals/..., since fixed type isn't implemented yet.

--- src/java/com/unifina/signalpath/blockchain/templates/PayByUse.java
+++ src/java/com/unifina/signalpath/blockchain/templates/PayByUse.java
@@ -13,42 +13,52 @@ public class PayByUse extends SolidityModule {
 		return  "pragma solidity ^0.4.0;\n" +
 				"contract PayByUse {\n" +
 				"    address recipient;\n" +
-				"    uint unitCost;\n" +
+				"    uint unitPriceWei;\n" +
 				"    uint public unpaidWei;\n" +
 				"    \n" +
 				"    event OutOfFunds(uint debt);\n" +
 				"    event Paid(uint amount);\n" +
 				"    \n" +
-				"    function PayByUse(address recipientAddress, uint weiPerUnit) payable {\n" +
-				"        unitCost = weiPerUnit;\n" +
+				"    function PayByUse(address recipientAddress, uint unitPrice_wei) payable {\n" +
+				"        unitPriceWei = unitPrice_wei;\n" +
 				"        recipient = recipientAddress;\n" +
 				"    }\n" +
 				"    \n" +
-				"    function update(uint addedUnits) {\n" +
-				"        if (msg.sender != " + streamrAddress + ") { throw; }\n" +
-				"        unpaidWei += addedUnits * unitCost;\n" +
-				"        this.withdraw();\n" +
-				"    }\n" +
-				"    \n" +
-				"    function withdraw() {\n" +
-				"        var sendAmount = min(unpaidWei, this.balance - 10);\n" +
+				"    function payOut() {\n" +
+				"        var sendAmount = min(unpaidWei, this.balance);\n" +
 				"        if (sendAmount > 0) {\n" +
+				"            unpaidWei -= sendAmount;\n" +
 				"            if (recipient.send(sendAmount)) {\n" +
-				"                unpaidWei -= sendAmount;\n" +
 				"                Paid(sendAmount);\n" +
+				"            } else {\n" +
+				"                unpaidWei += sendAmount;\n" +
 				"            }\n" +
 				"        }\n" +
-				"        if (unpaidWei > 0) {\n" +
+				"        if (this.balance == 0) {\n" +
 				"            OutOfFunds(unpaidWei);\n" +
 				"        }\n" +
+				"    }\n" +
+				"    \n" +
+				"    function update(uint addedUnits) canvasCommand {\n" +
+				"        unpaidWei += addedUnits * unitPriceWei;\n" +
+				"        this.payOut();\n" +
+				"    }\n" +
+				"    \n" +
+				"    function kill() canvasCommand {\n" +
+				"        selfdestruct(msg.sender);\n" +
+				"    }\n" +
+				"    \n" +
+				"    modifier canvasCommand {\n" +
+				"        if (msg.sender != " + streamrAddress + ") { throw; }\n" +
+				"        _;\n" +
 				"    }\n" +
 				"    \n" +
 				"    function min(uint x, uint y) constant internal returns (uint) {\n" +
 				"        return (x < y) ? x : y;\n" +
 				"    }\n" +
 				"    \n" +
-				"    // to \"top-up\", the payer simply sends money to this contract\n" +
+				"    // to \"top-up\", the payer simply sends ether to this contract's address\n" +
 				"    function () payable {}\n" +
-				"}";
+				"}\n";
 	}
 }
