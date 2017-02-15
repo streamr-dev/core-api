7747543a88b44c1559974dcd9a6759e11743717a
Henri Pihkala <henri.pihkala@streamr.com>
2017-02-15 11:06:57 +0200

    better eth server defaults

--- grails-app/conf/UnifinaCoreDefaultConfig.groovy
+++ grails-app/conf/UnifinaCoreDefaultConfig.groovy
@@ -269,11 +269,11 @@ environments {
 /**
  * Streamr-web3 Ethereum bridge address
  */
-streamr.ethereum.server = System.getProperty("streamr.ethereum.server") ?: "http://dev-data.streamr"
+streamr.ethereum.server = System.getProperty("streamr.ethereum.server") ?: "http://localhost:3000"
 streamr.ethereum.address = System.getProperty("streamr.ethereum.address") ?: "0xb3428050ea2448ed2e4409be47e1a50ebac0b2d2"
 environments {
 	production {
-		streamr.ethereum.server = System.getProperty("streamr.ethereum.server") ?: "https://data.streamr.com"
+		streamr.ethereum.server = System.getProperty("streamr.ethereum.server") ?: "http://localhost:3000"
 	}
 }
 
