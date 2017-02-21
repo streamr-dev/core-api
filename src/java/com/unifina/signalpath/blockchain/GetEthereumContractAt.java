dbb23ed2bfaf4da43a619b896b378282fc6a4600
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-02-21 17:30:47 +0200

    ETH-14: ask ABI from etherscan

--- src/java/com/unifina/signalpath/blockchain/GetEthereumContractAt.java
+++ src/java/com/unifina/signalpath/blockchain/GetEthereumContractAt.java
@@ -1,11 +1,17 @@
 package com.unifina.signalpath.blockchain;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.unifina.signalpath.AbstractSignalPathModule;
 import com.unifina.signalpath.StringParameter;
 import com.unifina.utils.MapTraversal;
 import grails.util.Holders;
+import org.codehaus.groovy.grails.web.json.JSONObject;
 
 import java.util.Map;
 
@@ -31,21 +37,33 @@ public class GetEthereumContractAt extends AbstractSignalPathModule {
 	@Override
 	protected void onConfiguration(Map<String, Object> config) {
 		String address = addressParam.getValue();
-		String abiString = MapTraversal.getString(config, "params[1].value", "[]");
+		String abiString = MapTraversal.getString(config, "params[1].value");
 
 		// TODO: check address is valid?
 		if (address.length() > 2) {
 			addInput(abiParam);
 			addOutput(out);
 
-			EthereumABI abi = new EthereumABI(abiString);
-			if (abi.getFunctions().size() < 1) {
-				// TODO: query streamr-web3 for known ABI
-//				try {
-//					Unirest.get(ETH_SERVER_URL + "/contract?at=" + address).asJson().getBody();
-//				} catch (UnirestException e) {
-//
-//				}
+			EthereumABI abi = null;
+			if (abiString != null) {
+				abi = new EthereumABI(abiString);
+			} else {
+				// ABI param not yet added to UI => query streamr-web3 for known ABI
+				try {
+					String responseString = Unirest.get(ETH_SERVER_URL + "/contract?at=" + address).asString().getBody();
+					JsonObject response = new JsonParser().parse(responseString).getAsJsonObject();
+					if (response.has("abi")) {
+						JsonArray abiArray = response.getAsJsonArray("abi");
+						abi = new EthereumABI(abiArray);
+						abiParam.receive(abiArray.toString());
+					}
+				} catch (UnirestException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			// parsing failed, ABI is empty or invalid, or etherscan didn't return anything
+			if (abi == null || abi.getFunctions().size() < 1) {
 				abi = new EthereumABI("[{\"type\":\"fallback\",\"payable\":true}]");
 			}
 
