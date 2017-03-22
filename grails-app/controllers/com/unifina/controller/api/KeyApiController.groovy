970acf2b65abace906b333ecfcd5f67c3eef723e
Eric Andrews <eric.andrews@streamr.com>
2017-03-22 13:39:44 +0200

    KeyApiController: delete key endpoint

--- grails-app/controllers/com/unifina/controller/api/KeyApiController.groovy
+++ grails-app/controllers/com/unifina/controller/api/KeyApiController.groovy
@@ -13,6 +13,7 @@ import com.unifina.security.StreamrApi
 import com.unifina.service.PermissionService
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
+import groovy.transform.CompileStatic
 
 @Secured(["IS_AUTHENTICATED_ANONYMOUSLY"])
 class KeyApiController {
@@ -35,6 +36,36 @@ class KeyApiController {
 		}
 
 		render(key.toMap() as JSON)
+	}
+
+	@StreamrApi(authenticationLevel = AuthLevel.USER)
+	def delete(String id) {
+		Key key = Key.get(id)
+		if (key == null) {
+			throw new NotFoundException(Key.toString(), id)
+		} else if (!canDeleteKey(key, request.apiUser)) {
+			throw new NotPermittedException(request.apiUser.username, "Key", key.id.toString(), Permission.Operation.SHARE.toString())
+		} else {
+			key.delete(flush: true)
+			response.status = 204
+			render ""
+		}
+	}
+
+
+	@CompileStatic
+	private boolean canDeleteKey(Key key, SecUser currentUser) {
+		if (key.user != null) {
+			return key.user == currentUser
+		} else {
+			List<Stream> streams = permissionService.get(Stream, currentUser, Permission.Operation.SHARE)
+			for (Stream s : streams) {
+				if (permissionService.canReadKey(key, s)) {
+					return true
+				}
+			}
+			return false
+		}
 	}
 
 	private Key saveAnonymousKeyAndLinkToStream(SaveKeyCommand saveKeyCommand) {
