50fa51310cdd79143c3fdf4dcc725175f9797896
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-05-15 13:59:16 +0300

    CORE-876: Removed "strict" feature

--- src/java/com/unifina/signalpath/text/StringToNumber.java
+++ src/java/com/unifina/signalpath/text/StringToNumber.java
@@ -11,44 +11,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringToNumber extends AbstractSignalPathModule {
-	private static final java.lang.String DOUBLE_REGEX = "[-+]?([0-9]+\\.?|\\.[0-9])[0-9]*([eE][-+]?[0-9]+)?";
-
 	private StringInput in = new StringInput(this, "in");
 	private TimeSeriesOutput out = new TimeSeriesOutput(this, "out");
-
-	private boolean strict = true;
+	private StringOutput error = new StringOutput(this, "error");
 
 	@Override
 	public void sendOutput() {
-		if (strict) {
-			try {
-				out.send(Double.parseDouble(in.getValue()));
-			} catch (NumberFormatException e) {
-				// send nothing for bad strings
-			}
-		} else {
-			Pattern p = Pattern.compile(DOUBLE_REGEX);
-			Matcher m = p.matcher(in.getValue());
-			if (m.find()) {
-				out.send(Double.parseDouble(m.group()));
-			} else {
-				out.send(0.0);
-			}
+		try {
+			out.send(Double.parseDouble(in.getValue()));
+		} catch (NumberFormatException e) {
+			error.send("Failed to parse: '" + in.getValue() + "'");
 		}
-	}
-
-	@Override
-	public Map<String,Object> getConfiguration() {
-		Map<String, Object> config = super.getConfiguration();
-		ModuleOptions options = ModuleOptions.get(config);
-		options.add(new ModuleOption("strict", strict, ModuleOption.OPTION_BOOLEAN));
-		return config;
-	}
-
-	@Override
-	public void onConfiguration(Map<String, Object> config) {
-		super.onConfiguration(config);
-		strict = MapTraversal.getBoolean(config, "options.strict.value", true);
 	}
 
 	@Override
--- test/unit/com/unifina/signalpath/text/StringToNumberSpec.groovy
+++ test/unit/com/unifina/signalpath/text/StringToNumberSpec.groovy
@@ -12,22 +12,15 @@ class StringToNumberSpec extends Specification {
 		module.configure(module.configuration)
 	}
 
-	def "converts correctly (non-strict)"() {
-		module.configure([
-			options: [strict: [value: false]]
-		])
-		def inputs = [in: ["1+1", "3s", "43 ms", "2,4", "+.e2", "+.e", ".", "asdf+3.e2wer", "", "1", "-0.5", "-1.e3", "-.1e3", "+1e1"]]
-		def outputs = [out: [1, 3, 43, 2, 2, 0, 0, 300, 0, 1, -0.5, -1000, -100, 10].collect {it?.doubleValue()}]
-		expect:
-		new ModuleTestHelper.Builder(module, inputs, outputs).test()
-	}
-
-	def "converts correctly (strict)"() {
+	def "converts correctly"() {
 		module.configure([
 			options: [strict: [value: true]]
 		])
-		def inputs = [in: ["1+1", "3s", "43 ms", "2,4", "+.e2", "+.e", ".", "asdf+3.e2wer", "", "1", "-0.5", "-1.e3", "-.1e3", "+1e1"]]
-		def outputs = [out: [null, null, null, null, null, null, null, null, null, 1, -0.5, -1000, -100, 10].collect {it?.doubleValue()}]
+		def inputs = [in: ["1", "-0.5", "-1.e3", "-.1e3", "+1e1", "+.e2", ""]]
+		def outputs = [
+			out: [1, -0.5, -1000, -100, 10, 10, 10].collect {it?.doubleValue()},
+			error: [null, null, null, null, null, "Failed to parse: '+.e2'", "Failed to parse: ''"]
+		]
 		expect:
 		new ModuleTestHelper.Builder(module, inputs, outputs).test()
 	}
