a0077d16ee357970b68597ee53a1a35789db04c6
Eric Andrews <eric.andrews@streamr.com>
2017-06-16 11:33:01 +0300

    ITimeListener: add comment about tickRateInSec = 0

--- src/java/com/unifina/datasource/ITimeListener.java
+++ src/java/com/unifina/datasource/ITimeListener.java
@@ -4,5 +4,9 @@ import java.util.Date;
 
 public interface ITimeListener {
 	void setTime(Date time);
+
+	/**
+	 * How often to invoke setTime(Date) in seconds. Special case 0 = never.
+	 */
 	int tickRateInSec();
 }
