5e12ebcefb3e15a15e8b7a1da53d5c873bf6beae
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-07-10 20:03:13 +0300

    Variadic Concatenate module
    not tested! Tests may have broken (though probably not)

--- src/java/com/unifina/signalpath/text/StringConcatenate.java
+++ src/java/com/unifina/signalpath/text/StringConcatenate.java
@@ -1,13 +1,19 @@
 package com.unifina.signalpath.text;
 
 import com.unifina.signalpath.AbstractSignalPathModule;
+import com.unifina.signalpath.Input;
 import com.unifina.signalpath.StringInput;
 import com.unifina.signalpath.StringOutput;
+import com.unifina.signalpath.variadic.InputInstantiator;
+import com.unifina.signalpath.variadic.VariadicInput;
+
+import java.util.Map;
 
 public class StringConcatenate extends AbstractSignalPathModule {
 
 	StringInput inA = new StringInput(this,"A");
 	StringInput inB = new StringInput(this,"B");
+	VariadicInput<String> inX = new VariadicInput<>(this, new InputInstantiator.Strings(), 3);
 	StringOutput out = new StringOutput(this,"AB");
 	
 	@Override
@@ -19,7 +25,28 @@ public class StringConcatenate extends AbstractSignalPathModule {
 
 	@Override
 	public void sendOutput() {
-		out.send(inA.getValue()+inB.getValue());
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(inA.getValue());
+		buffer.append(inB.getValue());
+		for (String val : inX.getValues()) {
+			buffer.append(val);
+		}
+		out.send(buffer.toString());
+	}
+
+	@Override
+	public Input getInput(String name) {
+		Input input = super.getInput(name);
+		if (input == null) {
+			input = inX.addEndpoint(name);
+		}
+		return input;
+	}
+
+	@Override
+	protected void onConfiguration(Map<String, Object> config) {
+		super.onConfiguration(config);
+		inX.onConfiguration(config);
 	}
 
 	@Override
