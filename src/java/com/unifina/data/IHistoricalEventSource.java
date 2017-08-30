ecf727fecedceb5526f72542818a3a950bb37508
Eric Andrews <eric.andrews@streamr.com>
2017-08-30 12:02:57 +0300

    remove unused IHistoricalEventSource

--- src/java/com/unifina/data/IHistoricalEventSource.java
+++
@@ -1,9 +0,0 @@
-package com.unifina.data;
-
-import java.io.IOException;
-
-@Deprecated
-public interface IHistoricalEventSource {
-	public FeedEvent next() throws IOException;
-	public void close() throws IOException;
-}
