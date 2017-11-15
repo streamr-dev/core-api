98929ee59e8001e8748d2bf8dcf3871f6cb90529
Eric Andrews <eric.andrews@streamr.com>
2017-11-15 14:44:54 +0200

    fix MapModulesSpec

--- test/functional/MapModulesSpec.groovy
+++ test/functional/MapModulesSpec.groovy
@@ -117,8 +117,8 @@ class MapModulesSpec extends LoginTester1Spec {
 
 		connectEndpoints(findOutput("Stream", "value"), findInput("ForEach", "A"))
 		connectEndpoints(findOutput("Stream", "value"), findInput("ForEach", "in"))
-		connectEndpoints(findOutput("ForEach", "out"), findInput("Label", "label", 0))
-		connectEndpoints(findOutput("ForEach", "out2"), findInput("Label", "label", 1))
+		connectEndpoints(findOutputByDisplayName("ForEach", "out"), findInput("Label", "label", 0))
+		connectEndpoints(findOutputByDisplayName("ForEach", "out2"), findInput("Label", "label", 1))
 
 		and: "run canvas in realtime"
 		ensureRealtimeTabDisplayed()
