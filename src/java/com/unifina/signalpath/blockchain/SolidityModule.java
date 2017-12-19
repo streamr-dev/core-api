streamr-dev/core-api

commit 2ad2ce5560345dc5fcb3e52e017ad2291e62f5da

CORE-908: Added gas price option to EthereumCall / SolidityModule (#263)
* CORE-908: Added gas price option to EthereumCall / SolidityModule

in sync with streamr-web3 commit 0044c15355775a18b71961fbc4665e1b19567c6b

real testing kind of thwarted by editor bug that prevent copy-pasting, but checked that the argument comes through, and that it's passed to RPC calls

* improve SolidityModule behavior if no account is selected

* user enters gas price in Gwei

src/java/com/unifina/signalpath/blockchain/SolidityModule.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
@@ -29,6 +30,12 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 	private String code = null;
 	private EthereumContract contract = null;
 	private DoubleParameter sendEtherParam = new DoubleParameter(this, "initial ETH", 0.0);
+
+	@Override
+	public void init() {
+		super.init();
+		ethereumAccount.setUpdateOnChange(true);
+	}
 
 	@Override
 	public void sendOutput() {
@@ -63,13 +70,11 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 	@Override
 	protected void onConfiguration(Map<String, Object> config) {
 		super.onConfiguration(config);
-		boolean compileRequested = config.containsKey("compile");
 
 		if (config.containsKey("code")) {
 			code = config.get("code").toString();
 		} else {
 			code = getCodeTemplate();
-			compileRequested = true;
 		}
 
 		if (config.containsKey("contract")) {
@@ -80,27 +85,35 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 		ethereumOptions = EthereumModuleOptions.readFrom(options);
 
 		try {
-			if (compileRequested && code != null) {
+			if (config.containsKey("compile") || (code != null && !code.trim().isEmpty() && ethereumAccount.getAddress() != null && (contract == null || !contract.isDeployed()))) {
 				contract = compile(code);
-			} else if (config.containsKey("deploy") && !contract.isDeployed()) {
-				EthereumABI.Function constructor = contract.getABI().getConstructor();
-				String sendWei = "0";
-				Stack<Object> args = new Stack<>();
-
-				if (constructor != null) {
-					List<Map> params = (List)config.get("params");
-					for (Map param : params) {
-						args.push(param.get("value"));
-					}
-					// for payable constructors, sendEtherParam is added in params after the ordinary function arguments
-					if (constructor.payable) {
-						BigDecimal valueWei = BigDecimal.valueOf(sendEtherParam.getValue()).multiply(BigDecimal.TEN.pow(18));
-						sendWei = valueWei.toBigInteger().toString();
-						args.pop();
-					}
+			}
+			if (config.containsKey("deploy")) {
+				// Make sure the contract is compiled
+				if (contract == null) {
+					contract = compile(code);
 				}
 
-				contract = deploy(code, args, sendWei);
+				if (!contract.isDeployed()) {
+					EthereumABI.Function constructor = contract.getABI().getConstructor();
+					String sendWei = "0";
+					Stack<Object> args = new Stack<>();
+
+					if (constructor != null) {
+						List<Map> params = (List) config.get("params");
+						for (Map param : params) {
+							args.push(param.get("value"));
+						}
+						// for payable constructors, sendEtherParam is added in params after the ordinary function arguments
+						if (constructor.payable) {
+							BigDecimal valueWei = BigDecimal.valueOf(sendEtherParam.getValue()).multiply(BigDecimal.TEN.pow(18));
+							sendWei = valueWei.toBigInteger().toString();
+							args.pop();
+						}
+					}
+
+					contract = deploy(code, args, sendWei);
+				}
 			}
 		} catch (Exception e) {
 			// TODO: currently I got no notification when URL was incorrect
@@ -141,6 +154,9 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 	}
 
 	private String replaceDynamicFields(String code) {
+		if (ethereumAccount.getAddress() == null) {
+			throw new RuntimeException("No Ethereum account is selected. Please select the account you want to use, or if there are none, go to the user profile page to create one.");
+		}
 		return code.replace(ADDRESS_PLACEHOLDER, ethereumAccount.getAddress());
 	}
 
@@ -189,13 +205,14 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 	private EthereumContract deploy(String code, List<Object> args, String sendWei) throws Exception {
 		code = replaceDynamicFields(code);
 
-		String bodyJson = new Gson().toJson(ImmutableMap.of(
-			"source", ethereumAccount.getAddress(),
-			"key", ethereumAccount.getPrivateKey(),
-			"code", code,
-			"args", args,
-			"value", sendWei
-		)).toString();
+		Map body = new HashMap<>();
+		body.put("source", ethereumAccount.getAddress());
+		body.put("key", ethereumAccount.getPrivateKey());
+		body.put("gasprice", ethereumOptions.getGasPriceWei());
+		body.put("code", code);
+		body.put("args", args);
+		body.put("value", sendWei);
+		String bodyJson = new Gson().toJson(body);
 
 		Unirest.setTimeouts(10*1000, 10*60*1000); // wait patiently for the next mined block, up to 10 minutes
 

grails-app/domain/com/unifina/domain/security/IntegrationKey.groovy
@@ -5,7 +5,7 @@ import grails.converters.JSON
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.grails.web.json.JSONObject
 
-class IntegrationKey {
+class IntegrationKey implements Serializable {
 
 	String id
 	SecUser user

src/java/com/unifina/signalpath/blockchain/EthereumCall.java
@@ -218,9 +218,9 @@ public class EthereumCall extends AbstractHttpModule {
 		if (c.getAddress() == null) { throw new RuntimeException("Contract must be deployed before calling"); }
 
 		Map args = new HashMap<>();
-
 		args.put("source", ethereumAccount.getAddress());
 		args.put("key", ethereumAccount.getPrivateKey());
+		args.put("gasprice", ethereumOptions.getGasPriceWei());
 		args.put("target", c.getAddress());
 
 		HttpPost request = null;

src/java/com/unifina/signalpath/blockchain/templates/EthereumModuleOptions.java
@@ -12,6 +12,9 @@ public class EthereumModuleOptions implements Serializable {
 
 	private String network;
 
+	//private Long gasPriceWei = 20_000_000_000L;		// Long.MAX_VALUE wei == 9 ETH
+	private double gasPriceWei = 20e9; // 20 Gwei
+
 	public EthereumModuleOptions() {
 		// default values
 		network = MapTraversal.getString(Holders.getConfig(), "streamr.ethereum.defaultNetwork");
@@ -19,6 +22,19 @@ public class EthereumModuleOptions implements Serializable {
 
 	public void writeTo(ModuleOptions options) {
 		writeNetworkOption(options);
+		options.add(new ModuleOption("gasPriceGWei", gasPriceWei/1e9, "double"));
+	}
+
+	public static EthereumModuleOptions readFrom(ModuleOptions options) {
+		EthereumModuleOptions ethOpts = new EthereumModuleOptions();
+
+		ethOpts.readNetworkOption(options);
+
+		if (options.getOption("gasPriceGWei") != null) {
+			ethOpts.setGasPriceWei(options.getOption("gasPriceGWei").getDouble() * 1e9);
+		}
+
+		return ethOpts;
 	}
 
 	public void writeNetworkOption(ModuleOptions options) {
@@ -42,18 +58,20 @@ public class EthereumModuleOptions implements Serializable {
 		}
 	}
 
-	public static EthereumModuleOptions readFrom(ModuleOptions options) {
-		EthereumModuleOptions ethOpts = new EthereumModuleOptions();
-		ethOpts.readNetworkOption(options);
-		return ethOpts;
-	}
-
 	public String getNetwork() {
 		return network;
 	}
 
 	public void setNetwork(String network) {
 		this.network = network;
+	}
+
+	public double getGasPriceWei() {
+		return gasPriceWei;
+	}
+
+	public void setGasPriceWei(double gasPriceWei) {
+		this.gasPriceWei = gasPriceWei;
 	}
 
 	public String getServer() {

commit 76b42f99c413dbbb4e4b3166c9f27dcdb611296a

CORE-1048 hotfix: Non-payable constructors got the "initial ETH", also it was in wrong place
That whole thing about relying on parameter positions is a bit of a hack...

This thing was broken in 2ad2ce5560345dc5fcb3e52e017ad2291e62f5da in what looked like unrelated usability fix (make ethereum account re-selection do module onConfigure update; because "ethereumAccount.getAddress() != null" is tested inside onConfigure)

src/java/com/unifina/signalpath/blockchain/SolidityModule.java
@@ -32,7 +32,7 @@ public class SolidityModule extends ModuleWithUI implements Pullable<EthereumCon
 
 	@Override
 	public void init() {
-		super.init();
+		addInput(ethereumAccount);
 		ethereumAccount.setUpdateOnChange(true);
 	}
 