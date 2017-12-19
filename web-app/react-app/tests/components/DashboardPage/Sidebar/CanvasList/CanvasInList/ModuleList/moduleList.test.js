5f597a697700eb225c38ee996290da81b5d62358
Aapeli <aapeli.haanpuu@streamr.com>
2017-12-19 16:01:33 +0200

    moduleList.test

--- web-app/react-app/tests/components/DashboardPage/Sidebar/CanvasList/CanvasInList/ModuleList/moduleList.test.js
+++ web-app/react-app/tests/components/DashboardPage/Sidebar/CanvasList/CanvasInList/ModuleList/moduleList.test.js
@@ -1,7 +1,9 @@
+
 import React from 'react'
 import {shallow} from 'enzyme'
 import assert from 'assert-diff'
 import sinon from 'sinon'
+import ModuleList from '../../../../../../../components/DashboardPage/Sidebar/CanvasList/CanvasInList/ModuleList'
 
 describe('ModuleList', () => {
     let sandbox
@@ -14,8 +16,50 @@ describe('ModuleList', () => {
         sandbox.restore()
     })
     
-    it('is not developed yet', () => {
-        assert(false)
+    describe('render', () => {
+        it('must render only modules with uiChannel', () => {
+            const modules = [{
+                hash: 1,
+                uiChannel: {
+                    just: 'something'
+                }
+            }, {
+                hash: 2,
+                uiChannel: false
+            }, {
+                hash: 3,
+                uiChannel: 'nonFalse'
+            }, {
+                hash: 4
+            }]
+            const el = shallow(<ModuleList
+                modules={modules}
+            />)
+            assert.equal(el.children().length, 2)
+            assert.deepStrictEqual(el.childAt(0).props().module, modules[0])
+            assert.deepStrictEqual(el.childAt(1).props().module, modules[2])
+        })
+        it('must render the modules in alphabetical order by name', () => {
+            const modules = [{
+                name: 'b',
+                hash: 1,
+                uiChannel: true
+            }, {
+                name: 'c',
+                hash: 2,
+                uiChannel: true
+            }, {
+                name: 'a',
+                hash: 3,
+                uiChannel: true
+            }]
+            const el = shallow(<ModuleList
+                modules={modules}
+            />)
+            assert.deepStrictEqual(el.childAt(0).props().module, modules[2])
+            assert.deepStrictEqual(el.childAt(1).props().module, modules[0])
+            assert.deepStrictEqual(el.childAt(2).props().module, modules[1])
+        })
     })
     
 })
