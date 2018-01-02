3b1fa15551226ea0c247ee226320d67ca448d348
Henri Pihkala <henri.pihkala@unifina.com>
2018-01-02 19:06:52 +0200

    add note in StringEncryptorSpec about unlimited strength JCE policy

--- test/unit/com/unifina/security/StringEncryptorSpec.groovy
+++ test/unit/com/unifina/security/StringEncryptorSpec.groovy
@@ -4,15 +4,20 @@ import org.apache.commons.codec.binary.Hex
 import spock.lang.Specification
 
 class StringEncryptorSpec extends Specification {
+
 	void "can encrypt and then decrypt"() {
 		def salt = Hex.decodeHex("060EE583828D7C7BAC85757EDC966974".toCharArray())
-		def encrpytor = new StringEncryptor("password")
+		def encryptor = new StringEncryptor("password")
 
 		when:
-		String encrypted = encrpytor.encrypt("Hello world!", salt)
+		// NOTE: if the below line throws java.security.InvalidKeyException: Illegal key size,
+		// you need to install the Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files 7
+		// from http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html
+		String encrypted = encryptor.encrypt("Hello world!", salt)
 		then:
 		encrypted != "Hello world!"
 		and:
-		encrpytor.decrypt(encrypted, salt) == "Hello world!"
+		encryptor.decrypt(encrypted, salt) == "Hello world!"
 	}
+
 }
