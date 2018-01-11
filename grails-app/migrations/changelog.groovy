24ec3d95095c60737f53ebfa55f5f827fb56e3f9
Eric Andrews <harbu88@hotmail.com>
2018-01-11 12:21:07 +0100

    CORE-791: PassCompleteThrough module (#203)
    * CORE-791: PassCompleteThrough module
    
    Module that allows values to pass through only if all inputs have received a
    value during event propagation.
    
    * rename PassCompleteThrough to RequireAll
    
    As requested in pull request

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -79,4 +79,5 @@ databaseChangeLog = {
 	include file: 'core/2017-05-30-migrate-eth-accounts-as-integration-keys.groovy'
 	include file: 'core/2017-11-10-fix-run-canvas-spec.groovy'
 	include file: 'core/2017-11-23-encrypt-private-keys.groovy'
+	include file: 'core/2018-01-11-require-all-module.groovy'
 }
