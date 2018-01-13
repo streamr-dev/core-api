5d417a60440c2e6aae49b1cfd1a17feaf3b3835e
Eric Andrews <eric.andrews@streamr.com>
2018-01-14 00:16:56 +0200

    refactor FeedFactory, MessageHub and ParsedMessage

--- src/java/com/unifina/feed/ParsedMessage.java
+++ src/java/com/unifina/feed/ParsedMessage.java
@@ -3,16 +3,8 @@ package com.unifina.feed;
 /**
  * Created by henripihkala on 09/02/16.
  */
-public class ParsedMessage<RawMessageClass, MessageClass, KeyClass> extends Message<MessageClass, KeyClass> {
-
-	public RawMessageClass rawMessage;
-
-	public ParsedMessage(long counter, MessageClass message, RawMessageClass rawMessage) {
+public class ParsedMessage<MessageClass, KeyClass> extends Message<MessageClass, KeyClass> {
+	public ParsedMessage(long counter, MessageClass message) {
 		super(counter, message);
-		this.rawMessage = rawMessage;
-	}
-
-	public Object getRawMessage() {
-		return rawMessage;
 	}
 }
