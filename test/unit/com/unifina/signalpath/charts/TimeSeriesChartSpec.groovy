a3ebcd24832a1c7785ef8bb7fc8f84805225e08c
Kare Nuorteva <kare.nuorteva@streamr.com>
2018-03-02 11:44:38 +0200

    Fix failing TimeSeriesChartSpec

--- test/unit/com/unifina/signalpath/charts/TimeSeriesChartSpec.groovy
+++ test/unit/com/unifina/signalpath/charts/TimeSeriesChartSpec.groovy
@@ -1,11 +1,14 @@
 package com.unifina.signalpath.charts
 
 import com.unifina.UiChannelMockingSpecification
+import com.unifina.domain.security.SecUser
 import com.unifina.utils.testutils.ModuleTestHelper
+import grails.test.mixin.Mock
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 
 @TestMixin(GrailsUnitTestMixin)
+@Mock([SecUser])
 class TimeSeriesChartSpec extends UiChannelMockingSpecification {
 
 	TimeSeriesChart module
