c51f7a9bbcca1804a491f4eca9c8b803ae3a342e
Eric Andrews <eric.andrews@streamr.com>
2018-03-06 18:30:50 +0200

    Fix RunCanvasSpec

--- grails-app/migrations/core/2018-03-05-fix-canvas-spec.groovy
+++ grails-app/migrations/core/2018-03-05-fix-canvas-spec.groovy
@@ -9,4 +9,12 @@ databaseChangeLog = {
 			WHERE id = 'run-canvas-spec';
 		""")
 	}
+
+	changeSet(author: "eric", id: "fix-canvas-spec-2", context: "test") {
+		sql("""
+			UPDATE canvas
+			SET request_url = 'http://127.0.0.1:8081/unifina-core/api/live/request'
+			WHERE id = 'run-canvas-spec';
+		""")
+	}
 }
