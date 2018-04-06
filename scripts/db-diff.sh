0d657c6aad82be31fb8c9dc19f599a587e099261
Kare Nuorteva <kare.nuorteva@streamr.com>
2018-04-06 12:59:42 +0300

    db-diff.sh: Automatically format changelog and migration files

--- scripts/db-diff.sh
+++ scripts/db-diff.sh
@@ -16,3 +16,7 @@ grails dbm-gorm-diff $FILENAME --add $2 --stacktrace
 # Add package to beginning of file
 { printf "package core\n"; cat grails-app/migrations/$FILENAME; } > grails-app/migrations/$FILENAME.new
 mv grails-app/migrations/$FILENAME{.new,}
+
+sed -i '' -e '/^$/d' grails-app/migrations/changelog.groovy
+sed -i '' -e '/^$/d' -e 's/[[:space:]](generated)//' "grails-app/migrations/$FILENAME"
+
