ce6f1c0be704020d3f8d457f2da3bb7f7e516ccd
Eric Andrews <eric.andrews@streamr.com>
2018-04-26 18:16:48 +0300

    travis: add rest-e2e2-tests

---
+++ travis_scripts/rest_e2e_test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo /etc/init.d/mysql stop
+(cd rest-e2e-tests && npm install)
+git clone https://github.com/streamr-dev/streamr-docker-dev.git
+$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start 1
+sleep 45
+( nohup grails test run-app --non-interactive 2>&1 & )
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8081/streamr-core/api/v1/products)" != "200" ]]; do sleep 5; done' # Thanks: https://gist.github.com/rgl/f90ff293d56dbb0a1e0f7e7e89a81f42
+(cd rest-e2e-tests && npm test)
