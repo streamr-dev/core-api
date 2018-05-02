aa753a7cf4b42092724d3bfc6046ff7edb65f25b
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-02 15:21:36 +0300

    Add functional tests

---
+++ travis_scripts/functional_tests.sh
@@ -0,0 +1,1 @@
+docker run -i -t -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/eu-west-docker-ee init && terraform apply -target=module.docker-broker -var-file=docker-broker/conf/eu-west-1-stg-docker-broker.tfvars
