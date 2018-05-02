20bc3723b7597ced8131bececf58d910c6ab4671
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-02 18:50:14 +0300

    Add code to create infrastructure

--- travis_scripts/functional_test.sh
+++ travis_scripts/functional_test.sh
@@ -1,2 +1,7 @@
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-docker run -i -t -e AWS_ACCESS_KEY_ID="${TERRAFORM_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${TERRAFORM_SECRET}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee:latest make terraform-init terraform-stg destroy-docker-base-containers destroy-docker-broker destroy-docker-data destroy-docker-engine-editor destroy-docker-clusters destroy-docker-broker destroy-docker-data destroy-docker-engine-editor
+
+#Create Enviroment
+docker run -i -t -e AWS_ACCESS_KEY_ID="${TERRAFORM_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${TERRAFORM_SECRET}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee:latest make terraform-init terraform-stg apply-docker-clusters apply-docker-base-containers apply-docker-engine-editor apply-docker-data apply-docker-broker
+
+#Destroy Enviroment
+docker run -i -t -e AWS_ACCESS_KEY_ID="${TERRAFORM_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${TERRAFORM_SECRET}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee:latest make terraform-init terraform-stg destroy-docker-base-containers destroy-docker-broker destroy-docker-data destroy-docker-engine-editor destroy-docker-clusters
