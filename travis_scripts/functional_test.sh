5056a122655e74dc24c65fe0a694ebfabe84ffb0
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-02 15:41:20 +0300

    Fix image name

--- travis_scripts/functional_test.sh
+++ travis_scripts/functional_test.sh
@@ -1,2 +1,2 @@
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-docker run -i -t -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/eu-west-docker-ee init && terraform apply -target=module.docker-broker -var-file=docker-broker/conf/eu-west-1-stg-docker-broker.tfvars
+docker run -i -t -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee init && terraform apply -target=module.docker-broker -var-file=docker-broker/conf/eu-west-1-stg-docker-broker.tfvars
