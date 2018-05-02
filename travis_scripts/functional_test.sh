6a50852b7f0251942f704a535c528558b27af7ab
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-02 18:14:16 +0300

    Destroy more

--- travis_scripts/functional_test.sh
+++ travis_scripts/functional_test.sh
@@ -1,2 +1,2 @@
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-docker run -i -t -e AWS_ACCESS_KEY_ID="${TERRAFORM_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${TERRAFORM_SECRET}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee:latest make terraform-init terraform-stg destroy-docker-base-containers
+docker run -i -t -e AWS_ACCESS_KEY_ID="${TERRAFORM_KEY_ID}" -e AWS_SECRET_ACCESS_KEY="${TERRAFORM_SECRET}" -e AWS_DEFAULT_REGION="eu-west-1" streamr/infra-docker-ee:latest make terraform-init terraform-stg destroy-docker-base-containers destroy-docker-broker destroy-docker-data destroy-docker-engine-editor destroy-docker-clusters destroy-docker-broker destroy-docker-data destroy-docker-engine-editor
