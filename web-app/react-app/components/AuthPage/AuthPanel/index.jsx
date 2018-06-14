00db0cf08265564d3572f9294100e170fd7f7239
Mariusz Roliński <mariusz@mondoreale.pl>
2018-06-14 13:46:20 +0200

    New AuthPanel

--- web-app/react-app/components/AuthPage/AuthPanel/index.jsx
+++ web-app/react-app/components/AuthPage/AuthPanel/index.jsx
@@ -1,39 +1,61 @@
 // @flow
 
 import * as React from 'react'
-import { Card, CardHeader, CardBody } from '@streamr/streamr-layout'
-import classnames from 'classnames'
+import { Link } from 'react-router-dom'
+import cx from 'classnames'
+
+import createLink from '../../../utils/createLink'
 
 import styles from './authPanel.pcss'
 
 type Props = {
-    leftUtil?: React.Node,
-    rightUtil?: React.Node,
-    title: React.Node,
-    children: React.Node
+    title: string,
+    children: React.Node,
+    onGoBack?: () => void,
 }
 
-const AuthPanel = ({ leftUtil, rightUtil, title, children }: Props) => (
-    <div className={styles.authPanel}>
-        <div className={classnames(styles.utilRow, 'uppercase', 'ff-plex-mono')}>
-            <div className={styles.util}>
-                {leftUtil}
-            </div>
-            <div className={styles.util}>
-                {rightUtil}
-            </div>
-        </div>
-        <Card className={styles.panel}>
-            <CardHeader className={styles.panelHeading}>
-                {title}
-            </CardHeader>
-            <CardBody className={styles.panelBody}>
-                <div>
-                    {children}
+class AuthPanel extends React.Component<Props> {
+    onBackClick = (e: SyntheticInputEvent<EventTarget>) => {
+        const onGoBack = this.props.onGoBack || (() => {})
+        e.preventDefault()
+        onGoBack()
+    }
+
+    render = () => {
+        const { title, children, onGoBack } = this.props
+
+        return (
+            <div className={cx(styles.authPanel)}>
+                <a href="https://www.streamr.com" className={styles.logo}>
+                    <img src={createLink('static/images/streamr-logo.svg')} />
+                </a>
+                <div
+                    className={cx(styles.navbar)}
+                >
+                    {onGoBack ? (
+                        <a href="#" onClick={this.onBackClick}>
+                            Back
+                        </a>
+                    ) : (
+                        <React.Fragment>
+                            <div />
+                        </React.Fragment>
+                    )}
                 </div>
-            </CardBody>
-        </Card>
-    </div>
-)
+                <div className={styles.panel}>
+                    <div className={styles.header}>
+                        {title}
+                    </div>
+                    <div className={styles.body}>
+                        {children}
+                    </div>
+                </div>
+                <div className={styles.footer}>
+                    Made with ❤️ & ☕️ by Streamr Network AG in 2018
+                </div>
+            </div>
+        )
+    }
+}
 
 export default AuthPanel
--- web-app/react-app/components/AuthPage/index.jsx
+++ web-app/react-app/components/AuthPage/index.jsx
@@ -8,10 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@streamr/streamr-layout/css'
 import '@streamr/streamr-layout/pcss'
 
-import Nav from './AuthPageNav'
-import Footer from './AuthPageFooter'
 import LoginPage from './LoginPage'
-import RegisterPage from './RegisterPage'
+// import RegisterPage from './RegisterPage'
 
 const basename = createLink('/').replace(window.location.origin, '')
 
@@ -20,17 +18,15 @@ import styles from './authPage.pcss'
 const AuthPage = () => (
     <BrowserRouter basename={basename}>
         <div className={styles.authPage}>
-            <Nav />
             <section className={styles.content}>
                 <Switch>
                     <Route exact path="/login/auth" component={LoginPage} />
-                    <Route exact path="/register/signup" component={RegisterPage} />
+                    {/* <Route exact path="/register/signup" component={RegisterPage} /> */}
                     <Redirect from="/register" to="/register/signup" />
                     <Redirect from="/login" to="/login/auth" />
                     <Route path="/" component={() => '404'} />
                 </Switch>
             </section>
-            <Footer />
         </div>
     </BrowserRouter>
 )
