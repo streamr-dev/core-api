f7250026c477772f4ab58364a14ecd62650e2dcd
Mariusz Roliński <mariusz@mondoreale.pl>
2018-06-15 11:49:02 +0200

    Add Next button to “Sign Up” page

--- web-app/react-app/components/AuthPage/RegisterPage/index.jsx
+++ web-app/react-app/components/AuthPage/RegisterPage/index.jsx
@@ -1,12 +1,18 @@
 // @flow
 
 import React from 'react'
+
 import AuthPanel from '../AuthPanel'
 import Input from '../Input'
+import Actions from '../Actions'
+import Button from '../Button'
 
 const RegisterPage = () => (
     <AuthPanel title="Sign Up" ethLink signinLink>
         <Input placeholder="Email" />
+        <Actions>
+            <Button>Next</Button>
+        </Actions>
     </AuthPanel>
 )
 
