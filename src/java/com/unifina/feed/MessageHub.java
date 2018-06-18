de356870c94e2c67f9624cb63d74276bbc5bdf89
Eric Andrews <eric.andrews@streamr.com>
2018-06-18 20:47:30 +0300

    Revert "MessageHub#run logging limit time"
    This reverts commit 1d7392b7be84ac37c8bb34456e93918b020dbf93.

--- src/java/com/unifina/feed/MessageHub.java
+++ src/java/com/unifina/feed/MessageHub.java
@@ -27,7 +27,6 @@ public class MessageHub<RawMessageClass, MessageClass, KeyClass> extends Thread 
 	private final List<MessageRecipient> proxies = new ArrayList<>();
 	private final List<MessageRecipient> proxiesByPriority = new ArrayList<>();
 	private final Map<KeyClass, List<MessageRecipient>> proxiesByKey = new HashMap<>();
-	private long lastRunLoggingTime = 0;
 
 	private final Comparator<MessageRecipient> proxyPriorityComparator = new Comparator<MessageRecipient>() {
 		@Override
@@ -74,12 +73,8 @@ public class MessageHub<RawMessageClass, MessageClass, KeyClass> extends Thread 
 			}
 
 			if (parsedMessage != null) {
-				long time = System.currentTimeMillis();
-				if (time - lastRunLoggingTime > 60000) {
-					lastRunLoggingTime = time;
-					final String msg = String.format("run: MessageHub.proxiesByKey size = %d", proxiesByKey.size());
-					log.info(msg);
-				}
+				final String msg = String.format("run: MessageHub.proxiesByKey size = %d", proxiesByKey.size());
+				log.info(msg);
 				try {
 					// If the message contains a key, distribute to subscribers for that key only
 					List<MessageRecipient> proxyList = m.key != null ? proxiesByKey.get(m.key) : proxiesByPriority;
