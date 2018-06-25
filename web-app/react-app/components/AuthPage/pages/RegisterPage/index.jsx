33533bdb7f25e950b4fce91aa87a6e273acaf9c7
Mariusz Roliński <mariusz@mondoreale.pl>
2018-06-25 13:59:03 +0200

    Comment RegisterPage out for now

--- web-app/react-app/components/AuthPage/pages/RegisterPage/index.jsx
+++ web-app/react-app/components/AuthPage/pages/RegisterPage/index.jsx
@@ -2,48 +2,49 @@
 
 import React from 'react'
 
-import AuthPanel from '../../shared/AuthPanel'
-import Input from '../../shared/Input'
-import Actions from '../../shared/Actions'
-import Button from '../../shared/Button'
-import AuthStep from '../../shared/AuthStep'
+// import AuthPanel from '../../shared/AuthPanel'
+// import Input from '../../shared/Input'
+// import Actions from '../../shared/Actions'
+// import Button from '../../shared/Button'
+// import AuthStep from '../../shared/AuthStep'
 
 const RegisterPage = () => (
-    <AuthPanel>
-        <AuthStep title="Sign Up" showEth showSignup>
-            <Input placeholder="Your Name" />
-            <Actions>
-                <Button proceed>Next</Button>
-            </Actions>
-        </AuthStep>
-        <AuthStep title="Sign Up">
-            <Input placeholder="Create a Password" type="password" />
-            <Actions>
-                <Button proceed>Next</Button>
-            </Actions>
-        </AuthStep>
-        <AuthStep title="Sign Up" showBack>
-            <Input placeholder="Confirm your password" type="password" />
-            <Actions>
-                <Button proceed>Next</Button>
-            </Actions>
-        </AuthStep>
-        <AuthStep title="Timezone" showBack>
-            Timezone
-            <Actions>
-                <Button proceed>Next</Button>
-            </Actions>
-        </AuthStep>
-        <AuthStep title="Terms">
-            Terms
-            <Actions>
-                <Button proceed>Finish</Button>
-            </Actions>
-        </AuthStep>
-        <AuthStep title="Done" showSignin>
-            Yep.
-        </AuthStep>
-    </AuthPanel>
+    <div />
+    // <AuthPanel>
+    //     <AuthStep title="Sign Up" showEth showSignup>
+    //         <Input placeholder="Your Name" />
+    //         <Actions>
+    //             <Button proceed>Next</Button>
+    //         </Actions>
+    //     </AuthStep>
+    //     <AuthStep title="Sign Up">
+    //         <Input placeholder="Create a Password" type="password" />
+    //         <Actions>
+    //             <Button proceed>Next</Button>
+    //         </Actions>
+    //     </AuthStep>
+    //     <AuthStep title="Sign Up" showBack>
+    //         <Input placeholder="Confirm your password" type="password" />
+    //         <Actions>
+    //             <Button proceed>Next</Button>
+    //         </Actions>
+    //     </AuthStep>
+    //     <AuthStep title="Timezone" showBack>
+    //         Timezone
+    //         <Actions>
+    //             <Button proceed>Next</Button>
+    //         </Actions>
+    //     </AuthStep>
+    //     <AuthStep title="Terms">
+    //         Terms
+    //         <Actions>
+    //             <Button proceed>Finish</Button>
+    //         </Actions>
+    //     </AuthStep>
+    //     <AuthStep title="Done" showSignin>
+    //         Yep.
+    //     </AuthStep>
+    // </AuthPanel>
 )
 
 export default RegisterPage
