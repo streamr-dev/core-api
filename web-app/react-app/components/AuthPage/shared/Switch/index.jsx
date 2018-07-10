ad59a5af4124bf8a0a291374e63bf46cc636b2f4
Aapeli <aapeli.haanpuu@streamr.com>
2018-07-10 10:29:55 +0200

    Change Switch to use react-transition-groups

--- web-app/react-app/components/AuthPage/shared/Switch/index.jsx
+++ web-app/react-app/components/AuthPage/shared/Switch/index.jsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react'
 import cx from 'classnames'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 import styles from './switch.pcss'
 
@@ -19,58 +20,19 @@ class Switch extends React.Component<Props, State> {
         current: 0,
     }
 
-    elements: Array<?HTMLDivElement> = []
-
-    timeout: TimeoutID
-
-    focus = () => {
-        clearTimeout(this.timeout)
-
-        this.timeout = setTimeout(() => {
-            const { current } = this.props
-            const root = this.elements[current]
-
-            if (root) {
-                const input = root.querySelector('input')
-
-                if (input) {
-                    input.focus()
-                }
-            }
-        }, 100)
-    }
-
-    setElementAt = (index: number) => (element: ?HTMLDivElement) => {
-        this.elements[index] = element
-    }
-
-    componentDidMount = () => {
-        this.focus()
-    }
-
-    componentDidUpdate = (prevProps: Props) => {
-        const { current } = this.props
-
-        if (current !== prevProps.current) {
-            this.focus()
-        }
-    }
-
     render = () => {
         const { current, children } = this.props
 
         return (
-            <div className={styles.root}>
-                {React.Children.map(children, (child, index) => (
-                    <div
-                        className={cx(styles.switchable, {
-                            [styles.active]: current === index,
-                        })}
-                        ref={this.setElementAt(index)}
-                    >
-                        {child}
-                    </div>
-                ))}
+            <div className={styles.switch}>
+                <CSSTransitionGroup
+                    transitionName="switchTransition"
+                    transitionAppear={false}
+                    transitionEnterTimeout={200}
+                    transitionLeaveTimeout={200}
+                >
+                    {React.Children.toArray(children)[current]}
+                </CSSTransitionGroup>
             </div>
         )
     }
