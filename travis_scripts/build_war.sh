1b9b847f5f82f43fea3df70b15b0259e0c0bd324
mikhaelsantos <mikhaelsantos@gmail.com>
2018-08-09 13:38:32 +0300

    chore: Add codedeploy support:
    * Convert release to codedeploy bundle
    * Insert codedpeloy provider

--- travis_scripts/build_war.sh
+++ travis_scripts/build_war.sh
@@ -5,3 +5,5 @@ npm run build
 grails prod war
 mkdir build
 cp $(pwd)/target/ROOT.war $(pwd)/build
+mv $(pwd)/.appspec.yml $(pwd)/build/appspec.yml
+tar -czvf $(pwd)/build/ee.tar $(pwd)/build/ROOT.war $(pwd)/build/appspec.yml
