71f9ded2ba5356e3b180ea13a078801be299133e
mikhaelsantos <mikhaelsantos@gmail.com>
2018-08-27 12:11:13 +0300

    fix: add codeploy to tar path

--- travis_scripts/build_war.sh
+++ travis_scripts/build_war.sh
@@ -8,5 +8,5 @@ cp $(pwd)/target/ROOT.war $(pwd)/build
 # Set .appspec in root
 mv $(pwd)/.codedeploy/.appspec.yml $(pwd)/build/appspec.yml
 # Copy bash scripts to be deployed in the tar
-mv $(pwd)/.codedeploy $(pwd)/build
-tar -czvf $(pwd)/build/ee.tar -C $(pwd)/build ROOT.war appspec.yml
+mv $(pwd)/.codedeploy $(pwd)/build/
+tar -czvf $(pwd)/build/ee.tar -C $(pwd)/build ROOT.war appspec.yml .codedeploy
