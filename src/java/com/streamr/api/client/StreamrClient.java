7d7b67dcb8f4c7d1f08a4c187b1d98442de5da98
Kare Nuorteva <kare.nuorteva@streamr.com>
2018-09-04 16:11:25 +0300

    Add shutdown() to Java Client

--- src/java/com/streamr/api/client/StreamrClient.java
+++ src/java/com/streamr/api/client/StreamrClient.java
@@ -1,5 +1,9 @@
 package com.streamr.api.client;
 
+import java.util.List;
+import java.util.Map;
+
 public interface StreamrClient {
 	CanvasesPerNode canvasesPerNode(String token, String nodeIp);
+	List<Map<String, Object>> shutdown(String token, String nodeIp);
 }
--- src/java/com/streamr/api/client/StreamrClientProvider.java
+++ src/java/com/streamr/api/client/StreamrClientProvider.java
@@ -2,10 +2,14 @@ package com.streamr.api.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
 
 public class StreamrClientProvider implements StreamrClient {
 	// webTarget is the base URL for Streamr REST API service derived from baseUrl (without trailing slash).
@@ -28,5 +32,23 @@ public class StreamrClientProvider implements StreamrClient {
 		final Response res = target.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, token).get();
 		final CanvasesPerNode result = res.readEntity(CanvasesPerNode.class);
 		return result;
+	}
+
+	/**
+	 * @param token is the value of HTTP Authorization header. For example: "token value-of-token".
+	 */
+	@Override
+	public List<Map<String, Object>> shutdown(final String token, final String nodeIp) {
+		final WebTarget target = webTarget.path(String.format("/nodes/%s/shutdown", nodeIp));
+		final Entity<String> input = Entity.json("");
+		final GenericType<List<Map<String, Object>>> resultType = new GenericType<List<Map<String, Object>>>() {};
+		final List<Map<String, Object>> res = target.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, token).post(input, resultType);
+		return res;
+	}
+
+	public static void main(final String... args) {
+		StreamrClient client = new StreamrClientProvider("http://localhost:8081/streamr-core/api/v1");
+		final List<Map<String, Object>> list = client.shutdown("token tester-admin-api-key", "192.168.10.116");
+		System.out.println(list);
 	}
 }
