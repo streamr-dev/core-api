89d4c036e46aebd0c87a3375e2e421b390aacc12
mthambipillai <melchior.thambipillai@streamr.com>
2018-10-04 15:44:57 +0200

    add challenge response verification method

--- grails-app/services/com/unifina/service/ChallengeService.groovy
+++ grails-app/services/com/unifina/service/ChallengeService.groovy
@@ -1,5 +1,6 @@
 package com.unifina.service
 
+import com.unifina.crypto.ECRecover
 import com.unifina.domain.security.Challenge
 
 class ChallengeService {
@@ -9,5 +10,20 @@ class ChallengeService {
 		ch.challenge = "This is a challenge created by Streamr to prove private key ownership by signing this random data with it:\n\n" + ch.id
 		ch.save()
 		return ch
+	}
+
+	boolean verifyChallengeResponse(String challengeID, String challenge, String signature, String publicKey) {
+		Challenge dbChallenge = Challenge.get(challengeID)
+		boolean invalidChallenge = dbChallenge == null || challenge != dbChallenge.challenge
+		if(invalidChallenge){
+			return false
+		}
+		byte[] messageHash = ECRecover.calculateMessageHash(challenge)
+		String address = ECRecover.recoverAddress(messageHash, signature)
+		boolean valid = address == publicKey
+		if(valid){
+			dbChallenge.delete()
+		}
+		return valid
 	}
 }
