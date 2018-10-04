02ef99569ca96cf20aa285d69e86d80546355031
mthambipillai <melchior.thambipillai@streamr.com>
2018-10-04 15:47:05 +0200

    add challenge response endpoint

--- grails-app/controllers/com/unifina/controller/api/LoginApiController.groovy
+++ grails-app/controllers/com/unifina/controller/api/LoginApiController.groovy
@@ -1,19 +1,38 @@
 package com.unifina.controller.api
 
+import com.unifina.api.ApiException
+import com.unifina.crypto.ECRecover
+import com.unifina.domain.security.SessionToken
 import com.unifina.security.AuthLevel
 import com.unifina.security.StreamrApi
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
+import com.unifina.domain.security.Challenge
 
 @Secured(["IS_AUTHENTICATED_ANONYMOUSLY"])
 class LoginApiController {
 
-	static allowedMethods = [challenge: "POST"]
+	static allowedMethods = [challenge: "POST", response: "POST"]
 	def challengeService
+	def sessionService
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
 	def challenge() {
 		def ch = challengeService.createChallenge()
 		render([id: ch.id, challenge: ch.challenge] as JSON)
+	}
+
+	@StreamrApi(authenticationLevel = AuthLevel.NONE)
+	def response(ChallengeResponseCommand cmd) {
+		def valid = challengeService.verifyChallengeResponse(cmd.challenge.id, cmd.challenge.challenge, cmd.signature, cmd.address)
+		if(!valid){
+			throw new ApiException(400, 'INVALID_CHALLENGE', "challenge validation failed: "+cmd.challenge.id+" "+cmd.address)
+		}else{
+			SessionToken sk = sessionService.generateToken(cmd.address)
+			render([
+				token	: sk.token,
+				expires	: sk.expiration.toString()
+			] as JSON)
+		}
 	}
 }
