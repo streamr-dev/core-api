de8facdf14ddb4571c271ca8848d9183dcadea52
mthambipillai <melchior.thambipillai@streamr.com>
2018-10-05 18:46:30 +0200

    update test

--- test/unit/com/unifina/controller/api/LoginApiControllerSpec.groovy
+++ test/unit/com/unifina/controller/api/LoginApiControllerSpec.groovy
@@ -4,25 +4,27 @@ import com.unifina.api.ApiException
 import com.unifina.domain.security.Challenge
 import com.unifina.domain.security.Key
 import com.unifina.domain.security.SecUser
-import com.unifina.domain.security.SessionToken
+import com.unifina.security.SessionToken
 import com.unifina.service.ChallengeService
+import com.unifina.service.EthereumIntegrationKeyService
 import com.unifina.service.SessionService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
-import org.joda.time.DateTime
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(LoginApiController)
-@Mock([Challenge, SessionToken, SecUser, com.unifina.filters.UnifinaCoreAPIFilters, Key])
+@Mock([Challenge, SecUser, com.unifina.filters.UnifinaCoreAPIFilters, Key])
 class LoginApiControllerSpec extends Specification {
 	ChallengeService challengeService
 	SessionService sessionService
+	EthereumIntegrationKeyService ethereumIntegrationKeyService
 	def setup() {
 		challengeService = controller.challengeService = Mock(ChallengeService)
 		sessionService = controller.sessionService = Mock(SessionService)
+		ethereumIntegrationKeyService = controller.ethereumIntegrationKeyService = Mock(EthereumIntegrationKeyService)
 		def me = new SecUser().save(failOnError: true, validate: false)
 		Key key = new Key(name: "key", user: me)
 		key.id = "myApiKey"
@@ -53,8 +55,15 @@ class LoginApiControllerSpec extends Specification {
 
 		Challenge challenge = new Challenge(id: "id", challenge: "challenge").save(failOnError: true, validate: true)
 
-		SessionToken sk = new SessionToken(id: "id", token: "token", associatedAddress: address, expiration: new DateTime())
-			.save(failOnError: true, validate: true)
+		SecUser user = new SecUser(
+			username: "username",
+			password: "password",
+			name: "name",
+			email: "email@email.com",
+			timezone: "timezone"
+		).save(failOnError: true, validate: false)
+
+		SessionToken sk = new SessionToken(64, user, 3)
 
 		when:
 		request.addHeader("Authorization", "Token myApiKey")
@@ -73,13 +82,14 @@ class LoginApiControllerSpec extends Specification {
 		}
 
 		then:
+		1 * challengeService.verifyChallengeResponse(challenge.id, challenge.challenge, signature, address) >> true
+		1 * ethereumIntegrationKeyService.getOrCreateFromEthereumAddress(address) >> user
+		1 * sessionService.generateToken(user) >> sk
 		response.status == 200
 		response.json == [
-			token	: sk.token,
-			expires	: sk.expiration.toString()
+			token	: sk.getToken(),
+			expires	: sk.getExpiration().toString()
 		]
-		1 * challengeService.verifyChallengeResponse(challenge.id, challenge.challenge, signature, address) >> true
-		1 * sessionService.generateToken(address) >> sk
 	}
 
 	def "response to challenge should fail"() {
