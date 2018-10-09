e95200c900cd884de69d4b1857f1f051424aa2bd
mthambipillai <melchior.thambipillai@streamr.com>
2018-10-09 17:34:02 +0200

    add password-based login endpoint

--- grails-app/controllers/com/unifina/controller/api/LoginApiController.groovy
+++ grails-app/controllers/com/unifina/controller/api/LoginApiController.groovy
@@ -15,6 +15,7 @@ class LoginApiController {
 	def challengeService
 	def sessionService
 	def ethereumIntegrationKeyService
+	def userService
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
 	def challenge() {
@@ -24,10 +25,13 @@ class LoginApiController {
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
 	def response(ChallengeResponseCommand cmd) {
+		if(cmd.challenge==null){
+			throw new ApiException(400, 'INVALID_CHALLENGE', "challenge validation failed")
+		}
 		boolean valid = challengeService.verifyChallengeResponse(cmd.challenge.id,
 			cmd.challenge.challenge, cmd.signature.toLowerCase(), cmd.address.toLowerCase())
 		if(!valid){
-			throw new ApiException(400, 'INVALID_CHALLENGE', "challenge validation failed")
+			throw new ApiException(400, 'INVALID_CHALLENGE', "challenge-based login failed")
 		}else{
 			SecUser user = ethereumIntegrationKeyService.getOrCreateFromEthereumAddress(cmd.address)
 			SessionToken sk = sessionService.generateToken(user)
@@ -36,6 +40,19 @@ class LoginApiController {
 				expires	: sk.getExpiration().toString()
 			] as JSON)
 		}
+	}
+
+	@StreamrApi(authenticationLevel = AuthLevel.NONE)
+	def password(UsernamePasswordCommand cmd) {
+		SecUser user = userService.getUserFromUsernameAndPassword(cmd.username, cmd.password)
+		if(user==null){
+			throw new ApiException(400, 'INVALID_USERNAME_OR_PASSWORD', "password-based login failed")
+		}
+		SessionToken sk = sessionService.generateToken(user)
+		render([
+			token	: sk.getToken(),
+			expires	: sk.getExpiration().toString()
+		] as JSON)
 	}
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
