3d6d1c3676925f968b18f29cd65c7190f4231890
mthambipillai <melchior.thambipillai@streamr.com>
2018-10-17 10:31:28 +0200

    add toMap method for Challenge and SessionToken

--- src/java/com/unifina/security/SessionToken.java
+++ src/java/com/unifina/security/SessionToken.java
@@ -4,12 +4,26 @@ import com.unifina.domain.security.SecUser;
 import org.apache.commons.lang.RandomStringUtils;
 import org.joda.time.DateTime;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class SessionToken {
 	private String token;
 	private SecUser user;
 	private Date expiration;
+
+	private static DateFormat df;
+	private static DateFormat getDateFormat() {
+		if (df == null) {
+			df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+			df.setTimeZone(TimeZone.getTimeZone("UTC"));
+		}
+		return df;
+	}
 
 	public SessionToken(int tokenLength, SecUser user, int ttlHours) {
 		this.token = RandomStringUtils.randomAlphanumeric(tokenLength);
@@ -27,5 +41,12 @@ public class SessionToken {
 
 	public Date getExpiration() {
 		return expiration;
+	}
+
+	public Map<String, Object> toMap() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("token", token);
+		map.put("expires", getDateFormat().format(expiration));
+		return map;
 	}
 }
