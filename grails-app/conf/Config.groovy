40f02bae7411ec458f92453855dc3f6b81a0ad20
Eric Andrews <eric.andrews@streamr.com>
2018-11-07 12:24:25 +0200

    CORE-1567: enable CORS for j_spring_security_check

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -211,7 +211,7 @@ environments {
 /**
  * API & CORS config
  */
-cors.url.pattern = ['/api/*', '/contact/send', '/profile/*', '/logout/*', '/login/*']
+cors.url.pattern = ['/api/*', '/contact/send', '/profile/*', '/logout/*', '/login/*', '/j_spring_security_check']
 streamr.apiKey.revokeNotificationStream = "revoked-api-keys"
 
 /**
