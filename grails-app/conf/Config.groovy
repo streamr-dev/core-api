8b686bdf44cd93e81dda6f7f708cc9a542e1c96f
Eric Andrews <eric.andrews@streamr.com>
2018-11-09 15:08:32 +0200

    enable more CORS

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -211,7 +211,7 @@ environments {
 /**
  * API & CORS config
  */
-cors.url.pattern = ['/api/*', '/contact/send', '/profile/*', '/logout/*', '/login/*', '/j_spring_security_check', '/canvas']
+cors.url.pattern = ['/api/*', '/contact/send', '/profile/*', '/logout/*', '/login/*', '/j_spring_security_check', '/canvas', '/auth/forgotPassword', '/logout', '/auth/register', '/auth/resetPassword', '/auth/signup']
 streamr.apiKey.revokeNotificationStream = "revoked-api-keys"
 
 /**
