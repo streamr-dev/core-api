3834f2b29cfdc60f4f34aae8d5ec6e558bd6ab4d
mthambipillai <melchior.thambipillai@streamr.com>
2018-11-13 15:48:31 +0100

    add tests for version 29

--- test/unit/com/unifina/feed/StreamrBinaryMessageSpec.groovy
+++ test/unit/com/unifina/feed/StreamrBinaryMessageSpec.groovy
@@ -7,8 +7,12 @@ import java.nio.ByteBuffer
 
 class StreamrBinaryMessageSpec extends Specification {
 
-	def msg = new StreamrBinaryMessage("testId", 0, System.currentTimeMillis(), 100,
+	StreamrBinaryMessage msg = new StreamrBinaryMessage("testId", 0, System.currentTimeMillis(), 100,
 		StreamrBinaryMessage.CONTENT_TYPE_STRING, "foobar hello world 666".getBytes("UTF-8"))
+	StreamrBinaryMessage msg2 = new StreamrBinaryMessage((byte) 29, "testId", 0, System.currentTimeMillis(), 100,
+		StreamrBinaryMessage.CONTENT_TYPE_STRING, "foobar hello world 666".getBytes("UTF-8"),
+		StreamrBinaryMessage.SIGNATURE_TYPE_ETH, '0xF915eD664e43C50eB7b9Ca7CfEB992703eDe55c4',
+		'0xcb1fa20f2f8e75f27d3f171d236c071f0de39e4b497c51b390306fc6e7e112bb415ecea1bd093320dd91fd91113748286711122548c52a15179822a014dc14931b')
 
 	def "data is not altered on encode/decode"() {
 
@@ -25,9 +29,25 @@ class StreamrBinaryMessageSpec extends Specification {
 		new String(decoded.getContentBytes(), "UTF-8") == new String(msg.getContentBytes(), "UTF-8")
 	}
 
+	def "data is not altered on encode/decode for version 29"() {
+
+		when:
+		byte[] encoded = msg2.toBytes()
+		StreamrBinaryMessage decoded = new StreamrBinaryMessage(ByteBuffer.wrap(encoded))
+
+		then:
+		decoded.getStreamId() == msg2.getStreamId()
+		decoded.getPartition() == msg2.getPartition()
+		decoded.getTimestamp() == msg2.getTimestamp()
+		decoded.getContentType() == msg2.getContentType()
+		decoded.getTTL() == msg2.getTTL()
+		new String(decoded.getContentBytes(), "UTF-8") == new String(msg2.getContentBytes(), "UTF-8")
+	}
+
 	def "sizeInBytes reports correct size"() {
 		expect:
 		msg.toBytes().length == msg.sizeInBytes()
+		msg2.toBytes().length == msg2.sizeInBytes()
 	}
 
 }
