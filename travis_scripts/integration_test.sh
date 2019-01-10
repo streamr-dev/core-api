b6cebc9cfa6cb7773a5e8f4f450c472019ebdeb8
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-01-10 18:09:25 +0100

    add -no-reports flag to integration tests

--- travis_scripts/integration_test.sh
+++ travis_scripts/integration_test.sh
@@ -4,4 +4,6 @@ git clone https://github.com/streamr-dev/streamr-docker-dev.git
 sudo ifconfig docker0 10.200.10.1/24
 $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start 1
 grails clean
-grails test-app -integration --stacktrace
+
+# Report generation phase will crash if a SecurityManager is installed. Fix: skip report generation with -no-reports
+grails test-app -no-reports -integration --stacktrace
