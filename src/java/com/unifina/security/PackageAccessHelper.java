3c0ff4ab00809537492f0705ac542a90356324b4
Eric Andrews <eric.andrews@streamr.com>
2019-01-10 16:41:43 +0200

    PackageAccessHelper: prevent System in dev env

--- src/java/com/unifina/security/PackageAccessHelper.java
+++ src/java/com/unifina/security/PackageAccessHelper.java
@@ -6,29 +6,37 @@ import java.util.List;
 import java.util.Set;
 
 public class PackageAccessHelper {
-	
+
 	private static final Set<String> restrictedClasses = new HashSet<>();
-	
+
 	static {
 		restrictedClasses.add("java.lang.Thread");
+		restrictedClasses.add("java.lang.ThreadGroup");
+		restrictedClasses.add("java.lang.ThreadLocal");
 		restrictedClasses.add("java.lang.Runtime");
+		restrictedClasses.add("java.lang.System");
+		restrictedClasses.add("java.lang.Compiler");
+		restrictedClasses.add("java.lang.ClassLoader");
+		restrictedClasses.add("java.lang.Process");
+		restrictedClasses.add("java.lang.ProcessBuilder");
+		restrictedClasses.add("java.lang.SecurityManager");
 	}
-	
+
 	private static final String[] allowedPackages = new String[] {
 			"CustomModule",
 			"java.lang.",
 			"java.math.",
 			"java.util.",
-			
+
 			"java.text.SimpleDateFormat",
-			
+
 			"com.unifina.signalpath.",
 			"com.unifina.orderbook.event.",
 			"com.unifina.order.",
 			"com.unifina.orderbook.",
 			"com.unifina.utils.",
 			"com.unifina.event.",
-			
+
 			"org.apache.commons.math3",
 			"org.springsource.loaded.TypeRegistry", // safe?
 			"sun.reflect.SerializationConstructorAccessorImpl", // safe?
@@ -48,27 +56,27 @@ public class PackageAccessHelper {
 			"com.google.common.io.ByteStreams",
 			"java.security.MessageDigest"
 	};
-	
+
 	private static boolean checkMatches(String className, String[] packages) {
     	for (String s : packages)
     		if (className.startsWith(s))
     			return true;
     	return false;
 	}
-	
+
     public static boolean checkAccess(String className, String codeBase) {
     	// Matches for restricted packages are checked in Java security system, if listed in packages.access security property
     	if (restrictedClasses.contains(className) || !checkMatches(className, allowedPackages))
     		throw new SecurityException("Access denied to "+className);
     	else return true;
-    } 
-    
+    }
+
     public static List<String> getRestrictedPackages() {
     	return Arrays.asList(restrictedClasses.toArray(new String[restrictedClasses.size()]));
     }
-    
+
     public static List<String> getAllowedPackages() {
     	return Arrays.asList(allowedPackages);
     }
-    
+
 }
