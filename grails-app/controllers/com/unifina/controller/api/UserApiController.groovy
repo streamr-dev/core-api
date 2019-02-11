ab03ad15c012ecf215539330b6d36157c3dc007e
Kare Nuorteva <kare.nuorteva@streamr.com>
2019-02-11 11:43:53 +0200

    CORE-1593: Remove user account

--- grails-app/controllers/com/unifina/controller/api/UserApiController.groovy
+++ grails-app/controllers/com/unifina/controller/api/UserApiController.groovy
@@ -1,14 +1,29 @@
 package com.unifina.controller.api
 
+import com.unifina.domain.security.SecUser
 import com.unifina.security.StreamrApi
+import com.unifina.service.UserService
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(["IS_AUTHENTICATED_ANONYMOUSLY"])
 class UserApiController {
+	static allowedMethods = [
+		getUserInfo: "GET",
+		delete: "DELETE",
+	]
+
+	UserService userService
 
 	@StreamrApi
 	def getUserInfo() {
 		render(request.apiUser?.toMap() as JSON)
+	}
+
+	@StreamrApi
+	def delete(Long id) {
+		SecUser user = (SecUser) request.apiUser
+		userService.delete(user, id)
+		render(status: 204, "")
 	}
 }
