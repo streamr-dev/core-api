6c5670802a666f1d6b1016443f89978abc58e0b7
Melchior Thambipillai <melkiorthambipillai@gmail.com>
2019-03-25 16:48:15 +0100

    replace murmur3 with md5 (#579)
    * replace murmur3 with md5
    
    * Update misc.xml

--- src/java/com/unifina/data/StreamPartitioner.java
+++ src/java/com/unifina/data/StreamPartitioner.java
@@ -1,16 +1,15 @@
 package com.unifina.data;
 
-import com.google.common.hash.HashFunction;
 import com.unifina.domain.data.Stream;
 
 import javax.annotation.Nullable;
-import java.io.Serializable;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class StreamPartitioner{
-
-	private static HashFunction murmur3_32 = com.google.common.hash.Hashing.murmur3_32(0);
 	private static final Charset utf8 = Charset.forName("UTF-8");
 
 	public static int partition(Stream stream, @Nullable String partitionKey) {
@@ -22,17 +21,21 @@ public class StreamPartitioner{
 			// Fast common case
 			return 0;
 		} else if (partitionKey != null) {
-			byte[] result = murmur3_32.newHasher()
-					.putBytes(partitionKey)
-					.hash()
-					.asBytes();
-
-			// Big-endian interpretation of the result as int
-			int intHash = ((result[0] & 0xFF) << 24) | ((result[1] & 0xFF) << 16) | ((result[2] & 0xFF) << 8) | (result[3] & 0xFF);
+			int intHash = hash(partitionKey);
 			return Math.abs(intHash) % partitionCount;
 		} else {
 			// Fallback to random partition if no key
 			return ThreadLocalRandom.current().nextInt(partitionCount);
 		}
+	}
+
+	private static int hash(byte[] partitionKey) {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		return ByteBuffer.wrap(md.digest(partitionKey)).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
 	}
 }
