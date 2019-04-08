7e94df15cdc68409d11954a54311b08dfaa434ca
Jonathan Wolff <jwolff@isentropy.com>
2019-04-08 18:39:00 +0200

    working test

--- test/unit/com/unifina/signalpath/blockchain/Web3jHelperSpec.groovy
+++ test/unit/com/unifina/signalpath/blockchain/Web3jHelperSpec.groovy
@@ -1,14 +1,18 @@
 package com.unifina.signalpath.blockchain
 
+import com.unifina.ModuleTestingSpecification
+import com.unifina.security.StreamrApi
+import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Uint
 import org.web3j.abi.datatypes.generated.Uint32
 
-class Web3jHelperSpec {
+class Web3jHelperSpec extends ModuleTestingSpecification{
+	@StreamrApi
 	void "instantiateType produces correct Solidity type"() {
 		when:
 
-		then:
+		expect:
 		Web3jHelper.instantiateType("uint",123) instanceof Uint
-		Web3jHelper.instantiateType("uint",123) instanceof Uint32
+		Web3jHelper.instantiateType("address","0x123") instanceof Address
 	}
 }
