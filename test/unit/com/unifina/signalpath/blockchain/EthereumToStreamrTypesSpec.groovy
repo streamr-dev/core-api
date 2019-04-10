af3542dbe436cec6ac5fd3448656808907012be8
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-04-10 18:57:43 +0300

    CORE-1652 fix tests

--- test/unit/com/unifina/signalpath/blockchain/EthereumToStreamrTypesSpec.groovy
+++ test/unit/com/unifina/signalpath/blockchain/EthereumToStreamrTypesSpec.groovy
@@ -1,7 +1,8 @@
 package com.unifina.signalpath.blockchain
 
-import com.google.gson.JsonElement
 import com.google.gson.JsonParser
+import com.unifina.domain.security.SecUser
+import com.unifina.signalpath.AbstractSignalPathModule
 import com.unifina.signalpath.BooleanOutput
 import com.unifina.signalpath.ListOutput
 import com.unifina.signalpath.StringOutput
@@ -9,14 +10,26 @@ import com.unifina.signalpath.TimeSeriesOutput
 import spock.lang.Specification
 
 class EthereumToStreamrTypesSpec extends Specification {
+	AbstractSignalPathModule module
+	def setup() {
+		module = new AbstractSignalPathModule() {
+			@Override void sendOutput() {}
+			@Override void clearState() {}
+		}
+	}
+
 	def "ConvertAndSend works for boolean output"() {
 		def output = new BooleanOutput(module, "out")
-		def input = new JsonParser().parse("1")
+		def input = new JsonParser().parse("true")
+		def output2 = new BooleanOutput(module, "out")
+		def input2 = new JsonParser().parse("1")
 
 		when:
-		module.convertAndSend(output, input)
+		EthereumToStreamrTypes.convertAndSend(output, input)
+		EthereumToStreamrTypes.convertAndSend(output2, input2)
 		then:
 		output.getValue() == true
+		output2.getValue() == false
 	}
 
 	def "ConvertAndSend works for string output"() {
@@ -24,7 +37,7 @@ class EthereumToStreamrTypesSpec extends Specification {
 		def input = new JsonParser().parse("test")
 
 		when:
-		module.convertAndSend(output, input)
+		EthereumToStreamrTypes.convertAndSend(output, input)
 		then:
 		output.getValue() == "test"
 	}
@@ -34,21 +47,18 @@ class EthereumToStreamrTypesSpec extends Specification {
 		def input = new JsonParser().parse("1")
 
 		when:
-		module.convertAndSend(output, input)
+		EthereumToStreamrTypes.convertAndSend(output, input)
 		then:
 		output.getValue() == 1
 	}
 
 	def "ConvertAndSend works for array output"() {
 		def output = new ListOutput(module, "out")
-		def input = new JsonParser().parse('["asdf":4,"qwer":"zxcv"]')
+		def input = new JsonParser().parse('["asdf",4,"qwer",true]')
 
 		when:
-		module.convertAndSend(output, input)
+		EthereumToStreamrTypes.convertAndSend(output, input)
 		then:
-		output.getValue() == [
-		    asdf: 4,
-			qwer: "zxcv"
-		]
+		output.getValue() == ["asdf", 4, "qwer", true]
 	}
 }
