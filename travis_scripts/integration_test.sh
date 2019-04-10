d997713ef292c3922723da7b9eff9d56cf6aaba9
Kare Nuorteva <kare.nuorteva@streamr.com>
2019-04-10 20:06:03 +0300

    CORE-1653: Fix randomly failing integration tests (#574)
    * CORE-163: Fix randomly failing integration tests
    
    * Skip 'npm install' in integration tests
    
    * Skip 'grails clean' in integration tests
    
    * try out travis_retry

--- travis_scripts/integration_test.sh
+++ travis_scripts/integration_test.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+
 sudo /etc/init.d/mysql stop
-npm install
 git clone https://github.com/streamr-dev/streamr-docker-dev.git
 sudo ifconfig docker0 10.200.10.1/24
-$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start 1
-grails clean
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start 1
 
 # Report generation phase will crash if a SecurityManager is installed. Fix: skip report generation with -no-reports
-grails test-app -no-reports -integration --stacktrace
+grails test-app -no-reports -integration --stacktrace --verbose
