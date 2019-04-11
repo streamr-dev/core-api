180c22e5a362f7ff578d8fefab06ea55d803cda0
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-04-11 18:30:14 +0300

    Refactor SolidityCompileDeploy
    untangled the constructor encoding functions. There really was too much abstraction on the way there. Very happy how it turned out, can't wait to deprecate streamr-web3!

--- src/java/com/unifina/signalpath/blockchain/SolidityCompileDeploy.java
+++ src/java/com/unifina/signalpath/blockchain/SolidityCompileDeploy.java
@@ -3,21 +3,15 @@ package com.unifina.signalpath.blockchain;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.unifina.data.FeedEvent;
 import com.unifina.signalpath.*;
 import com.unifina.utils.MapTraversal;
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
-import org.ethereum.solidity.compiler.Solc;
 import org.ethereum.solidity.compiler.SolidityCompiler;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.datatypes.Function;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.TransactionEncoder;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Numeric;
@@ -28,8 +22,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 public class SolidityCompileDeploy extends ModuleWithUI implements Pullable<EthereumContract> {
 
@@ -44,23 +36,18 @@ public class SolidityCompileDeploy extends ModuleWithUI implements Pullable<Ethe
 
 	private EthereumModuleOptions ethereumOptions = new EthereumModuleOptions();
 	private Web3j web3j;
+
 	@Override
 	public void init() {
 		addInput(ethereumAccount);
+
+		// TODO: document why this is needed. Parameter changes shouldn't trigger onConfiguration, but code changes should
 		ethereumAccount.setUpdateOnChange(true);
 	}
 
-	@Override
-	public void sendOutput() {
-
-	}
-
-	@Override
-	public void clearState() {
-
-	}
-
-	/** Override to provide contract template that will be compiled when module is added to canvas */
+	/**
+	 * Override to provide contract template that will be compiled when module is added to canvas
+	 */
 	public String getCodeTemplate() {
 		return null;
 	}
@@ -80,77 +67,126 @@ public class SolidityCompileDeploy extends ModuleWithUI implements Pullable<Ethe
 		return config;
 	}
 
-	protected SolidityCompiler.Options[] getSolcOptions(){
-		//List<SolidityCompiler.Options> opts = new ArrayList<SolidityCompiler.Options>();
+	protected SolidityCompiler.Options[] getSolcOptions() {
 		SolidityCompiler.Options[] opts = {SolidityCompiler.Options.ABI, SolidityCompiler.Options.BIN};
 		return opts;
 	}
 
-	protected JsonObject compile(String solidity_code) throws IOException {
-		SolidityCompiler.Result rslt = SolidityCompiler.compile(solidity_code.getBytes(StandardCharsets.UTF_8),true,getSolcOptions());
-		if(rslt.isFailed()){
-			throw new IOException("Error compiling contract: " + rslt.errors);
+	protected JsonObject compile(String solidity_code) {
+		String result = null;
+		String errors = null;
+		try {
+			SolidityCompiler.Result res = SolidityCompiler.compile(solidity_code.getBytes(StandardCharsets.UTF_8), true, getSolcOptions());
+			if (res.isFailed()) {
+				errors = res.errors;
+			} else {
+				result = res.output;
+			}
+		} catch (IOException e) {
+			errors = e.getLocalizedMessage();
 		}
-		JsonParser parser = new JsonParser();
-		Set<Map.Entry<String,JsonElement>> entries = parser.parse(rslt.output).getAsJsonObject().get("contracts").getAsJsonObject().entrySet();
-		Map.Entry<String,JsonElement> e = entries.iterator().next();
+		if (errors != null) {
+			throw new RuntimeException("Error compiling contract: " + errors);
+		}
+
+		// TODO: in case of many contracts, pick the one with the longest bytecode (or find better heuristic for the "main" conrtact)
+		Set<Map.Entry<String, JsonElement>> entries = new JsonParser().parse(result).getAsJsonObject().get("contracts").getAsJsonObject().entrySet();
+		Map.Entry<String, JsonElement> e = entries.iterator().next();
 		return e.getValue().getAsJsonObject();
 	}
 
-	protected BigInteger getGasLimit(){
-		return BigInteger.valueOf(6000000l);
-	}
-	protected Function encodeConstructor(List<Object> args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+	protected String deploy(String bytecode, List<Object> args, BigInteger sendWei) throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		BigInteger gasPrice = BigDecimal.valueOf(ethereumOptions.getGasPriceWei()).toBigInteger();
+		BigInteger nonce = web3j.ethGetTransactionCount(ethereumAccount.getAddress(), DefaultBlockParameterName.LATEST).send().getTransactionCount();
+		Credentials credentials = Credentials.create(ethereumAccount.getPrivateKey());
+
+		// convert constructor arguments into web3j "Types" that contain both a Java class and the argument's value
 		EthereumABI.Function constructor = contract.getABI().getConstructor();
-		ArrayList<String> solidity_inputs = new ArrayList<String>(constructor.inputs.size());
-		ArrayList<String> solidity_outputs = new ArrayList<String>(constructor.outputs.size());
-		for(EthereumABI.Slot s : constructor.inputs)
-			solidity_inputs.add(s.type);
-		for(EthereumABI.Slot s : constructor.outputs)
-			solidity_outputs.add(s.type);
-		return Web3jHelper.encodeFunction(constructor.name,solidity_inputs,args,solidity_outputs);
+		int argCount = Math.min(constructor.inputs.size(), args.size());
+		List<org.web3j.abi.datatypes.Type> argTypes = new ArrayList<>(argCount);
+		for (int i = 0; i < argCount; i++) {
+			String argType = constructor.inputs.get(i).type;
+			argTypes.add(Web3jHelper.instantiateType(argType, args.get(i)));
+		}
+		String encodedArgs = org.web3j.abi.FunctionEncoder.encodeConstructor(argTypes);
+		String txBytes = bytecode + encodedArgs;
+		RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, gasPrice, getGasLimit(), null, sendWei, txBytes);
+		byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+		String hexValue = Numeric.toHexString(signedMessage);
+
+		/* TODO: async deploy not implemented. Requires ability to send the resulting address to the editor UI during edit time.
+		CompletableFuture<EthSendTransaction> cf = web3j.ethSendRawTransaction(hexValue).sendAsync();
+		cf.thenAccept(new Consumer<EthSendTransaction>() {
+			@Override
+			public void accept(EthSendTransaction tx) {
+			}
+		});
+		*/
+		// synchronous deploy:
+		EthSendTransaction tx = web3j.ethSendRawTransaction(hexValue).send();
+		String txhash = tx.getTransactionHash();
+		log.debug("TX response: " + txhash);
+		String address = web3j.ethGetTransactionReceipt(txhash).send().getResult().getContractAddress();
+		return address;
 	}
 
+	protected BigInteger getGasLimit() {
+		return BigInteger.valueOf(6000000l);
+	}
+
+	protected Web3j getWeb3j() {
+		return Web3j.build(new HttpService(ethereumOptions.getRpcUrl()));
+	}
 
 	@Override
 	protected void onConfiguration(Map<String, Object> config) {
 		super.onConfiguration(config);
-		web3j = Web3j.build(new HttpService(ethereumOptions.getRpcUrl()));
 
-		Object codeFromEditor = config.get("code");
-		if (codeFromEditor == null) {
-			code = getCodeTemplate();			// initially, get template from inheriting class e.g. PayByUse
-		} else {
-			code = codeFromEditor.toString();
-		}
-
-		if (config.containsKey("contract")) {
-			contract = EthereumContract.fromMap(MapTraversal.getMap(config, "contract"));
+		// the button to send "deploy" message shouldn't be visible after deployment
+		// onConfiguration may still be triggered by parameter changes
+		if (contract != null && contract.isDeployed()) {
+			if (config.containsKey("compile")) {
+				throw new RuntimeException("Already deployed, changes ignored. Please create a new module.");
+			}
+			return;
 		}
 
 		ModuleOptions options = ModuleOptions.get(config);
 		ethereumOptions = EthereumModuleOptions.readFrom(options);
-		//final StreamrWeb3Interface web3 = createWeb3Interface();
-		try {
-			JsonParser parser = new JsonParser();
-			if (config.containsKey("compile") || (code != null && !code.trim().isEmpty() && ethereumAccount.getAddress() != null && (contract == null || !contract.isDeployed()))) {
-				JsonObject jsonTree = compile(code);
-				contract = new EthereumContract(null, new EthereumABI(parser.parse(jsonTree.get("abi").getAsString()).getAsJsonArray()));
-//				contract = web3.compile(code);
+		web3j = getWeb3j();
+		boolean compileRequested = config.containsKey("compile");
+		boolean deployRequested = config.containsKey("deploy");
+
+		// old contract, previously compiled
+		if (config.containsKey("contract")) {
+			contract = EthereumContract.fromMap(MapTraversal.getMap(config, "contract"));
+		}
+
+		JsonObject compilationResult = null;
+		if (compileRequested || deployRequested) {
+			Object codeFromEditor = config.get("code");
+			if (codeFromEditor == null) {
+				code = getCodeTemplate();            // initially, get template from inheriting class e.g. PayByUse
+			} else {
+				code = codeFromEditor.toString();
 			}
-			if (config.containsKey("deploy")) {
-				if(contract != null && contract.isDeployed())
-					return;
-				JsonObject jsonTree = compile(code);
-				// Make sure the contract is compiled
-				if (contract == null) {
-					contract = new EthereumContract(null, new EthereumABI(parser.parse(jsonTree.get("abi").getAsString()).getAsJsonArray()));
-				}
+
+			boolean hasCode = code != null && !code.trim().isEmpty();
+			boolean hasDeployer = ethereumAccount.getAddress() != null;
+			if (hasCode && hasDeployer) {
+				compilationResult = compile(code);
+				contract = new EthereumContract(null, new EthereumABI(new JsonParser().parse(compilationResult.get("abi").getAsString()).getAsJsonArray()));
+			}
+		}
+
+		if (contract != null) {
+			if (deployRequested && compilationResult != null) {
+				// transform Streamr params into list of constructor arguments
+				Stack<Object> args = new Stack<>();
+				List<Map> params = (List) config.get("params");
 				EthereumABI.Function constructor = contract.getABI().getConstructor();
 				BigInteger sendWei = BigInteger.ZERO;
-				Stack<Object> args = new Stack<>();
 				if (constructor != null) {
-					List<Map> params = (List) config.get("params");
 					// skip first parameter (ethAccount, not constructor parameter)
 					for (Map param : params.subList(1, params.size())) {
 						args.push(param.get("value"));
@@ -162,54 +198,17 @@ public class SolidityCompileDeploy extends ModuleWithUI implements Pullable<Ethe
 						sendWei = sendEtherParamValue.multiply(BigDecimal.TEN.pow(18)).toBigInteger();
 					}
 				}
-
-				BigInteger gasPrice = BigDecimal.valueOf(ethereumOptions.getGasPriceWei()).toBigInteger();
-				EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
-						ethereumAccount.getAddress(), DefaultBlockParameterName.LATEST).sendAsync().get();
-				BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-				Credentials credentials = Credentials.create(ethereumAccount.getPrivateKey());
-
-				//createTransaction(BigInteger nonce, BigInteger gasPrice, BigInteger gasLimit, String to, BigInteger value, String data) {
-				Function cons = encodeConstructor(args);
-				String cons_encoded = FunctionEncoder.encodeConstructor(cons.getInputParameters());
-				String createContract  = jsonTree.get("bin").getAsString()+ cons_encoded;
-				RawTransaction rawTransaction = RawTransaction.createTransaction(nonce, gasPrice, getGasLimit(), null, sendWei, createContract);
-				byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
-				String hexValue = Numeric.toHexString(signedMessage);
-
-				//async deploy not implemented atm because of UI constraints:
-				/*
-				CompletableFuture<EthSendTransaction> cf = web3j.ethSendRawTransaction(hexValue).sendAsync();
-				cf.thenAccept(new Consumer<EthSendTransaction>() {
-					@Override
-					public void accept(EthSendTransaction tx) {
-					}
-				});
-				*/
-				//synchronous deploy:
-				EthSendTransaction tx = web3j.ethSendRawTransaction(hexValue).send();
-				String txhash = tx.getTransactionHash();
-				log.debug("TX response: " + txhash);
-				String address = web3j.ethGetTransactionReceipt(txhash).send().getResult().getContractAddress();
-				contract = new EthereumContract(address,contract.getABI());
+				String bytecode = compilationResult.get("bin").getAsString();
+				try {
+					String address = deploy(bytecode, args, sendWei);
+					contract = new EthereumContract(address, contract.getABI());
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
 			}
-		} catch (Exception e) {
-			if (ExceptionUtils.getRootCause(e) instanceof java.net.ConnectException) {
-				log.error("Could not connect to web3 backend!", e);
-				throw new RuntimeException("Sorry, we couldn't contact Ethereum at this time. We'll try to fix this soon.");
-			} else {
-				throw new RuntimeException(e);
-			}
-		}
-
-		if (contract != null) {
 			createContractOutput();
 			createParameters(contract.getABI());
 		}
-	}
-	
-	protected StreamrWeb3Interface createWeb3Interface() {
-		return new StreamrWeb3Interface(ethereumOptions.getServer(), ethereumOptions.getGasPriceWei());
 	}
 
 	private void createContractOutput() {
@@ -231,6 +230,16 @@ public class SolidityCompileDeploy extends ModuleWithUI implements Pullable<Ethe
 				addInput(sendEtherParam);
 			}
 		}
+	}
+
+	@Override
+	public void sendOutput() {
+
+	}
+
+	@Override
+	public void clearState() {
+
 	}
 
 	@Override
