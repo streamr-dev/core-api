15a6ab858758f08fb176c2f0e0bf6374958bdc50
Melchior Thambipillai <melkiorthambipillai@gmail.com>
2019-05-03 13:58:06 +0200

    replace real service with a mock and update tests accordingly (#591)

--- test/unit/com/unifina/service/SubscriptionServiceSpec.groovy
+++ test/unit/com/unifina/service/SubscriptionServiceSpec.groovy
@@ -1,5 +1,6 @@
 package com.unifina.service
 
+import com.unifina.BeanMockingSpecification
 import com.unifina.api.ProductNotFreeException
 import com.unifina.domain.data.Stream
 import com.unifina.domain.marketplace.FreeSubscription
@@ -11,15 +12,15 @@ import com.unifina.domain.security.Permission
 import com.unifina.domain.security.SecUser
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
-import spock.lang.Specification
 
 @TestFor(SubscriptionService)
 @Mock([FreeSubscription, IntegrationKey, PaidSubscription, Permission, Product, Stream, Subscription])
-class SubscriptionServiceSpec extends Specification {
+class SubscriptionServiceSpec extends BeanMockingSpecification {
 
 	SecUser user, user2
 	Stream s1, s2, s3
 	Product product
+	PermissionService permissionService
 
 	void setup() {
 		user = new SecUser(username: "me@streamr.com").save(failOnError: true, validate: false)
@@ -30,7 +31,7 @@ class SubscriptionServiceSpec extends Specification {
 		[s1, s2, s3].eachWithIndex { s, i -> s.id = "stream-${i + 1}" }
 		[s1, s2, s3]*.save(failOnError: true, validate: false)
 		product = new Product(streams: [s1, s2]).save(failOnError: true, validate: false)
-		service.permissionService = new PermissionService()
+		permissionService = service.permissionService = mockBean(PermissionService, Mock(PermissionService))
 	}
 
 	void "getSubscriptionsOfUser() returns empty if user has no integration keys or free subscriptions"() {
@@ -134,83 +135,83 @@ class SubscriptionServiceSpec extends Specification {
 	}
 
 	void "onSubscribed() creates subscription-linked permissions if user found for address"() {
+		String address = "0x0000000000000000000000000000000000000000"
 		new IntegrationKey(
 			user: user,
-			idInService: "0x0000000000000000000000000000000000000000",
+			idInService: address,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
+		Permission p1 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p2 = new Permission(operation: "READ", user: user, stream: s2)
 
 		when:
-		def date = new Date()
-		service.onSubscribed(product, "0x0000000000000000000000000000000000000000", date)
+
+		service.onSubscribed(product, address, new Date())
 
 		then:
-		Permission.findAll()*.toInternalMap() as Set == [
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-1",
-				subscription: 1L,
-				endsAt: date
-			],
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-2",
-				subscription: 1L,
-				endsAt: date
-			]
-		] as Set
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p1
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p2
+		Permission.findAll()*.toInternalMap() as Set == [p1.toInternalMap(), p2.toInternalMap()] as Set
 	}
 
 	void "onSubscribed() does not remove existing non-subscription permissions if user found for address"() {
-		def permissionService = service.permissionService
+		String address = "0x0000000000000000000000000000000000000000"
 
-		Permission p1 = permissionService.systemGrant(user, s1, Permission.Operation.READ)
-		Permission p2 = permissionService.systemGrant(user, s2, Permission.Operation.READ)
+		Permission p1 = new Permission(user: user, stream: s1, operation: "READ").save(failOnError: true, validate: false)
+		Permission p2 = new Permission(user: user, stream: s2, operation: "READ").save(failOnError: true, validate: false)
 
 		assert Permission.exists(p1.id)
 		assert Permission.exists(p2.id)
 
 		new IntegrationKey(
 			user: user,
-			idInService: "0x0000000000000000000000000000000000000000",
+			idInService: address,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
 
+		Permission p3 = new Permission(operation: "READ", user: user, stream: s1, endsAt: new Date())
+		Permission p4 = new Permission(operation: "READ", user: user, stream: s2, endsAt: new Date())
+
 		when:
-		service.onSubscribed(product, "0x0000000000000000000000000000000000000000", new Date())
+		service.onSubscribed(product, address, new Date())
 
 		then:
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p3
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p4
 		Permission.exists(p1.id)
 		Permission.exists(p2.id)
 	}
 
 	void "onSubscribed() removes existing subscription-linked permissions if user found for address"() {
-		def permissionService = service.permissionService
+		String address = "0x0000000000000000000000000000000000000000"
+		Subscription s = new PaidSubscription(product: product, address: address, endsAt: new Date(0))
+			.save(failOnError: true, validate: false)
 
-		def s = service.onSubscribed(product, "0x0000000000000000000000000000000000000000", new Date(0))
-
-		Permission p1 = permissionService.systemGrant(user, s1, Permission.Operation.READ)
-		Permission p2 = permissionService.systemGrant(user, s2, Permission.Operation.READ)
+		Permission p1 = new Permission(user: user, stream: s1, operation: "READ")
+		Permission p2 = new Permission(user: user, stream: s2, operation: "READ")
 		p1.subscription = s
 		p2.subscription = s
-		p1.save(failOnError: true)
-		p2.save(failOnError: true)
+		p1.save(failOnError: true, validate: false)
+		p2.save(failOnError: true, validate: false)
 
 		assert Permission.exists(p1.id)
 		assert Permission.exists(p2.id)
 
 		new IntegrationKey(
 			user: user,
-			idInService: "0x0000000000000000000000000000000000000000",
+			idInService: address,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
 
+		Permission p3 = new Permission(operation: "READ", user: user, stream: s1, endsAt: new Date())
+		Permission p4 = new Permission(operation: "READ", user: user, stream: s2, endsAt: new Date())
+
 		when:
-		service.onSubscribed(product, "0x0000000000000000000000000000000000000000", new Date())
+		service.onSubscribed(product, address, new Date())
 
 		then:
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p3
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p4
 		!Permission.exists(p1.id)
 		!Permission.exists(p2.id)
 	}
@@ -226,11 +227,15 @@ class SubscriptionServiceSpec extends Specification {
 		product.pricePerSecond = 0
 
 		assert FreeSubscription.count() == 0
+		Permission p1 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p2 = new Permission(operation: "READ", user: user, stream: s2)
 
 		when:
 		def s = service.subscribeToFreeProduct(product, user, new Date())
 
 		then:
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p1
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p2
 		s.id != null
 		FreeSubscription.findAll() == [s]
 	}
@@ -238,16 +243,20 @@ class SubscriptionServiceSpec extends Specification {
 	void "subscribeToFreeProduct() updates existing Subscription if product-user exists"() {
 		product.pricePerSecond = 0
 
-		def s1 = service.subscribeToFreeProduct(product, user, new Date(0))
+		Subscription sub1 = new FreeSubscription(product: product, user: user, endsAt: new Date()).save(failOnError: true, validate: false)
+		Permission p1 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p2 = new Permission(operation: "READ", user: user, stream: s2)
 
 		when:
 		def newDate = new Date()
-		def s2 = service.subscribeToFreeProduct(product, user, newDate)
+		def sub2 = service.subscribeToFreeProduct(product, user, newDate)
 
 		then:
-		s2.id == s1.id
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p1
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p2
+		sub2.id == sub1.id
 		Subscription.count() == 1
-		Subscription.findById(s2.id).endsAt == newDate
+		Subscription.findById(sub2.id).endsAt == newDate
 	}
 
 	void "subscribeToFreeProduct() creates subscription-linked permissions"() {
@@ -255,47 +264,42 @@ class SubscriptionServiceSpec extends Specification {
 
 		assert Permission.count() == 0
 
+		Permission p1 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p2 = new Permission(operation: "READ", user: user, stream: s2)
+
 		when:
-		def endDate = new Date()
-		service.subscribeToFreeProduct(product, user, endDate)
+		service.subscribeToFreeProduct(product, user, new Date())
 
 		then:
-		Permission.findAll()*.toInternalMap() as Set == [
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-1",
-				subscription: 1L,
-				endsAt: endDate
-			],
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-2",
-				subscription: 1L,
-				endsAt: endDate
-			]
-		] as Set
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p1
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p2
+		Permission.findAll()*.toInternalMap() as Set == [p1.toInternalMap(), p2.toInternalMap()] as Set
 	}
 
 	void "subscribeToFreeProduct() removes existing subscription-linked permissions"() {
 		product.pricePerSecond = 0
 
-		def s = service.subscribeToFreeProduct(product, user2, new Date(0))
-		Permission p1 = service.permissionService.systemGrant(user, s1, Permission.Operation.READ)
-		Permission p2 = service.permissionService.systemGrant(user, s2, Permission.Operation.READ)
+		Subscription s = new FreeSubscription(product: product, user: user2, endsAt: new Date(0))
+			.save(failOnError: true, validate: false)
+		Permission p1 = new Permission(user: user, stream: s1, operation: Permission.Operation.READ)
+		Permission p2 = new Permission(user: user, stream: s2, operation: Permission.Operation.READ)
 		p1.subscription = s
 		p2.subscription = s
-		p1.save(failOnError: true)
-		p2.save(failOnError: true)
+		p1.save(failOnError: true, validate: false)
+		p2.save(failOnError: true, validate: false)
 
 		assert Permission.exists(p1.id)
 		assert Permission.exists(p2.id)
+
+		Permission p3 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p4 = new Permission(operation: "READ", user: user, stream: s2)
 
 		when:
 		service.subscribeToFreeProduct(product, user2, new Date())
 
 		then:
+		1 * permissionService.systemGrant(user2, s1, Permission.Operation.READ) >> p3
+		1 * permissionService.systemGrant(user2, s2, Permission.Operation.READ) >> p4
 		!Permission.exists(p1.id)
 		!Permission.exists(p2.id)
 	}
@@ -309,7 +313,8 @@ class SubscriptionServiceSpec extends Specification {
 	}
 
 	void "beforeIntegrationKeyRemoved() removes all subscription-linked permissions for given integration key"() {
-		def permissionService = service.permissionService = new PermissionService()
+		String address1 = "0x0000000000000000000000000000000000000000"
+		String address2 = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
 
 		def integrationKey = new IntegrationKey(
 			user: user,
@@ -326,11 +331,21 @@ class SubscriptionServiceSpec extends Specification {
 		def product2 = new Product(streams: [s3]).save(failOnError: true, validate: false)
 
 		setup: "create permissions"
-		service.onSubscribed(product, "0x0000000000000000000000000000000000000000", new Date())
-		service.onSubscribed(product2, "0x0000000000000000000000000000000000000000", new Date())
-		service.onSubscribed(product, "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", new Date())
-		service.onSubscribed(product2, "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", new Date())
-		permissionService.systemGrant(user, s1, Permission.Operation.READ)
+		Subscription sub1 = new PaidSubscription(product: product, address: address1).save(failOnError: true, validate: false)
+		new Permission(stream: s1, subscription: sub1, endsAt: new Date()).save(failOnError: true, validate: false)
+		new Permission(stream: s2, subscription: sub1, endsAt: new Date()).save(failOnError: true, validate: false)
+
+		Subscription sub2 = new PaidSubscription(product: product2, address: address1).save(failOnError: true, validate: false)
+		new Permission(stream: s3, subscription: sub2, endsAt: new Date()).save(failOnError: true, validate: false)
+
+		Subscription sub3 = new PaidSubscription(product: product, address: address2).save(failOnError: true, validate: false)
+		new Permission(stream: s1, subscription: sub3, endsAt: new Date()).save(failOnError: true, validate: false)
+		new Permission(stream: s2, subscription: sub3, endsAt: new Date()).save(failOnError: true, validate: false)
+
+		Subscription sub4 = new PaidSubscription(product: product2, address: address2).save(failOnError: true, validate: false)
+		new Permission(stream: s3, subscription: sub4, endsAt: new Date()).save(failOnError: true, validate: false)
+
+		new Permission(user: user, stream: s1, operation: "READ").save(failOnError: true, validate: false)
 		assert Permission.count() == 2 + 1 + 2 + 1 + 1
 
 		when:
@@ -348,84 +363,72 @@ class SubscriptionServiceSpec extends Specification {
 	}
 
 	void "afterIntegrationKeyCreated() creates subscription-linked permissions for given integration key"() {
-		service.permissionService = new PermissionService()
-
 		def date = new Date()
 		def product2 = new Product(streams: [s3]).save(failOnError: true, validate: false)
-		def sub1 = new PaidSubscription(
-			address: "0x0000000000000000000000000000000000000000",
+		String address = "0x0000000000000000000000000000000000000000"
+		new PaidSubscription(
+			address: address,
 			product: product,
 			endsAt: date
 		).save(failOnError: true, validate: true)
-		def sub2 = new PaidSubscription(
-			address: "0x0000000000000000000000000000000000000000",
+		new PaidSubscription(
+			address: address,
 			product: product2,
 			endsAt: date
 		).save(failOnError: true, validate: true)
 
 		def integrationKey = new IntegrationKey(
 			user: user,
-			idInService: "0x0000000000000000000000000000000000000000",
+			idInService: address,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
+
+		Permission p1 = new Permission(operation: "READ", user: user, stream: s1)
+		Permission p2 = new Permission(operation: "READ", user: user, stream: s2)
+		Permission p3 = new Permission(operation: "READ", user: user, stream: s3)
 
 		when:
 		service.afterIntegrationKeyCreated(integrationKey)
 
 		then:
-		Permission.findAll()*.toInternalMap() as Set == [
-		    [
-		        operation: "READ",
-				user: 1L,
-				stream: "stream-1",
-				subscription: 1L,
-				endsAt: date
-		    ],
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-2",
-				subscription: 1L,
-				endsAt: date
-			],
-			[
-				operation: "READ",
-				user: 1L,
-				stream: "stream-3",
-				subscription: 2L,
-				endsAt: date
-			]
-		] as Set
+		1 * permissionService.systemGrant(user, s1, Permission.Operation.READ) >> p1
+		1 * permissionService.systemGrant(user, s2, Permission.Operation.READ) >> p2
+		1 * permissionService.systemGrant(user, s3, Permission.Operation.READ) >> p3
+		Permission.findAll()*.toInternalMap() as Set == [p1.toInternalMap(), p2.toInternalMap(), p3.toInternalMap()] as Set
 	}
 
 	void "afterIntegrationKeyCreated() updates subscription-linked permissions on a per-user basis"() {
-		service.permissionService = new PermissionService()
+		String address1 = "0x0000000000000000000000000000000000000000"
+		String address2 = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
 
 		new IntegrationKey(
 			user: user,
-			idInService: "0x0000000000000000000000000000000000000000",
+			idInService: address1,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
 
 		new IntegrationKey(
 			user: user2,
-			idInService: "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+			idInService: address2,
 			service: IntegrationKey.Service.ETHEREUM_ID
 		).save(failOnError: true, validate: false)
 
+		Subscription sub1, sub2
+		Permission p1, p2, p3, p4
+
 		setup: "create permissions"
 		def date = new Date()
-		service.onSubscribed(product, "0x0000000000000000000000000000000000000000", date) // 1, 2
+		sub1 = new PaidSubscription(product: product, address: address1).save(failOnError: true, validate: false)
+		p1 = new Permission(user: user, stream: s1, operation: "READ", subscription: sub1, endsAt: date).save(failOnError: true, validate: false)
+		p2 = new Permission(user: user, stream: s2, operation: "READ", subscription: sub1, endsAt: date).save(failOnError: true, validate: false)
 
 		product.streams = [s1]
 		product.save(failOnError: true, validate: false)
-		service.onSubscribed(product, "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", date) // 3
 
-		assert Permission.findAll()*.toInternalMap() as Set == [
-			[operation: "READ", subscription: 1L, user: 1L, stream: "stream-1", endsAt: date],
-			[operation: "READ", subscription: 1L, user: 1L, stream: "stream-2", endsAt: date],
-			[operation: "READ", subscription: 2L, user: 2L, stream: "stream-1", endsAt: date],
-		] as Set
+		sub2 = new PaidSubscription(product: product, address: address2).save(failOnError: true, validate: false)
+		p3 = new Permission(user: user2, stream: s3, operation: "READ", subscription: sub2, endsAt: date).save(failOnError: true, validate: false)
+
+		assert Permission.findAll()*.toInternalMap() as Set == [p1.toInternalMap(), p2.toInternalMap(), p3.toInternalMap()] as Set
 
 		and: "change product"
 		product.streams = [s2, s3]
@@ -434,11 +437,13 @@ class SubscriptionServiceSpec extends Specification {
 		when:
 		service.afterProductUpdated(product)
 		then:
-		Permission.findAll()*.toInternalMap() as Set == [
-			[operation: "READ", subscription: 1L, user: 1L, stream: "stream-2", endsAt: date],
-			[operation: "READ", subscription: 1L, user: 1L, stream: "stream-3", endsAt: date],
-			[operation: "READ", subscription: 2L, user: 2L, stream: "stream-2", endsAt: date],
-			[operation: "READ", subscription: 2L, user: 2L, stream: "stream-3", endsAt: date],
-		] as Set
+		interaction {
+			p1 = new Permission(operation: "READ", user: user, stream: s3)
+			p4 = new Permission(operation: "READ", user: user2, stream: s2)
+			1 * permissionService.systemGrant(user, s3, Permission.Operation.READ) >> p1
+			1 * permissionService.systemGrant(user2, s2, Permission.Operation.READ) >> p4
+		}
+		Permission.findAll()*.toInternalMap() as Set == [p1.toInternalMap(), p2.toInternalMap(),
+														 p3.toInternalMap(), p4.toInternalMap()] as Set
 	}
 }
