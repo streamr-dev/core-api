e884527bc073586a48bb9b41e9b84fe380d89b87
mthambipillai <melchior.thambipillai@streamr.com>
2019-05-31 18:06:04 +0200

    update migration

--- grails-app/migrations/core/2019-04-01-add-inbox-streams.groovy
+++ grails-app/migrations/core/2019-04-01-add-inbox-streams.groovy
@@ -14,21 +14,23 @@ databaseChangeLog = {
 	changeSet(author: "mthambipillai", id: "create-inbox-stream-for-ethereum-users") {
 		grailsChange {
 			change {
-				sql.eachRow('SELECT DISTINCT user_id, id_in_service FROM integration_key WHERE service = \"ETHEREUM_ID\" OR service = \"ETHEREUM\"') { row ->
-					String userId = row['user_id']
+				sql.eachRow('SELECT DISTINCT id_in_service FROM integration_key WHERE service = \"ETHEREUM_ID\" OR service = \"ETHEREUM\"') { row ->
 					String ethereumAddress = row['id_in_service']
 					Date d = new Date()
 					sql.execute('INSERT INTO stream (version, feed_id, id, name, date_created, last_updated, partitions, require_signed_data, auto_configure, storage_days, inbox)' +
 						' VALUES (0, 7, ?, ?, ?, ?, 1, b\'0\', b\'0\', ?, b\'1\')', ethereumAddress, ethereumAddress, d, d, Stream.DEFAULT_STORAGE_DAYS)
-					sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "read", ?, b\'0\', ?)',
-						ethereumAddress, userId
-					)
-					sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "write", ?, b\'0\', ?)',
-						ethereumAddress, userId
-					)
-					sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "share", ?, b\'0\', ?)',
-						ethereumAddress, userId
-					)
+					sql.eachRow('SELECT user_id FROM integration_key WHERE id_in_service = ?', ethereumAddress) { userRow ->
+						String userId = userRow['user_id']
+						sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "read", ?, b\'0\', ?)',
+							ethereumAddress, userId
+						)
+						sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "write", ?, b\'0\', ?)',
+							ethereumAddress, userId
+						)
+						sql.execute('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (0, "share", ?, b\'0\', ?)',
+							ethereumAddress, userId
+						)
+					}
 				}
 			}
 		}
