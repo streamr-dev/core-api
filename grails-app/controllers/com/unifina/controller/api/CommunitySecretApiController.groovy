47327b84715bd1ca197f41afe6db92b38388a417
Kare Nuorteva <kare.nuorteva@streamr.com>
2019-06-05 12:57:23 +0300

    Add admin access control checks

--- grails-app/controllers/com/unifina/controller/api/CommunitySecretApiController.groovy
+++ grails-app/controllers/com/unifina/controller/api/CommunitySecretApiController.groovy
@@ -1,11 +1,14 @@
 package com.unifina.controller.api
 
+import com.unifina.api.ApiException
 import com.unifina.api.BadRequestException
 import com.unifina.api.CommunitySecretCommand
 import com.unifina.api.NotFoundException
 import com.unifina.domain.community.CommunitySecret
+import com.unifina.domain.security.SecUser
 import com.unifina.security.StreamrApi
 import com.unifina.service.CommunitySecretService
+import com.unifina.service.CommunityService
 import com.unifina.utils.EthereumAddressValidator
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
@@ -13,6 +16,7 @@ import grails.plugin.springsecurity.annotation.Secured
 @Secured(["IS_AUTHENTICATED_ANONYMOUSLY"])
 class CommunitySecretApiController {
 	CommunitySecretService communitySecretService
+	CommunityService communityService
 
 	static boolean isCommunityAddress(String value) {
 		return EthereumAddressValidator.validate(value)
@@ -27,11 +31,18 @@ class CommunitySecretApiController {
 		return value ==~ /^[a-zA-Z0-9-_]{44}$/
 	}
 
+	void checkAdminAccessControl(SecUser user, String communityAddress) {
+		if (!communityService.checkAdminAccessControl(user, communityAddress)) {
+			throw new ApiException(403, "ACCESS_DENIED", "required admin role is missing")
+		}
+	}
+
 	@StreamrApi
 	def findAll(String communityAddress) {
 		if (!isCommunityAddress(communityAddress)) {
 			throw new BadRequestException("community address is not an ethereum address")
 		}
+		checkAdminAccessControl(loggedInUser(), communityAddress)
 		List<CommunitySecret> secrets = communitySecretService.findAll(communityAddress)
 		render(secrets*.toMap() as JSON)
 	}
@@ -44,6 +55,7 @@ class CommunitySecretApiController {
 		if (cmd.errors.getFieldError("name")) {
 			throw new BadRequestException("name in json is not a valid name")
 		}
+		checkAdminAccessControl(loggedInUser(), communityAddress)
 		CommunitySecret secret = communitySecretService.create(communityAddress, cmd)
 		render(secret.toMap() as JSON)
 	}
@@ -56,6 +68,7 @@ class CommunitySecretApiController {
 		if (!isValidID(communitySecretId)) {
 			throw new BadRequestException("community secret id is not valid")
 		}
+		checkAdminAccessControl(loggedInUser(), communityAddress)
 		CommunitySecret secret = communitySecretService.find(communityAddress, communitySecretId)
 		if (secret == null) {
 			throw new NotFoundException("community secret not found by id")
@@ -74,6 +87,7 @@ class CommunitySecretApiController {
 		if (cmd.errors.getFieldError("name")) {
 			throw new BadRequestException("name in json is not a valid name")
 		}
+		checkAdminAccessControl(loggedInUser(), communityAddress)
 		CommunitySecret secret = communitySecretService.update(communityAddress, communitySecretId, cmd)
 		if (secret == null) {
 			throw new NotFoundException("community secret not found by id")
@@ -89,7 +103,12 @@ class CommunitySecretApiController {
 		if (!isValidID(communitySecretId)) {
 			throw new BadRequestException("community secret id is not valid")
 		}
+		checkAdminAccessControl(loggedInUser(), communityAddress)
 		communitySecretService.delete(communityAddress, communitySecretId)
 		render(status: 204)
+	}
+
+	private SecUser loggedInUser() {
+		return (SecUser) request.apiUser
 	}
 }
--- test/unit/com/unifina/controller/api/CommunitySecretApiControllerSpec.groovy
+++ test/unit/com/unifina/controller/api/CommunitySecretApiControllerSpec.groovy
@@ -1,22 +1,28 @@
 package com.unifina.controller.api
 
+import com.unifina.api.ApiException
 import com.unifina.api.BadRequestException
 import com.unifina.api.CommunitySecretCommand
 import com.unifina.api.NotFoundException
 import com.unifina.domain.community.CommunitySecret
+import com.unifina.domain.security.SecUser
 import com.unifina.filters.UnifinaCoreAPIFilters
 import com.unifina.service.CommunitySecretService
+import com.unifina.service.CommunityService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(CommunitySecretApiController)
-@Mock([UnifinaCoreAPIFilters, CommunitySecret])
+@Mock([UnifinaCoreAPIFilters, CommunitySecret, SecUser])
 class CommunitySecretApiControllerSpec extends Specification {
+	SecUser me
 	final String communityAddress = "0x6c90aece04198da2d5ca9b956b8f95af8041de37"
 	final String validID = "L-TvrBkyQTS_JK1ABHFEZAaZ3FHq7-TPqMXe9JNz1x6g"
 
 	void setup() {
+		me = new SecUser(id: 1, name: "firstname lastname", username: "firstname.lastname@address.com", password: "salasana")
+		me.save(validate: true, failOnError: true)
 		CommunitySecret secret = new CommunitySecret(
 			name: "name of the community secret",
 			secret: "secret",
@@ -26,6 +32,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 		secret.save(validate: true, failOnError: true)
 
 		controller.communitySecretService = Mock(CommunitySecretService)
+		controller.communityService = Mock(CommunityService)
 	}
 
 	void "findAll() test"() {
@@ -45,12 +52,14 @@ class CommunitySecretApiControllerSpec extends Specification {
 		s2.id = "2"
 		s2.save(validate: true, failOnError: true)
 		when:
+		request.apiUser = me
 		request.method = "GET"
 		params.communityAddress = communityAddress
 		withFilters(action: "findAll") {
 			controller.findAll()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.findAll(communityAddress) >> [s1, s2]
 		response.json[0].id == "1"
 		response.json[0].name == "secret 1"
@@ -74,6 +83,21 @@ class CommunitySecretApiControllerSpec extends Specification {
 		e.code == "PARAMETER_MISSING"
 	}
 
+	void "findAll() checks admin access control"() {
+		when:
+		request.apiUser = me
+		request.method = "GET"
+		params.communityAddress = communityAddress
+		withFilters(action: "findAll") {
+			controller.findAll()
+		}
+		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> false
+		def e = thrown(ApiException)
+		e.statusCode == 403
+		e.code == "ACCESS_DENIED"
+	}
+
 	void "create() test"() {
 		CommunitySecret secret = new CommunitySecret(
 			name: "secret name",
@@ -83,6 +107,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 		secret.id = "1"
 		secret.save(validate: true, failOnError: true)
 		when:
+		request.apiUser = me
 		request.method = "POST"
 		request.json = [
 			name: "secret name",
@@ -93,6 +118,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.create()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.create(communityAddress, _ as CommunitySecretCommand) >> secret
 		response.json.id == "1"
 		response.json.name == "secret name"
@@ -134,6 +160,24 @@ class CommunitySecretApiControllerSpec extends Specification {
 		e.code == "PARAMETER_MISSING"
 	}
 
+	void "create() checks admin access control"() {
+		when:
+		request.apiUser = me
+		request.method = "POST"
+		request.json = [
+			name: "community name",
+		]
+		params.communityAddress = communityAddress
+		withFilters(action: "create") {
+			controller.create()
+		}
+		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> false
+		def e = thrown(ApiException)
+		e.statusCode == 403
+		e.code == "ACCESS_DENIED"
+	}
+
 	void "find() test"() {
 		setup:
 		CommunitySecret s1 = new CommunitySecret(
@@ -144,6 +188,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 		s1.id = "1"
 		s1.save(validate: true, failOnError: true)
 		when:
+		request.apiUser = me
 		request.method = "GET"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID // ID not found in DB
@@ -151,6 +196,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.find()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.find(communityAddress, validID) >> s1
 		response.json.id == "1"
 		response.json.name == "secret 1"
@@ -189,6 +235,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 
 	void "find() not found when community secret not found by id"() {
 		when:
+		request.apiUser = me
 		request.method = "GET"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID // ID not found in DB
@@ -196,10 +243,27 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.find()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.find(communityAddress, validID) >> null
 		def e = thrown(NotFoundException)
 		e.statusCode == 404
 		e.code == "NOT_FOUND"
+	}
+
+	void "find() checks admin access control"() {
+		when:
+		request.apiUser = me
+		request.method = "GET"
+		params.communityAddress = communityAddress
+		params.communitySecretId = validID
+		withFilters(action: "find") {
+			controller.find()
+		}
+		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> false
+		def e = thrown(ApiException)
+		e.statusCode == 403
+		e.code == "ACCESS_DENIED"
 	}
 
 	void "update() test"() {
@@ -212,6 +276,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 		s1.id = "1"
 		s1.save(validate: true, failOnError: true)
 		when:
+		request.apiUser = me
 		request.method = "PUT"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID // ID not found in DB
@@ -222,6 +287,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.update()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.update(communityAddress, validID, _ as CommunitySecretCommand) >> {
 			s1.name = "new name"
 			return s1
@@ -287,6 +353,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 
 	void "update() not found on when community secret not found by id"() {
 		when:
+		request.apiUser = me
 		request.method = "PUT"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID // ID not found in DB
@@ -297,14 +364,35 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.update()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.update(communityAddress, validID, _ as CommunitySecretCommand) >> null
 		def e = thrown(NotFoundException)
 		e.statusCode == 404
 		e.code == "NOT_FOUND"
 	}
 
+	void "update() checks admin access control"() {
+		when:
+		request.apiUser = me
+		request.method = "PUT"
+		params.communityAddress = communityAddress
+		params.communitySecretId = validID
+		request.json = [
+			name: "name",
+		]
+		withFilters(action: "update") {
+			controller.update()
+		}
+		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> false
+		def e = thrown(ApiException)
+		e.statusCode == 403
+		e.code == "ACCESS_DENIED"
+	}
+
 	void "delete() test"() {
 		when:
+		request.apiUser = me
 		request.method = "DELETE"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID
@@ -312,6 +400,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.delete()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.delete(communityAddress, validID)
 		response.status == 204
 	}
@@ -348,6 +437,7 @@ class CommunitySecretApiControllerSpec extends Specification {
 
 	void "delete() not found on non-existing community secret id"() {
 		when:
+		request.apiUser = me
 		request.method = "DELETE"
 		params.communityAddress = communityAddress
 		params.communitySecretId = validID
@@ -355,11 +445,28 @@ class CommunitySecretApiControllerSpec extends Specification {
 			controller.delete()
 		}
 		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> true
 		1 * controller.communitySecretService.delete(communityAddress, validID) >> {
 			throw new NotFoundException("mocked: not found!")
 		}
 		def e = thrown(NotFoundException)
 		e.statusCode == 404
 		e.code == "NOT_FOUND"
+	}
+
+	void "delete() checks admin access control"() {
+		when:
+		request.apiUser = me
+		request.method = "DELETE"
+		params.communityAddress = communityAddress
+		params.communitySecretId = validID
+		withFilters(action: "delete") {
+			controller.delete()
+		}
+		then:
+		1 * controller.communityService.checkAdminAccessControl(me, communityAddress) >> false
+		def e = thrown(ApiException)
+		e.statusCode == 403
+		e.code == "ACCESS_DENIED"
 	}
 }
