89a550ec535674fd8a9678ec0c46c0598d49ce09
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-07-08 18:23:00 +0200

    StreamMessageComparator implements Serializable

--- src/java/com/unifina/feed/util/StreamMessageComparator.java
+++ src/java/com/unifina/feed/util/StreamMessageComparator.java
@@ -2,9 +2,10 @@ package com.unifina.feed.util;
 
 import com.streamr.client.protocol.message_layer.StreamMessage;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class StreamMessageComparator implements Comparator<StreamMessage> {
+public class StreamMessageComparator implements Comparator<StreamMessage>, Serializable {
 	@Override
 	public int compare(StreamMessage o1, StreamMessage o2) {
 		int ts = Long.compare(o1.getTimestamp(), o2.getTimestamp());
