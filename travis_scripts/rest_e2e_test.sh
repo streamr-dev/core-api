0dd48d5f1ef11c72df6474fdffbe8695d26a43bb
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-09-02 17:53:34 +0200

    try to get data-api logs to show

--- travis_scripts/rest_e2e_test.sh
+++ travis_scripts/rest_e2e_test.sh
@@ -6,9 +6,6 @@ git clone https://github.com/streamr-dev/streamr-docker-dev.git
 # Start everything except engine-and-editor
 $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start 1
 
-# Uncomment to show data-api logs // TODO: comment out
-$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh log -f data-api &
-
 echo "Sleeping for 30 seconds..." && sleep 30
 
 # Start engine-and-editor in the background. To see the logs, remove the "> ee-logs.txt" redirect. TODO: add back the redirect
@@ -16,6 +13,9 @@ nohup grails test run-app --non-interactive &
 
 # Wait for EE to come up
 while true; do http_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/streamr-core/api/v1/users/me); if [ $http_code = 401 ]; then echo "EE up and running"; break; else echo "EE not receiving connections"; sleep 5s; fi; done
+
+# TODO: remove, show data-api logs
+$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api && $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh log -f data-api &
 
 # Wait for data-api to come up
 while true; do http_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8890/); if [ $http_code = 404 ]; then echo "Data-api up and running"; break; else echo "Data API not receiving connections"; sleep 5s; fi; done
