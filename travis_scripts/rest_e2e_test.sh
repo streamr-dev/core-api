d49e81369f033a12f180398d416a691be6233e1e
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-09-02 18:27:26 +0200

    try to get data-api logs to show

--- travis_scripts/rest_e2e_test.sh
+++ travis_scripts/rest_e2e_test.sh
@@ -14,8 +14,9 @@ nohup grails test run-app --non-interactive &
 # Wait for EE to come up
 while true; do http_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/streamr-core/api/v1/users/me); if [ $http_code = 401 ]; then echo "EE up and running"; break; else echo "EE not receiving connections"; sleep 5s; fi; done
 
-# TODO: remove, show data-api logs
-$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api && $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh log -f data-api &
+# TODO: remove, show data-api logs for debugging
+$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api
+$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh log -f data-api &
 
 # Wait for data-api to come up
 while true; do http_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8890/); if [ $http_code = 404 ]; then echo "Data-api up and running"; break; else echo "Data API not receiving connections"; sleep 5s; fi; done
