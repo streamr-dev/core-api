081e98d7edd02a220da641a32482f2df7a4cdb6a
Henri Pihkala <hpihkala@users.noreply.github.com>
2019-09-10 11:51:08 +0200

    fix migration to work with both dev and prod db

--- grails-app/migrations/core/2019-04-26-drop-feed.groovy
+++ grails-app/migrations/core/2019-04-26-drop-feed.groovy
@@ -1,13 +1,37 @@
 package core
 databaseChangeLog = {
-	changeSet(author: "hpihkala", id: "drop-feed-1") {
-		dropForeignKeyConstraint(baseTableName: "feed", constraintName: "FK2FE59EB6140F06")
+	/**
+	 * Annoyingly, some of the constraint names are different in prod database vs. dev/test.
+	 * Below, we try both possible constraint names.
+	 */
+	changeSet(author: "hpihkala", id: "drop-feed-to-module-id-constraint") {
+		grailsChange {
+			change {
+				try {
+					// dev & test
+					sql.execute("ALTER TABLE `feed` DROP FOREIGN KEY `FK2FE59EB6140F06`")
+				} catch (Exception e) {
+					// prod
+					sql.execute("ALTER TABLE `feed` DROP FOREIGN KEY `FK2FE59E26BD3526`")
+				}
+			}
+		}
+	}
+	changeSet(author: "hpihkala", id: "drop-stream-to-feed-id-constraint") {
+		grailsChange {
+			change {
+				try {
+					// dev & test
+					sql.execute("ALTER TABLE `stream` DROP FOREIGN KEY `FKCAD54F8072507A49`")
+				} catch (Exception e) {
+					// prod
+					sql.execute("ALTER TABLE `stream` DROP FOREIGN KEY `FK4DA325A8F511C069`")
+				}
+			}
+		}
 	}
 	changeSet(author: "hpihkala", id: "drop-feed-2") {
 		dropForeignKeyConstraint(baseTableName: "permission", constraintName: "FKE125C5CF72507A49")
-	}
-	changeSet(author: "hpihkala", id: "drop-feed-3") {
-		dropForeignKeyConstraint(baseTableName: "stream", constraintName: "FKCAD54F8072507A49")
 	}
 	changeSet(author: "hpihkala", id: "drop-feed-4") {
 		dropColumn(columnName: "feed_id", tableName: "permission")
