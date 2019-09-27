73aeb78997d7449f58a112d7efaa0ca7e43c1cfb
mthambipillai <melchior.thambipillai@streamr.com>
2019-09-27 14:20:48 +0200

    reuse SecureRandom

--- src/java/com/unifina/utils/AlphanumericStringGenerator.java
+++ src/java/com/unifina/utils/AlphanumericStringGenerator.java
@@ -5,7 +5,8 @@ import org.apache.commons.lang.RandomStringUtils;
 import java.security.SecureRandom;
 
 public class AlphanumericStringGenerator {
+	private static final SecureRandom secureRandom = new SecureRandom();
 	public static String getRandomAlphanumericString(int length) {
-		return RandomStringUtils.random(length, 0, 0, true, true, null, new SecureRandom());
+		return RandomStringUtils.random(length, 0, 0, true, true, null, secureRandom);
 	}
 }
