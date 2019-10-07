fbbf4e1224c466eb856e1e0c91a584d84451ce9d
Isentropy <jwolff@isentropy.com>
2019-10-07 16:51:25 +0200

    CORE-1794-compile-without-account (#675)
    * error with msg thrown when deployed without acct
    
    * tests serialization with deploy=false
    
    * remove "deploy" config key
    
    * unit test for null acct selected

--- test/unit/com/unifina/signalpath/blockchain/SolidityCompileDeploySpec.groovy
+++ test/unit/com/unifina/signalpath/blockchain/SolidityCompileDeploySpec.groovy
@@ -206,6 +206,7 @@ class SolidityCompileDeploySpec extends ModuleTestingSpecification {
 		when:
 		applyConfig.contract.abi[2].payable = true
 		applyConfig.code = payable_constructor
+		applyConfig.remove("deploy")
 		module = new ModifiedSolidityCompileDeploy(null)
 		module.onConfiguration(applyConfig)
 		byte[] bytes = serializationService.serialize(module)
@@ -213,6 +214,30 @@ class SolidityCompileDeploySpec extends ModuleTestingSpecification {
 
 		then:
 		module.pullValue(module.getOutput("contract")) != null
+	}
+
+	void "compile works with null ethAccount"() {
+		when:
+		applyConfig.params[0].value = null
+		// set ethereum account
+		module.inputs[0].setConfiguration(applyConfig.params[0])
+		module.onConfiguration(applyConfig)
+
+		then:
+		module.pullValue(module.getOutput("contract")) != null
+	}
+
+	void "deploy doesn't work with null ethAccount"() {
+		when:
+		applyConfig.params[0].value = null
+		applyConfig << [deploy: true]
+		// set ethereum account
+		module.inputs[0].setConfiguration(applyConfig.params[0])
+		module.onConfiguration(applyConfig)
+
+		then:
+		def e = thrown(RuntimeException)
+		e.message.contains("no private key is selected")
 	}
 
 	static Map applyConfig = new Gson().fromJson('''
