0e4adc5a966e2a0ffd3d15c4d481b75013f0f2fa
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-11-01 14:16:10 +0200

    Merge conflicting file from origin/master

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,30 +7,14 @@ import com.unifina.domain.signalpath.Canvas
 
 class UrlMappings {
 	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
 
-		"/"(controller:"canvas")
-
-		// 403 would be handled by Spring Security Core by default, but due to
-		// https://jira.grails.org/browse/GPSPRINGSECURITYCORE-253 it needs to be specified explicitly
-		"403"(controller: "login", action: "denied")
 		"500"(controller: "error", action: "index", exception: Exception)
 
-		"/login/auth"(controller: "auth", action: "index")
-		"/login/full"(controller: "auth", action: "fullAuth")
-		"/register/$action**?"(controller: "auth", action: "index")
-
-		"/webcomponents/$view"(controller: "webcomponents", action: "index")
-
 		// API v1 url mappings
-		"/api/v1/signups"(controller: "authApi", action: "signup")
-		"/api/v1/users"(controller: "authApi", action: "register")
-		"/api/v1/passwords/tokens"(controller: "authApi", action: "forgotPassword")
-		"/api/v1/passwords"(controller: "authApi", action: "resetPassword")
+		"/api/v1/signups"(method: "POST", controller: "authApi", action: "signup")
+		"/api/v1/users"(method: "POST", controller: "authApi", action: "register")
+		"/api/v1/passwords/tokens"(method: "POST", controller: "authApi", action: "forgotPassword")
+		"/api/v1/passwords"(method: "POST", controller: "authApi", action: "resetPassword")
 
 		"/api/v1/canvases"(resources: "canvasApi", excludes: ["create", "edit"])
 		"/api/v1/canvases/$id/start"(controller: "canvasApi", action: "start")
@@ -44,12 +28,12 @@ class UrlMappings {
 		"/api/v1/streams"(resources: "streamApi", excludes: ["create", "edit"])
 		"/api/v1/streams/$resourceId/permissions"(resources: "permissionApi", excludes: ["create", "edit", "update"]) { resourceClass = Stream }
 		"/api/v1/streams/$resourceId/permissions/me"(controller: "permissionApi", action: "getOwnPermissions") { resourceClass = Stream }
-		"/api/v1/streams/$id/fields"(controller: "streamApi", action: "setFields")
+		"/api/v1/streams/$id/fields"(method: "POST", controller: "streamApi", action: "setFields")
 		"/api/v1/streams/$id/detectFields"(method: "POST", controller: "streamApi", action: "detectFields")
 		"/api/v1/streams/$id/detectFields"(method: "GET", controller: "streamApi", action: "detectFields")
 		"/api/v1/streams/$id/range"(controller: "streamApi", action: "range")
-		"/api/v1/streams/$id/uploadCsvFile"(controller: "streamApi", action: "uploadCsvFile")
-		"/api/v1/streams/$id/confirmCsvFileUpload"(controller: "streamApi", action: "confirmCsvFileUpload")
+		"/api/v1/streams/$id/uploadCsvFile"(method: "POST", controller: "streamApi", action: "uploadCsvFile")
+		"/api/v1/streams/$id/confirmCsvFileUpload"(method: "POST", controller: "streamApi", action: "confirmCsvFileUpload")
 		"/api/v1/streams/$id/dataFiles"(controller: "streamApi", action: "dataFiles")
 		"/api/v1/streams/$id/publishers"(controller: "streamApi", action: "publishers")
 		"/api/v1/streams/$id/publisher/$address"(controller: "streamApi", action: "publisher")
@@ -80,10 +64,11 @@ class UrlMappings {
 
 		"/api/v1/users/me/keys"(resources: "keyApi", excludes: ["create", "edit", "update"]) { resourceClass = SecUser }
 		"/api/v1/users/me/keys/$keyId"(method: "PUT", controller: "keyApi", action: "updateUserKey")
-		"/api/v1/users/me/products"(controller: "productApi", action: "index") { operation = Permission.Operation.SHARE }
-		"/api/v1/users/me/changePassword"(controller: "userApi", action: "changePassword")
-		"/api/v1/users/me/image"(controller: "userApi", action: "uploadAvatarImage")
+		"/api/v1/users/me/products"(method: "GET", controller: "productApi", action: "index") { operation = Permission.Operation.SHARE }
+		"/api/v1/users/me/changePassword"(method: "POST", controller: "userApi", action: "changePassword")
+		"/api/v1/users/me/image"(method: "POST", controller: "userApi", action: "uploadAvatarImage")
 		"/api/v1/users/me/balance"(method: "GET", controller: "userApi", action: "getCurrentUserBalance")
+
 		"/api/v1/integration_keys"(resources: "integrationKeyApi")
 
 		"/api/v1/canvases/($path**)/request"(controller: "canvasApi", action: "runtimeRequest") // for internal use, runtime requests to canvases
@@ -91,42 +76,39 @@ class UrlMappings {
 
 		"/api/v1/oembed"(controller: "oembedApi", action: "index")
 
-		"/api/v1/login/challenge/$address"(controller: "loginApi", action: "challenge")
-		"/api/v1/login/$action"(controller: "loginApi")
-		"/api/v1/logout"(controller: "logoutApi", action: "logout")
+		"/api/v1/login/challenge/$address"(method: "POST", controller: "loginApi", action: "challenge")
+		"/api/v1/login/$action"(method: "POST", controller: "loginApi")
+		"/api/v1/logout"(method: "POST",  controller: "logoutApi", action: "logout")
 
 		"/api/v1/categories"(resources: "categoryApi")
 
 		"/api/v1/products"(resources: "productApi")
 		"/api/v1/products/$productId/streams"(resources: "productStreamsApi")
 		"/api/v1/products/$id/$action"(controller: "productApi")
-		"/api/v1/products/$id/images"(controller: "productApi", action: "uploadImage")
+		"/api/v1/products/$id/images"(method: "POST", controller: "productApi", action: "uploadImage")
 		"/api/v1/products/$resourceId/permissions/me"(controller: "permissionApi", action: "getOwnPermissions") { resourceClass = Product }
-		"/api/v1/products/remove/$username"(controller: "removeUsersProducts", action: "index", method: "DELETE")
-		"/api/v1/products/$id/related"(controller: "productApi", action: "related", method: "GET")
-		"/api/v1/products/stale"(controller: "productApi", action: "staleProducts", method: "GET")
+		"/api/v1/products/remove/$username"(method: "DELETE", controller: "removeUsersProducts", action: "index")
+		"/api/v1/products/$id/related"(method: "GET", controller: "productApi", action: "related")
+		"/api/v1/products/stale"(method: "GET", controller: "productApi", action: "staleProducts")
 
 		"/api/v1/subscriptions"(resources: "subscriptionApi")
 
-		"/api/v1/nodes"(controller: "nodeApi", action: "index")
-		"/api/v1/nodes/ip"(controller: "nodeApi", action: "ip")
-		"/api/v1/nodes/config"(controller: "nodeApi", action: "config")
-		"/api/v1/nodes/shutdown"(controller: "nodeApi", action: "shutdown")
-		"/api/v1/nodes/canvases"(controller: "nodeApi", action: "canvases")
-		"/api/v1/nodes/canvases/sizes"(controller: "nodeApi", action: "canvasSizes")
-		"/api/v1/nodes/$nodeIp/config"(controller: "nodeApi", action: "configNode")
-		"/api/v1/nodes/$nodeIp/shutdown"(controller: "nodeApi", action: "shutdownNode")
-		"/api/v1/nodes/$nodeIp/canvases"(controller: "nodeApi", action: "canvasesNode")
+		"/api/v1/nodes"(method: "GET", controller: "nodeApi", action: "index")
+		"/api/v1/nodes/ip"(method: "GET", controller: "nodeApi", action: "ip")
+		"/api/v1/nodes/config"(method: "GET", controller: "nodeApi", action: "config")
+		"/api/v1/nodes/shutdown"(method: "POST", controller: "nodeApi", action: "shutdown")
+		"/api/v1/nodes/canvases"(method: "GET", controller: "nodeApi", action: "canvases")
+		"/api/v1/nodes/canvases/sizes"(method: "GET", controller: "nodeApi", action: "canvasSizes")
+		"/api/v1/nodes/$nodeIp/config"(method: "GET", controller: "nodeApi", action: "configNode")
+		"/api/v1/nodes/$nodeIp/shutdown"(method: "POST", controller: "nodeApi", action: "shutdownNode")
+		"/api/v1/nodes/$nodeIp/canvases"(method: "GET", controller: "nodeApi", action: "canvasesNode")
 
 		"/api/v1/cluster/$action"(controller: "clusterApi")
 
-		"/api/v1/communities/$communityAddress/joinRequests"(resources: "communityJoinRequestApi", excludes: ["create"])
-		"/api/v1/communities/$communityAddress/secrets"(resources: "communitySecretApi", excludes: ["create"])
+		"/api/v1/communities/$communityAddress/joinRequests"(resources: "communityJoinRequestApi", excludes: ["create", "edit"])
+		"/api/v1/communities/$communityAddress/secrets"(resources: "communitySecretApi", excludes: ["create", "edit"])
 		"/api/v1/communities/$communityAddress/stats"(method: "GET", controller: "communityOperatorApi", action: "stats")
 		"/api/v1/communities/$communityAddress/members"(method: "GET", controller: "communityOperatorApi", action: "members")
 		"/api/v1/communities/$communityAddress/members/$memberAddress"(method: "GET", controller: "communityOperatorApi", action: "memberStats")
-
-		// Mappings for pages using React Router (the root for the router)
-		"/dashboard/editor/$id**?"(controller: "dashboard", action: "editor")
 	}
 }
