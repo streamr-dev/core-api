d4f295fe510e0aaaed9d46ac543d5959ba786ef8
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-01-29 11:00:59 +0200

    CORE-1833: New permissions

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -143,4 +143,5 @@ databaseChangeLog = {
 	include file: 'core/2019-09-27-stream-require-encrypted-data.groovy'
 	include file: 'core/2019-09-27-alter-pending-changes.groovy'
 	include file: 'core/2019-11-02-rm-old-ethereum-modules.groovy'
+	include file: 'core/2020-01-24-new-permissions.groovy'
 }
