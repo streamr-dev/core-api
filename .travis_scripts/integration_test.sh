ff68711af25658d10f76fdf555bcab9da6760199
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-02-17 18:29:55 +0100

    try running tests with cleanup branch of streamr-docker-dev

--- .travis_scripts/integration_test.sh
+++ .travis_scripts/integration_test.sh
@@ -3,9 +3,12 @@
 set -e
 
 sudo /etc/init.d/mysql stop
-git clone https://github.com/streamr-dev/streamr-docker-dev.git
+# TODO: remove "--branch cleanup" before merging
+git clone --branch cleanup https://github.com/streamr-dev/streamr-docker-dev.git
+
 sudo ifconfig docker0 10.200.10.1/24
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --except engine-and-editor
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" wait
 
 # Report generation phase will crash if a SecurityManager is installed. Fix: skip report generation with -no-reports
 grails test-app -no-reports -integration --stacktrace --verbose
--- .travis_scripts/rest_e2e_test.sh
+++ .travis_scripts/rest_e2e_test.sh
@@ -2,18 +2,21 @@
 
 sudo /etc/init.d/mysql stop
 (cd rest-e2e-tests && npm install)
-git clone https://github.com/streamr-dev/streamr-docker-dev.git
+
+# TODO: remove "--branch cleanup" before merging
+git clone --branch cleanup https://github.com/streamr-dev/streamr-docker-dev.git
 
 # same as streamr-docker-dev bind-ip
 sudo ifconfig docker0 10.200.10.1/24
 
 # Start everything except engine-and-editor
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --except engine-and-editor
+
 # Print app output to console
 "$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" log -f &
 
-# Allow time for services to start
-echo "Sleeping for 30 seconds..." && sleep 30
+# Wait for services to start
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" wait
 
 # Start engine-and-editor in the background
 nohup grails test run-app --non-interactive &
@@ -26,18 +29,6 @@ while true; do
 		break
 	else
 		echo "EE not receiving connections"
-		sleep 5s
-	fi
-done
-
-# Wait for network to come up
-while true; do
-	http_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/api/v1/volume)
-	if [ "$http_code" -eq 200 ]; then
-		echo "brokers up and running"
-		break
-	else
-		echo "brokers not receiving connections"
 		sleep 5s
 	fi
 done
