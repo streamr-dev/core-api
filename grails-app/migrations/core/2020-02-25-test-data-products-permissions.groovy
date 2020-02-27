1cb820fafebf2f58a101e1d96bcfd9bbf079431d
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-02-27 10:23:39 +0200

    Improve migration

--- grails-app/migrations/core/2020-02-25-test-data-products-permissions.groovy
+++ grails-app/migrations/core/2020-02-25-test-data-products-permissions.groovy
@@ -4,18 +4,13 @@ import com.unifina.domain.security.Permission
 
 databaseChangeLog = {
 	// See grails-app/migrations/core/2018-05-02-test-data-products-subscriptions.groovy
-	changeSet(author: "kkn", id: "test-data-products-permissions", context: "test") {
+	changeSet(author: "kkn", id: "test-data-products-permissions-1", context: "test") {
 		grailsChange {
 			change {
 				sql.eachRow("select id from product") { row ->
 					String id = row['id']
 					sql.execute("delete from permission where product_id = ?", [id])
-					[
-						Permission.Operation.PRODUCT_GET.id,
-						Permission.Operation.PRODUCT_SHARE.id,
-						Permission.Operation.PRODUCT_EDIT.id,
-						Permission.Operation.PRODUCT_DELETE.id,
-					].each { operation ->
+					Permission.Operation.productOperations()*.id.each { operation ->
 						sql.execute("insert into permission(version, product_id, operation, user_id, anonymous) values(0, ?, ?, 1, 0)", [id, operation])
 					}
 					sql.execute("insert into permission(version, product_id, operation, anonymous) values(0, ?, 'product_get', 1)", [id])
