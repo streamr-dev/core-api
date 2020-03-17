3f8ab7f34c93e9c436f566de8caa5aaee5deaef1
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-03-17 18:24:37 +0200

    Integrate spring-security-plugin to Engine
    - Remove most unnecessary code from plugin export
    - Add default security config
    - Add missing configurations

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,10 +1,14 @@
 import com.streamr.api.client.StreamrClientProvider
 import com.unifina.provider.S3FileUploadProvider
 import com.unifina.utils.CustomEditorRegistrar
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.plugin.springsecurity.authentication.encoding.BCryptPasswordEncoder
 
 // Place your Spring DSL code here
 beans = {
 	myOwnCustomEditorRegistrar(CustomEditorRegistrar)
+
+	passwordEncoder(BCryptPasswordEncoder, SpringSecurityUtils.securityConfig.password.bcrypt.logrounds)
 
 	fileUploadProvider(S3FileUploadProvider,
 		(String) grailsApplication.config.streamr.fileUpload.s3.region,
