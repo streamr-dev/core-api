f6e8bdb6af5e3c357e8c256d83688c0914f1ccae
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-04-14 17:06:14 +0300

    db-diff.sh: remove shellcheck nits
    While at it, introduce variables.

--- scripts/db-diff.sh
+++ scripts/db-diff.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if ! [ -n "$1" ]
-then
+if [ -z "$1" ]; then
 	echo "Usage: ./scripts/db-diff.sh description-of-your-changes"
 	exit 1
 fi
 
-DATE=`date +%Y-%m-%d`
-FILENAME="core/$DATE-$1.groovy"
-echo $FILENAME
+FILENAME="$(date +%Y-%m-%d)-$1.groovy"
+FILEPATH="grails-app/migrations/core/$FILENAME"
 
 # Run the migration diff script
-grails dbm-gorm-diff $FILENAME --add $2 --stacktrace
+if ! grails dbm-gorm-diff "core/$FILENAME" --add --stacktrace; then
+	exit 1
+fi
 
-# Add package to beginning of file
-{ printf "package core\n"; cat grails-app/migrations/$FILENAME; } > grails-app/migrations/$FILENAME.new
-mv grails-app/migrations/$FILENAME{.new,}
+echo "package core" > "$FILEPATH.new"
+cat "$FILEPATH" >> "$FILEPATH.new"
+mv "$FILEPATH.new" "$FILEPATH"
 
 sed -i '' -e '/^$/d' grails-app/migrations/changelog.groovy # remove empty lines
-sed -i '' -e '/^$/d' -e 's/[[:space:]](generated)//' "grails-app/migrations/$FILENAME" # replace " (generated)" with ""
+sed -i '' -e '/^$/d' -e 's/[[:space:]](generated)//' "$FILEPATH" # replace " (generated)" with ""
 # replace generated numeric (len 13-14) changeset id with $1
-sed -i '' -E 's/^	changeSet\(author: "([a-zA-Z]+)", id: "[0-9]{13,14}-([0-9])"\) \{$/	changeSet\(author: "\1", id: "'"$1"'-\2"\) \{/g' "grails-app/migrations/$FILENAME"
+sed -i '' -E 's/^	changeSet\(author: "([a-zA-Z]+)", id: "[0-9]{13,14}-([0-9])"\) \{$/	changeSet\(author: "\1", id: "'"$1"'-\2"\) \{/g' "$FILEPATH"
 
