bcdfbe7613ac50f2f028b88cca23c15b34372569
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-04-15 12:23:51 +0300

    Migration for username to email

--- grails-app/migrations/core/2020-04-15-user-refactor-username-add-email.groovy
+++ grails-app/migrations/core/2020-04-15-user-refactor-username-add-email.groovy
@@ -1,9 +1,25 @@
 package core
+
+import com.unifina.utils.EmailValidator
+
 databaseChangeLog = {
 	changeSet(author: "kkn", id: "user-refactor-username-add-email-1") {
 		addColumn(tableName: "sec_user") {
 			column(name: "email", type: "varchar(255)") {
 				constraints(nullable: "false")
+			}
+		}
+	}
+	changeSet(author: "kkn", id: "user-refactor-username-add-email-2") {
+		grailsChange {
+			change {
+				sql.eachRow("select username from sec_user") { row ->
+					String username = row["username"]
+					if (EmailValidator.validate(username)) {
+						println("not a valid email: " + username)
+						sql.execute('update sec_user set email = ? where username = ?', username, username)
+					}
+				}
 			}
 		}
 	}
