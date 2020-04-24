dd6f0f7ac124a14722a9e470141eea240a9bd875
Eric Andrews <eric.andrews@streamr.com>
2020-04-24 18:52:51 +0300

    update streamr-client-java to 1.3.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -110,7 +110,7 @@ grails.project.dependency.resolution = {
 		compile('org.glassfish.jersey.media:jersey-media-json-jackson:2.27')
 		compile('com.fasterxml.jackson.core:jackson-databind:2.9.6')
 		compile('com.fasterxml.jackson.core:jackson-annotations:2.9.6')
-		compile('com.streamr:client:1.2.5')
+		compile('com.streamr:client:1.3.2')
 
 		compile('com.google.code.gson:gson:2.8.5')
 		runtime('mysql:mysql-connector-java:5.1.20')
