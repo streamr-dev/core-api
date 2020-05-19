fcfbfb4601cc8c77b5c791313ca7bb865123509e
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-05-19 18:25:34 +0200

    cache SecUser instead of EthereumIntegrationKeyService

--- src/java/com/streamr/client/authentication/InternalAuthenticationMethod.java
+++ src/java/com/streamr/client/authentication/InternalAuthenticationMethod.java
@@ -11,17 +11,16 @@ import java.nio.charset.Charset;
 
 public class InternalAuthenticationMethod extends EthereumAuthenticationMethod {
 	private final SessionService sessionService;
-	private final EthereumIntegrationKeyService ethereumIntegrationKeyService;
+	private final SecUser user;
 
 	public InternalAuthenticationMethod(String ethereumPrivateKey, EthereumIntegrationKeyService ethereumIntegrationKeyService, SessionService sessionService) {
 		super(ethereumPrivateKey);
-		this.ethereumIntegrationKeyService = ethereumIntegrationKeyService;
+		this.user = ethereumIntegrationKeyService.getOrCreateFromEthereumAddress(this.getAddress());
 		this.sessionService = sessionService;
 	}
 
 	@Override
 	protected AuthenticationMethod.LoginResponse login(String restApiUrl) throws IOException {
-		final SecUser user = ethereumIntegrationKeyService.getOrCreateFromEthereumAddress(this.getAddress());
 		final SessionToken sessionToken = sessionService.generateToken(user);
 
 		// TODO: replace when AuthenticationMethod.LoginResponse is visible to subclass
