aadc931caa1bc8e0d7b0c52b334f75709f6cb3ce
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-05-19 18:18:03 +0200

    use InternalAuthenticationMethod to obtain sessionToken

---
+++ src/java/com/streamr/client/authentication/InternalAuthenticationMethod.java
@@ -0,0 +1,44 @@
+package com.streamr.client.authentication;
+
+import com.google.gson.Gson;
+import com.unifina.domain.security.SecUser;
+import com.unifina.security.SessionToken;
+import com.unifina.service.EthereumIntegrationKeyService;
+import com.unifina.service.SessionService;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class InternalAuthenticationMethod extends EthereumAuthenticationMethod {
+	private final SessionService sessionService;
+	private final EthereumIntegrationKeyService ethereumIntegrationKeyService;
+
+	public InternalAuthenticationMethod(String ethereumPrivateKey, EthereumIntegrationKeyService ethereumIntegrationKeyService, SessionService sessionService) {
+		super(ethereumPrivateKey);
+		this.ethereumIntegrationKeyService = ethereumIntegrationKeyService;
+		this.sessionService = sessionService;
+	}
+
+	@Override
+	protected AuthenticationMethod.LoginResponse login(String restApiUrl) throws IOException {
+		final SecUser user = ethereumIntegrationKeyService.getOrCreateFromEthereumAddress(this.getAddress());
+		final SessionToken sessionToken = sessionService.generateToken(user);
+
+		// TODO: replace when AuthenticationMethod.LoginResponse is visible to subclass
+		String jsonResponse = new Gson().toJson(sessionToken.toMap());
+		return this.parse(new okio.Buffer().writeString(jsonResponse, Charset.forName("UTF-8")));
+
+		/*
+		new AuthenticationMethod.LoginResponse() {
+			@Override
+			String getSessionToken() {
+				return sessionToken.getToken()
+			}
+			@Override
+			Date getExpiration() {
+				return sessionToken.getExpiration()
+			}
+		}
+		*/
+	}
+}
