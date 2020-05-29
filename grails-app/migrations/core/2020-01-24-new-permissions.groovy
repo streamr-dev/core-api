59e1bbd9870d89b48ebe8d08f3edac06c697af76
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-05-29 10:43:33 +0300

    CORE-1938: Fix migration bug

--- grails-app/migrations/core/2020-01-24-new-permissions.groovy
+++ grails-app/migrations/core/2020-01-24-new-permissions.groovy
@@ -5,15 +5,17 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-1") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'read'") { row ->
+				sql.eachRow("select id, user_id, invite_id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'read'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String streamID = row["stream_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_get', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_subscribe', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_get', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_subscribe', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -22,16 +24,18 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-2") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'write'") { row ->
+				sql.eachRow("select id, user_id, invite_id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'write'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String streamID = row["stream_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_edit', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_publish', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_delete', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_edit', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_publish', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_delete', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -40,14 +44,16 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-3") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'share'") { row ->
+				sql.eachRow("select id, user_id, invite_id, stream_id, ends_at, parent_id, anonymous from permission where stream_id is not null and operation = 'share'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String streamID = row["stream_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous) values(0, 'stream_share', ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, stream_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'stream_share', ?, ?, ?, ?, ?, ?)", [streamID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -58,15 +64,17 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-4") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'read'") { row ->
+				sql.eachRow("select id, user_id, invite_id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'read'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String canvasID = row["canvas_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_get', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_interact', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_get', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_interact', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -75,16 +83,18 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-5") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'write'") { row ->
+				sql.eachRow("select id, user_id, invite_id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'write'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String canvasID = row["canvas_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_edit', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_startstop', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_delete', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_edit', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_startstop', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_delete', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -93,14 +103,16 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-6") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'share'") { row ->
+				sql.eachRow("select id, user_id, invite_id, canvas_id, ends_at, parent_id, anonymous from permission where canvas_id is not null and operation = 'share'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String canvasID = row["canvas_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous) values(0, 'canvas_share', ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, canvas_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'canvas_share', ?, ?, ?, ?, ?, ?)", [canvasID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -111,15 +123,17 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-7") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'read'") { row ->
+				sql.eachRow("select id, user_id, invite_id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'read'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String dashboardID = row["dashboard_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous) values(0, 'dashboard_get', ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous) values(0, 'dashboard_interact', ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'dashboard_get', ?, ?, ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'dashboard_interact', ?, ?, ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -128,15 +142,17 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-8") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'write'") { row ->
+				sql.eachRow("select id, user_id, invite_id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'write'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String dashboardID = row["dashboard_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous) values(0, 'dashboard_edit', ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous) values(0, 'dashboard_delete', ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'dashboard_edit', ?, ?, ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'dashboard_delete', ?, ?, ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -145,14 +161,16 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-9") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'share'") { row ->
+				sql.eachRow("select id, user_id, invite_id, dashboard_id, ends_at, parent_id, anonymous from permission where dashboard_id is not null and operation = 'share'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String dashboardID = row["dashboard_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous) values(0, 'dashboard_share', ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, dashboard_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'dashboard_share', ?, ?, ?, ?, ?, ?)", [dashboardID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -171,15 +189,17 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-11") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, product_id, ends_at, parent_id, anonymous from permission where product_id is not null and operation = 'write'") { row ->
+				sql.eachRow("select id, user_id, invite_id, product_id, ends_at, parent_id, anonymous from permission where product_id is not null and operation = 'write'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String productID = row["product_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous) values(0, 'product_edit', ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous])
-					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous) values(0, 'product_delete', ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'product_edit', ?, ?, ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous, userID, inviteID])
+					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'product_delete', ?, ?, ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
@@ -188,14 +208,16 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "new-permissions-12") {
 		grailsChange {
 			change {
-				sql.eachRow("select id, product_id, ends_at, parent_id, anonymous from permission where product_id is not null and operation = 'share'") { row ->
+				sql.eachRow("select id, user_id, invite_id, product_id, ends_at, parent_id, anonymous from permission where product_id is not null and operation = 'share'") { row ->
 					Long permissionID = row["id"]
+					Long userID = row["user_id"]
+					Long inviteID = row["invite_id"]
 					String productID = row["product_id"]
 					Date endsAt = row["ends_at"]
 					Long parentID = row["parent_id"]
 					Boolean anonymous = row["anonymous"]
 					sql.execute("delete from permission where id = ?", [permissionID])
-					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous) values(0, 'product_share', ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous])
+					sql.execute("insert into permission(version, operation, product_id, ends_at, parent_id, anonymous, user_id, invite_id) values(0, 'product_share', ?, ?, ?, ?, ?, ?)", [productID, endsAt, parentID, anonymous, userID, inviteID])
 				}
 			}
 		}
