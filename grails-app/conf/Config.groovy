169965c91719d3b6267f98fea91f3b9aec6b671a
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-06-05 19:44:30 +0300

    Config mime

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -20,22 +20,22 @@ grails.serverURL = baseUrl
 grails.project.groupId = appName
 
 grails.mime.use.accept.header = true
-// Streamr uses the HTTP request Accept header to negoatiate about content with each client
-grails.mime.disable.accept.header.userAgents = []
+// The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
+grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [ // the first one is the default format
-					  json         : ['application/json', 'text/json'],
-					  all          : '*/*', // 'all' maps to '*' or the first available format in withFormat
-					  atom         : 'application/atom+xml',
-					  css          : 'text/css',
-					  csv          : 'text/csv',
-					  form         : 'application/x-www-form-urlencoded',
-					  html         : ['text/html', 'application/xhtml+xml'],
-					  js           : 'text/javascript',
+					  all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
+					  atom:          'application/atom+xml',
+					  css:           'text/css',
+					  csv:           'text/csv',
+					  form:          'application/x-www-form-urlencoded',
+					  html:          ['text/html','application/xhtml+xml'],
+					  js:            'text/javascript',
+					  json:          ['application/json', 'text/json'],
 					  multipartForm: 'multipart/form-data',
-					  rss          : 'application/rss+xml',
-					  text         : 'text/plain',
-					  hal          : ['application/hal+json', 'application/hal+xml'],
-					  xml          : ['text/xml', 'application/xml'],
+					  rss:           'application/rss+xml',
+					  text:          'text/plain',
+					  hal:           ['application/hal+json','application/hal+xml'],
+					  xml:           ['text/xml', 'application/xml']
 ]
 
 environments {
