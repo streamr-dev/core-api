bed1c942c753ba4f880b9e2d9517faeb9ca51ecc
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-06-11 13:31:58 +0300

    Config: Remove redundant content-types

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -24,18 +24,11 @@ grails.mime.use.accept.header = true
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [ // the first one is the default format
 					  all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
-					  atom:          'application/atom+xml',
-					  css:           'text/css',
 					  csv:           'text/csv',
 					  form:          'application/x-www-form-urlencoded',
-					  html:          ['text/html','application/xhtml+xml'],
-					  js:            'text/javascript',
 					  json:          ['application/json', 'text/json'],
 					  multipartForm: 'multipart/form-data',
-					  rss:           'application/rss+xml',
 					  text:          'text/plain',
-					  hal:           ['application/hal+json','application/hal+xml'],
-					  xml:           ['text/xml', 'application/xml']
 ]
 
 environments {
