a1e370a36f40d34573b214f3d44fd5869ee6049f
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-09-02 14:35:32 +0300

    Fix unit tests

--- test/unit/com/unifina/service/StreamrClientServiceSpec.groovy
+++ test/unit/com/unifina/service/StreamrClientServiceSpec.groovy
@@ -28,7 +28,8 @@ class StreamrClientServiceSpec extends Specification {
 	}
 
 	void "getAuthenticatedInstance() should create a StreamrClient with EthereumAuthenticationMethod (user has one Key)"() {
-		new IntegrationKey(
+		setup:
+		IntegrationKey key = new IntegrationKey(
 			id: "ik-1",
 			user: user,
 			name: "Test key",
@@ -36,16 +37,19 @@ class StreamrClientServiceSpec extends Specification {
 			json: '{"address": "0xb794F5eA0ba39494cE839613fffBA74279579268", "privateKey": "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"}',
 			idInService: "0xb794F5eA0ba39494cE839613fffBA74279579268",
 		).save(failOnError: true, validate: true)
+		service.ethereumIntegrationKeyService = Mock(EthereumIntegrationKeyService)
 		assert IntegrationKey.countByUser(user) == 1
 
 		when:
 		FakeStreamrClient client = (FakeStreamrClient) service.getAuthenticatedInstance(user.id)
 		then:
 		client.getOptions().getAuthenticationMethod() instanceof EthereumAuthenticationMethod
+		1 * service.ethereumIntegrationKeyService.decryptPrivateKey(key) >> "3d1434fe42701a79f8073e1a47d60e57c7b4e951bb663a54f611a54cfc993b3aa8d769afca74aa28f914e3831bfc50b2f120a187021f1abafe703e92ae84f5bfee57d0a79936f0ef87c58d5774a179f15ac715a7bbcc9e357f18d4018e56ac7b"
 	}
 
 	void "getAuthenticatedInstance() should create a StreamrClient with EthereumAuthenticationMethod (user has several Keys)"() {
-		new IntegrationKey(
+		setup:
+		IntegrationKey key = new IntegrationKey(
 			id: "ik-1",
 			user: user,
 			name: "Test key",
@@ -61,12 +65,14 @@ class StreamrClientServiceSpec extends Specification {
 			json: '{"address": "0x222222222ba39494cE839613fffBA74279579268", "privateKey": "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"}',
 			idInService: "0x222222222ba39494cE839613fffBA74279579268",
 		).save(failOnError: true, validate: true)
+		service.ethereumIntegrationKeyService = Mock(EthereumIntegrationKeyService)
 		assert IntegrationKey.countByUser(user) == 2
 
 		when:
 		FakeStreamrClient client = (FakeStreamrClient) service.getAuthenticatedInstance(user.id)
 		then:
 		client.getOptions().getAuthenticationMethod() instanceof EthereumAuthenticationMethod
+		1 * service.ethereumIntegrationKeyService.decryptPrivateKey(key) >> "3d1434fe42701a79f8073e1a47d60e57c7b4e951bb663a54f611a54cfc993b3aa8d769afca74aa28f914e3831bfc50b2f120a187021f1abafe703e92ae84f5bfee57d0a79936f0ef87c58d5774a179f15ac715a7bbcc9e357f18d4018e56ac7b"
 	}
 
 	void "getAuthenticatedInstance() should generate a new integration key if none is found"() {
@@ -77,11 +83,13 @@ class StreamrClientServiceSpec extends Specification {
 			json: '{"address": "0x222222222ba39494cE839613fffBA74279579268", "privateKey": "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"}',
 		)
 		service.ethereumIntegrationKeyService = Mock(EthereumIntegrationKeyService)
+
 		when:
 		FakeStreamrClient client = (FakeStreamrClient) service.getAuthenticatedInstance(user.id)
 		then:
 		client.getOptions().getAuthenticationMethod() instanceof EthereumAuthenticationMethod
 		1 * service.ethereumIntegrationKeyService.createEthereumAccount(user, "Auto-generated key", _) >> key
+		1 * service.ethereumIntegrationKeyService.decryptPrivateKey(key) >> "3d1434fe42701a79f8073e1a47d60e57c7b4e951bb663a54f611a54cfc993b3aa8d769afca74aa28f914e3831bfc50b2f120a187021f1abafe703e92ae84f5bfee57d0a79936f0ef87c58d5774a179f15ac715a7bbcc9e357f18d4018e56ac7b"
 	}
 
 	void "getInstanceForThisEngineNode() uses sessionService to generate a sessionToken (instead of making an API call)"() {
