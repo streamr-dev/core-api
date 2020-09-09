5e4d5dc22c5ca60ec11bc6fc034e0d2cd72e451d
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-09-09 14:21:42 +0300

    BACK-3: Canvases use Ethereum-authenticated StreamrClient (#812)
    * BACK-3: Canvases use Ethereum-authenticated StreamrClient instance to publish and subscribe
    
    * Parse private key from IntegrationKey.json
    
    * Fix unit tests
    
    * Fix key generation
    
        - Update Streamr Java client 2.0.1
    
    * Fix unit tests
    
    * Fix private key generator
    
    Need to pad with 0 so that the is always 64 chars
    
    Co-authored-by: Teo Gebhard <teo@streamr.com>

--- test/unit/com/unifina/domain/IntegrationKeySpec.groovy
+++ test/unit/com/unifina/domain/IntegrationKeySpec.groovy
@@ -3,14 +3,27 @@ package com.unifina.domain
 import spock.lang.Specification
 
 class IntegrationKeySpec extends Specification {
-	def "toMap().json for Ethereum integration key"() {
-		def key = new IntegrationKey(
+	void "toMap().json for Ethereum integration key"() {
+		setup:
+		IntegrationKey key = new IntegrationKey(
 			user: new User(),
 			service: IntegrationKey.Service.ETHEREUM,
-			json: '{"address": "0xb794F5eA0ba39494cE839613fffBA74279579268", "privateKey": "0000000000000000000000000"}'
+			json: '{"address": "0xb794F5eA0ba39494cE839613fffBA74279579268", "privateKey": "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"}'
 		)
 
 		expect:
 		key.toMap().json == [address: "0xb794F5eA0ba39494cE839613fffBA74279579268"]
+	}
+
+	void "parse private key from json"() {
+		setup:
+		IntegrationKey key = new IntegrationKey(
+			user: new User(),
+			service: IntegrationKey.Service.ETHEREUM,
+			json: '{"address": "0xb794F5eA0ba39494cE839613fffBA74279579268", "privateKey": "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"}'
+		)
+
+		expect:
+		key.parsePrivateKey() == "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0"
 	}
 }
