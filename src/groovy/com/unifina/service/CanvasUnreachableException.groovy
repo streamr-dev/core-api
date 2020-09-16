dd27e3ac3cf5f19fec69d97fc2aa764ea0cbff81
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-09-16 12:21:42 +0300

    Cast exceptions null message to String

--- src/groovy/com/unifina/service/CanvasUnreachableException.groovy
+++ src/groovy/com/unifina/service/CanvasUnreachableException.groovy
@@ -2,6 +2,6 @@ package com.unifina.service
 
 class CanvasUnreachableException extends RuntimeException {
 	CanvasUnreachableException(String message) {
-		super(message)
+		super(message as String)
 	}
 }
