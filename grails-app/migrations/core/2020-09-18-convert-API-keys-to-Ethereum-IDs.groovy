84a367371816620b947b383397aceb1718a264f1
Teo Gebhard <teo@streamr.com>
2020-10-01 14:00:47 +0300

    Optimize script, create inbox streams also for integration keys that are created from user keys

--- grails-app/migrations/core/2020-09-18-convert-API-keys-to-Ethereum-IDs.groovy
+++ grails-app/migrations/core/2020-09-18-convert-API-keys-to-Ethereum-IDs.groovy
@@ -11,112 +11,247 @@ import com.unifina.service.EthereumIntegrationKeyService
 import com.unifina.utils.AlphanumericStringGenerator
 import grails.converters.JSON
 import groovy.sql.Sql
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Callable
+import java.util.concurrent.Future
+import java.lang.Runtime
 
-def getAccountAddress(String apiKeyId) {
-	String privateKey = ApiKeyConverter.createEthereumPrivateKey(apiKeyId)
-	return "0x" + EthereumIntegrationKeyService.getAddress(privateKey)
-}
+class GetAccountAddressTask implements Callable<String> {
+	private String apiKey
 
-def createIntegrationKey(String integrationKeyName, String accountAddress, Integer userId, Sql sql) {
-	Date now = new Date()
-	return sql.executeInsert("INSERT INTO integration_key (id, version, name, json, user_id, service, id_in_service, date_created, last_updated) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?)", [
-		IdGenerator.get(),
-		0,
-		integrationKeyName,
-		([ address: accountAddress ] as JSON).toString(),
-		userId,
-		IntegrationKey.Service.ETHEREUM_ID.name(),
-		accountAddress,
-		now,
-		now
-	]);
-}
-
-def createInboxStream(long userId, String accountAddress, Sql sql) {
-	Date now = new Date();
-	sql.executeInsert('INSERT INTO stream (version, id, name, date_created, last_updated, partitions, require_signed_data, auto_configure, storage_days, inbox, inactivity_threshold_hours, example_type, require_encrypted_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
-		0,
-		accountAddress,
-		accountAddress,
-		now,
-		now,
-		1,
-		false,
-		false,
-		Stream.DEFAULT_STORAGE_DAYS,
-		true,
-		Stream.DEFAULT_INACTIVITY_THRESHOLD_HOURS,
-		ExampleType.NOT_SET.value,
-		false
-	]);
-	List<Permission.Operation> permissionOperations = [
-		Permission.Operation.STREAM_GET,
-		Permission.Operation.STREAM_EDIT,
-		Permission.Operation.STREAM_DELETE,
-		Permission.Operation.STREAM_PUBLISH,
-		Permission.Operation.STREAM_SUBSCRIBE,
-		Permission.Operation.STREAM_SHARE,
-	];
-	permissionOperations.each { op ->
-		sql.executeInsert('INSERT INTO permission (version, operation, stream_id, anonymous, user_id) VALUES (?, ?, ?, ?, ?)', [
-			0,
-			op.id,
-			accountAddress,
-			false,
-			userId
-		]);
+	GetAccountAddressTask(String apiKey) {
+		this.apiKey = apiKey
 	}
+
+	@Override
+	String call() throws Exception {
+		String privateKey = ApiKeyConverter.createEthereumPrivateKey(apiKey)
+		return "0x" + EthereumIntegrationKeyService.getAddress(privateKey)
+	}
+}
+
+def convertUserKeysToIntegrationKeys(Sql sql) {
+	String statement = """
+		INSERT INTO
+			integration_key (id, version, name, json, user_id, service, id_in_service, date_created, last_updated)
+		SELECT
+			generated_integration_key_id,
+			0,
+			CONCAT('Converted from API key: ',key_id),
+			CONCAT(CONCAT('{\"address\":\"',account_address),'\"}'),
+			user_id,
+			?,
+			account_address,
+			CURRENT_TIMESTAMP(),
+			CURRENT_TIMESTAMP()
+		FROM `key`
+		LEFT JOIN tmp_apikey_lookup ON tmp_apikey_lookup.key_id = `key`.id
+		WHERE `key`.user_id is not null
+	"""
+	sql.execute(statement, [IntegrationKey.Service.ETHEREUM_ID.name()])
 }
 
 databaseChangeLog = {
 
-	// For each API key that is attached to a user:
-	// - create a new IntegrationKey for the user
-	// - delete the key
+	// fill a lookup table
 	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-1") {
+		createTable(tableName: "tmp_apikey_lookup") {
+			column(name: "key_id", type: "VARCHAR(255)") {
+				constraints(nullable: "false")
+			}
+			column(name: "user_id", type: "BIGINT(20)") {
+				constraints(nullable: "true")
+			}
+			column(name: "key_name", type: "VARCHAR(255)") {
+				constraints(nullable: "false")
+			}
+			column(name: "account_address", type: "VARCHAR(255)") {
+				constraints(nullable: "false")
+			}
+			column(name: "generated_integration_key_id", type: "VARCHAR(255)") {
+				constraints(nullable: "false")
+			}
+			column(name: "generated_user_password", type: "VARCHAR(255)") {
+				constraints(nullable: "true")
+			}
+		}
+		createIndex(tableName: "tmp_apikey_lookup", indexName: "tmp_apikey_lookup_index_1", unique: "true") {
+			column(name: "key_id")
+		}
+		createIndex(tableName: "tmp_apikey_lookup", indexName: "tmp_apikey_lookup_index_2", unique: "false") {
+			column(name: "user_id")
+		}
+		createIndex(tableName: "tmp_apikey_lookup", indexName: "tmp_apikey_lookup_index_3", unique: "true") {
+			column(name: "account_address")
+		}
 		grailsChange {
 			change {
-				sql.eachRow('SELECT id, name, user_id FROM `key` where user_id is not null') { row ->
+				def THREAD_COUNT = Runtime.getRuntime().availableProcessors() + 1
+				ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT)
+				def migrations = []
+				sql.eachRow('SELECT id, name, user_id FROM `key`') { row ->
 					String apiKeyId = row['id']
 					String apiKeyName = row['name']
-					int userId = row['user_id']
-					String accountAddress = getAccountAddress(apiKeyId)
-					String integrationKeyName = 'Converted from API key: ' + apiKeyName
-					createIntegrationKey(integrationKeyName, accountAddress, userId, sql)
+					Long userId = row['user_id']
+					migrations << [
+						apiKeyId: apiKeyId,
+						apiKeyName: apiKeyName,
+						userId: userId,
+						accountAddressProducer: executorService.submit(new GetAccountAddressTask(apiKeyId)),
+					]
+				}
+				sql.withBatch('INSERT INTO tmp_apikey_lookup (key_id, key_name, user_id, account_address, generated_integration_key_id, generated_user_password) VALUES (?, ?, ?, ?, ?, ?)') { statement ->
+					migrations.each { migration ->
+						statement.addBatch([
+							migration.apiKeyId,
+							migration.apiKeyName,
+							migration.userId,
+							migration.accountAddressProducer.get(),
+							IdGenerator.get(),
+							(migration.userId == null) ? AlphanumericStringGenerator.getRandomAlphanumericString(32) : null
+						])
+					}
 				}
 			}
 		}
-		sql('DELETE FROM `key` where user_id is not null')
 	}
 
-	// For each anonymous API key:
-	// - create a new Ethereum user (and inbox stream for that user)
-	// - migrate the key’s permissions to that user
-	// - delete the key
+	// create new users for anonymous keys (and update lookup table)
 	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-2") {
 		grailsChange {
 			change {
-				sql.eachRow('SELECT id, name, user_id FROM `key` where user_id is null') { row ->
-					String apiKeyId = row['id']
-					String accountAddress = getAccountAddress(apiKeyId)
-					def insertResult = sql.executeInsert("INSERT INTO user (version, account_expired, account_locked, enabled, name, password, password_expired, username, signup_method) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+				sql.execute("""
+					INSERT INTO user (version, account_expired, account_locked, enabled, name, password, password_expired, username, signup_method)
+					SELECT
 						0,
 						false,
 						false,
 						true,
-						"Anonymous User",
-						AlphanumericStringGenerator.getRandomAlphanumericString(32),
+						'Anonymous User',
+						generated_user_password,
 						false,
-						accountAddress,
-						SignupMethod.MIGRATED.name()
-					]);
-					int userId = insertResult[0][0]
-					sql.execute("UPDATE permission SET user_id = ?, key_id = null WHERE key_id = ?", [ userId, apiKeyId])
-					createIntegrationKey(accountAddress, accountAddress, userId, sql)
-					createInboxStream(userId, accountAddress, sql)
-				}
+						account_address,
+						?
+					FROM tmp_apikey_lookup
+					WHERE user_id is null
+				""", [SignupMethod.MIGRATED.name()])
+				sql.execute("""
+					UPDATE tmp_apikey_lookup
+					LEFT JOIN user ON user.username = tmp_apikey_lookup.account_address
+					SET
+						tmp_apikey_lookup.user_id = user.id
+					WHERE user_id is null
+				""");
 			}
 		}
-		sql('DELETE FROM `key` where user_id is null')
+	}
+
+	// move the permissions of anonymous keys
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-3") {
+		grailsChange {
+			change {
+				sql.execute("""
+					UPDATE permission
+					LEFT JOIN tmp_apikey_lookup ON tmp_apikey_lookup.key_id = permission.key_id
+					SET
+						permission.user_id = tmp_apikey_lookup.user_id,
+						permission.key_id = null
+					WHERE permission.key_id is not null
+				""");
+			}
+		}
+	}
+
+	// create integration keys
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-4") {
+		grailsChange {
+			change {
+				sql.execute("""
+					INSERT INTO integration_key (id, version, name, json, user_id, service, id_in_service, date_created, last_updated)
+					SELECT
+						generated_integration_key_id,
+						0,
+						CONCAT('Converted from API key: ',key_name),
+						CONCAT(CONCAT('{\"address\":\"',account_address),'\"}'),
+						user_id,
+						?,
+						account_address,
+						CURRENT_TIMESTAMP(),
+						CURRENT_TIMESTAMP()
+					FROM tmp_apikey_lookup
+				""", [IntegrationKey.Service.ETHEREUM_ID.name()])
+			}
+		}
+	}
+
+	// create inbox streams for the integration keys
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-5") {
+		grailsChange {
+			change {
+				sql.execute("""
+					INSERT INTO stream (version, id, name, date_created, last_updated, partitions, require_signed_data, auto_configure, storage_days, inbox, inactivity_threshold_hours, example_type, require_encrypted_data)
+					SELECT
+						0,
+						account_address,
+						account_address,
+						CURRENT_TIMESTAMP(),
+						CURRENT_TIMESTAMP(),
+						1,
+						false,
+						false,
+						?,
+						true,
+						?,
+						?,
+						false
+					FROM tmp_apikey_lookup
+				""", [Stream.DEFAULT_STORAGE_DAYS, Stream.DEFAULT_INACTIVITY_THRESHOLD_HOURS, ExampleType.NOT_SET.value])
+			}
+		}
+	}
+
+	// create permissions for the inbox streams
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-6") {
+		grailsChange {
+			change {
+				List<Permission.Operation> permissionOperations = [
+					Permission.Operation.STREAM_GET,
+					Permission.Operation.STREAM_EDIT,
+					Permission.Operation.STREAM_DELETE,
+					Permission.Operation.STREAM_PUBLISH,
+					Permission.Operation.STREAM_SUBSCRIBE,
+					Permission.Operation.STREAM_SHARE,
+				];
+				sql.execute("""
+					INSERT INTO permission (version, operation, stream_id, anonymous, user_id)
+					SELECT
+						0,
+						permission_id,
+						account_address,
+						false,
+						user_id
+					FROM tmp_apikey_lookup
+					JOIN (
+						(SELECT ? as permission_id FROM DUAL) UNION ALL
+						(SELECT ? as permission_id FROM DUAL) UNION ALL
+						(SELECT ? as permission_id FROM DUAL) UNION ALL
+						(SELECT ? as permission_id FROM DUAL) UNION ALL
+						(SELECT ? as permission_id FROM DUAL) UNION ALL
+						(SELECT ? as permission_id FROM DUAL)
+					) as permission_ids
+				""", permissionOperations.collect { it.id })
+			}
+		}
+	}
+
+	// delete keys
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-7") {
+		sql('DELETE FROM `key`')
+	}
+
+	// drop lookup table
+	changeSet(author: "teogeb", id: "convert-API-keys-to-Ethereum-IDs-8") {
+		dropTable(tableName: "tmp_apikey_lookup")
 	}
 }
