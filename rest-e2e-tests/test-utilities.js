4e51cd7a3a2506a521e123a9e3156237cdc79911
Teo Gebhard <teo@streamr.com>
2020-10-14 15:24:59 +0300

    Unit tests: utility to create a new session tokek

--- rest-e2e-tests/test-utilities.js
+++ rest-e2e-tests/test-utilities.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert
+const StreamrClient = require('streamr-client')
 
 async function assertResponseIsError(response, statusCode, programmaticCode, includeInMessage) {
     const json = await response.json()
@@ -9,6 +10,24 @@ async function assertResponseIsError(response, statusCode, programmaticCode, inc
     }
 }
 
+async function newSessionToken(restURL, privateKey) {
+	const client = new StreamrClient({
+		restUrl: restURL,
+		auth: {
+			privateKey
+		},
+	})
+	await client.connect()
+
+	const sessionToken = await client.session.getSessionToken()
+	if (client.isConnected()) {
+		await client.disconnect()
+	}
+
+	return sessionToken
+}
+
 module.exports = {
-    assertResponseIsError: assertResponseIsError
+	assertResponseIsError: assertResponseIsError,
+	newSessionToken: newSessionToken
 }
