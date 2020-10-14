d497ee25bab0317e2100be6155db4b0f192a05e1
Teo Gebhard <teo@streamr.com>
2020-10-14 14:57:33 +0300

    isENSOwnedBy returns false if queried with non-ethereum account

--- grails-app/services/com/unifina/service/EnsService.groovy
+++ grails-app/services/com/unifina/service/EnsService.groovy
@@ -8,12 +8,13 @@ class EnsService {
 	EthereumIntegrationKeyService ethereumIntegrationKeyService
 
 	boolean isENSOwnedBy(String domain, User expectedOwner) {
-		if (!expectedOwner.isEthereumUser()) {
-			throw new EthereumUserRequiredException()
+		if (expectedOwner.isEthereumUser()) {
+			String actualOwnerAddress = Web3jHelper.getENSDomainOwner(domain)
+			User actualOwner = ethereumIntegrationKeyService.getEthereumUser(actualOwnerAddress)
+			return (actualOwner != null) && (actualOwner.id == expectedOwner.id)
+		} else {
+			return false;
 		}
-		String actualOwnerAddress = Web3jHelper.getENSDomainOwner(domain)
-		User actualOwner = ethereumIntegrationKeyService.getEthereumUser(actualOwnerAddress)
-		return (actualOwner != null) && (actualOwner.id == expectedOwner.id)
 	}
 }
 
