b7ec913db3da7ffcab9cb40a642e1a2a7d25b2be
Teo Gebhard <teo@streamr.com>
2020-11-05 16:59:15 +0200

    Use checksum form

--- src/groovy/com/unifina/domain/EthereumAddress.groovy
+++ src/groovy/com/unifina/domain/EthereumAddress.groovy
@@ -1,9 +1,12 @@
 package com.unifina.domain
 
+import org.web3j.crypto.Keys
 import com.unifina.service.ValidationException
 import grails.compiler.GrailsCompileStatic
+import groovy.transform.EqualsAndHashCode
 
 @GrailsCompileStatic
+@EqualsAndHashCode
 class EthereumAddress {
 	private String value
 
@@ -11,7 +14,7 @@ class EthereumAddress {
 		if (!EthereumAddressValidator.validate.call(value)) {
 			throw new ValidationException("Address is not a valid Ethereum address")
 		}
-		this.value = value.toLowerCase()
+		this.value = Keys.toChecksumAddress(value)
 	}
 
 	public String toString() {
