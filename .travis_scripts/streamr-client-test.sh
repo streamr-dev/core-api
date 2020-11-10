82566977b4dc3f2ee56ace2640060b688ecfc9ce
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-11-10 12:24:03 +0200

    Start dockers

--- .travis_scripts/streamr-client-test.sh
+++ .travis_scripts/streamr-client-test.sh
@@ -8,7 +8,7 @@ fi
 ## Switch EE tag to the one built locally
 sed -i "s/engine-and-editor:dev/engine-and-editor:local/g" $TRAVIS_BUILD_DIR/streamr-docker-dev/docker-compose.override.yml
 sudo ifconfig docker0 10.200.10.1/24
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start smart-contracts-init nginx engine-and-editor --wait
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --except hsl-demo --wait
 
 ## Setup testing Tool
 git clone https://github.com/streamr-dev/streamr-client-testing.git
