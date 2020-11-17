e0aec0d0deb6060e9525f919043792a13d3cddae
Teo Gebhard <teo@streamr.com>
2020-11-17 22:39:35 +0200

    Integration key domains

--- test/unit/com/unifina/domain/CustomStreamIDValidatorSpec.groovy
+++ test/unit/com/unifina/domain/CustomStreamIDValidatorSpec.groovy
@@ -4,10 +4,14 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class CustomStreamIDValidatorSpec extends Specification {
+
+	private static final String MOCK_DOMAIN = "sub.my-domain.eth"
+	private static final String MOCK_INTEGRATION_KEY_ADDRESS = "0xAbcdeabCDE123456789012345678901234567890"
+
 	@Unroll
 	void "validate #value"(String value, Boolean expected) {
 		def User mockUser = new User()
-		def domainValidator = {domain, creator -> ((creator == mockUser) && (domain.equals("sub.my-domain.eth")))}
+		def domainValidator = {domain, creator -> ((creator == mockUser) && (domain.equals(MOCK_DOMAIN) || (domain.equals(MOCK_INTEGRATION_KEY_ADDRESS))))}
 		def validator = new CustomStreamIDValidator(domainValidator)
 		expect:
 		validator.validate(value, mockUser) == expected
@@ -19,11 +23,13 @@ class CustomStreamIDValidatorSpec extends Specification {
 		"sandbox/foo.bar/lorem.ipsum" | true
 		"sandbox/foo-bar/lorem.ipsum" | true
 		"sub.my-domain.eth/abc/def" | true
+		"0xAbcdeabCDE123456789012345678901234567890/abc/def" | true
 		"sandbox/foo-bar/lorem~ipsum" | false
 		"sandbox/abc/def/" | false
 		"sandbox/abc/def/file." | false
 		"sandbox/foo//bar" | false
 		"foobar.eth/abc/def" | false
+		"0x1111111111111111111111111111111111111111/abc/def" | false
 		"foo" | false
 		"sandbox" | false
 		"" | false
