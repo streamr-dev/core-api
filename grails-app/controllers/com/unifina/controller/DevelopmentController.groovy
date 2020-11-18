581a5fd8eb3d13d771a86f108f17c47b4282dc65
Teo Gebhard <teo@streamr.com>
2020-11-18 11:28:20 +0200

    Update DevelopmentController

--- grails-app/controllers/com/unifina/controller/DevelopmentController.groovy
+++ grails-app/controllers/com/unifina/controller/DevelopmentController.groovy
@@ -12,46 +12,44 @@ import com.unifina.service.StreamrClientService
 import com.unifina.utils.MapTraversal
 import grails.converters.JSON
 import grails.util.Holders
-import org.web3j.crypto.Credentials
 
 class DevelopmentController {
 
-	private static final String DU_NAME = 'du20201118a'
+	private static final String DU_NAME = 'du20201118c'
+	private static final String NODE_ADDRESS = '0xFCAd0B19bB29D4674531d6f115237E16AfCE377c'  // the adress of private key 0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF
 
 	// random users:
-	Credentials adminCreds = Credentials.create('0x7aa27733cfc64a44ae605bfd6ec4a2c73579d37a661ddb72fcbdb4f4a752f30d')
-	Credentials memberCreds = Credentials.create('0x31b5895362c37e7066b739bee7bcb3f327bbceb87d3a785a49217a8a539b985b')
+	private static final String ADMIN_ADDRESS = '0x3AB95c1f65a732EC4370876881DAEd825A8e6F1b'  // private key: 0x7aa27733cfc64a44ae605bfd6ec4a2c73579d37a661ddb72fcbdb4f4a752f30d
+	private static final String MEMBER_ADDRESS = '0x753182ff9378e7a8Cf243aDB69d70Cb8317D6B81'  // private key: 0x31b5895362c37e7066b739bee7bcb3f327bbceb87d3a785a49217a8a539b985b
 
 	StreamrClientService streamrClientService
 	ProductService productService
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
-	def deploy() {
-		println('deploy ' + DU_NAME)
-		String nodeAddress = '0xFCAd0B19bB29D4674531d6f115237E16AfCE377c'  // the adress of private key 0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF
-		DataUnion du = getClient().deployNewDataUnion(DU_NAME, adminCreds.getAddress(), 0.1d, [nodeAddress])
-		long startTime = System.currentTimeMillis()
+	def deploy(String name) {
+		println('Deploy: ' + name)
+		DataUnion du = getClient().deployNewDataUnion(name, ADMIN_ADDRESS, 0.1d, [NODE_ADDRESS])
 		boolean success = du.waitForDeployment(10 * 1000, 10 * 60 * 1000)
-		println('took ' + (System.currentTimeMillis() - startTime) + ' ms')
-		println('success: ' + success)
-		println('address: ' + du.mainnetAddress())
-		return render([success: success] as JSON)
+		String address = du.mainnetAddress()
+		println('Completed: success=' + success + ", address=" + address)
+		return render([success: success, address: address] as JSON)
 	}
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
-	def isMemberActive() {
-		println('isMemberActive')
-		DataUnion du = getClient().dataUnionFromName(DU_NAME)
-		return render([isMemberActive: du.isMemberActive(memberCreds.getAddress())] as JSON)
+	def isMemberActive(String name) {
+		println('Check member activity: ' + name)
+		DataUnion du = getClient().dataUnionFromName(name)
+		return render([isActive: du.isMemberActive(MEMBER_ADDRESS)] as JSON)
 	}
 
 	@StreamrApi(authenticationLevel = AuthLevel.NONE)
-	def join() {
-		println('join')
-		DataUnion du = getClient().dataUnionFromName(DU_NAME)
-		EthereumTransactionReceipt tr = du.joinMembers(memberCreds.getAddress())
-		println("transaction: " + tr.getTransactionHash())
-		return render([] as JSON)
+	def join(String name) {
+		println('Join: ' + name)
+		DataUnion du = getClient().dataUnionFromName(name)
+		EthereumTransactionReceipt tr = du.joinMembers(MEMBER_ADDRESS)
+		String transaction = tr.getTransactionHash()
+		println("Transaction: " + transaction)
+		return render([transaction: transaction] as JSON)
 	}
 
 	def getClient() {
