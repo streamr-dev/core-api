b926ca580bcdf3fed5d4f9755c726b777a4c603e
Teo Gebhard <teo@streamr.com>
2020-11-25 16:31:50 +0200

    DEVOP-16: Rebase Core database dumps #880
    All DB migration scripts have been applied to core_dev.sql and core_test.sql database dumps (in streamr-docker-dev). We can remove the script files.

--- grails-app/migrations/core/2019-09-13-product-pendingchanges.groovy
+++
@@ -1,8 +0,0 @@
-package core
-databaseChangeLog = {
-	changeSet(author: "kkn", id: "product-pendingchanges-1") {
-		addColumn(tableName: "product") {
-			column(name: "pending_changes", type: "varchar(255)")
-		}
-	}
-}
