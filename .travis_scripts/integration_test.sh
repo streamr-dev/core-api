ed00d0729fd3dfc31127611453c54f2067c2dfc8
Kare Nuorteva <kare.nuorteva@streamr.com>
2020-11-25 16:25:22 +0200

    DEVOP-18: Optimise CI integration test

--- .travis_scripts/integration_test.sh
+++ .travis_scripts/integration_test.sh
@@ -7,7 +7,7 @@ if [ ! -d streamr-docker-dev ]; then # Skip clone on subsequent attemps.
 	git clone https://github.com/streamr-dev/streamr-docker-dev.git
 fi
 sudo ifconfig docker0 10.200.10.1/24
-"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start --except engine-and-editor --wait
+"$TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh" start mysql --wait
 
 # Report generation phase will crash if a SecurityManager is installed. Fix: skip report generation with -no-reports
 grails test-app -no-reports -integration --stacktrace --verbose
