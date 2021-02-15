9e323ecf6ad452c2fbc04dd572b4191479caa722
Henri Pihkala <hpihkala@users.noreply.github.com>
2021-02-15 12:57:03 +0100

    BACK-129: remove empty streams array from Product list (#919)
    * remove empty streams array
    
    * remove streams as a required field from swagger
    
    * Remove Product.streams from test JSON schema
    
    Co-authored-by: Kare Nuorteva <kare.nuorteva@streamr.com>

--- grails-app/domain/com/unifina/domain/Product.groovy
+++ grails-app/domain/com/unifina/domain/Product.groovy
@@ -152,7 +152,6 @@ class Product {
 			imageUrl: imageUrl,
 			thumbnailUrl: thumbnailUrl,
 			category: category?.id,
-			streams: [],
 			state: state.toString(),
 			previewStream: previewStream?.id,
 			previewConfigJson: previewConfigJson,
