d341db3b8cc5fed1ae1030f7d8d0b7aa8c457bea
Teo Gebhard <teo@streamr.com>
2021-04-12 13:59:39 +0300

    Fix indentation, make class/fields final

--- src/java/com/unifina/service/NotifyStorageNodeTask.java
+++ src/java/com/unifina/service/NotifyStorageNodeTask.java
@@ -8,20 +8,20 @@ import org.apache.log4j.Logger;
 import java.util.Map;
 import java.util.HashMap;
 
-public class NotifyStorageNodeTask extends Thread {
+final class NotifyStorageNodeTask extends Thread {
 
-    private static final Logger log = Logger.getLogger(NotifyStorageNodeTask.class);
+	private static final Logger log = Logger.getLogger(NotifyStorageNodeTask.class);
 
-    public enum AssigmentEvent {
-	    STREAM_ADDED,
-	    STREAM_REMOVED
-    }
+	public enum AssigmentEvent {
+		STREAM_ADDED,
+		STREAM_REMOVED
+	}
 
-	EthereumAddress storageNodeAddress;
-	String streamId;
-	AssigmentEvent eventType;
-	StreamService streamService;
-	StreamrClientService streamrClientService;
+	final EthereumAddress storageNodeAddress;
+	final String streamId;
+	final AssigmentEvent eventType;
+	final StreamService streamService;
+	final StreamrClientService streamrClientService;
 
 	public NotifyStorageNodeTask(EthereumAddress storageNodeAddress, String streamId, AssigmentEvent eventType, StreamService streamService, StreamrClientService streamrClientService) {
 		super("NotifyStorageNodeTask-" + System.currentTimeMillis());
@@ -34,21 +34,21 @@ public class NotifyStorageNodeTask extends Thread {
 
 	public void run() {
 		StreamrClient client = streamrClientService.getInstanceForThisEngineNode();
-        try {
+		try {
 			com.streamr.client.rest.Stream assignmentStream = client.getStream(StorageNodeService.createAssignmentStreamId());
-		    client.publish(assignmentStream, createMessage());
-        } catch (Exception e) {
-            log.warn("Unable to notify StorageNode: streamId=" + streamId + ", event=" + eventType);
-        }
+			client.publish(assignmentStream, createMessage());
+		} catch (Exception e) {
+			log.warn("Unable to notify StorageNode: streamId=" + streamId + ", event=" + eventType);
+		}
 	}
 
-    private Map<String,Object> createMessage() {
+	private Map<String,Object> createMessage() {
 		Map<String,Object> message = new HashMap();
-        message.put("event", eventType.name());
-        Map<String,Object> stream = new HashMap();
-        stream.put("id", streamId);
-        stream.put("partitions", streamService.getStream(streamId).getPartitions());
+		message.put("event", eventType.name());
+		Map<String,Object> stream = new HashMap();
+		stream.put("id", streamId);
+		stream.put("partitions", streamService.getStream(streamId).getPartitions());
 		message.put("stream", stream);
-        return message;
-    }
+		return message;
+	}
 }
