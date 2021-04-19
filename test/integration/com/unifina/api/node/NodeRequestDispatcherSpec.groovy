10061ed8687a2c8a8f98ed75852ce510fed36630
Kare Nuorteva <kare.nuorteva@streamr.com>
2021-04-19 16:54:25 +0300

    Remove test/integration/com/unifina/api/node/NodeRequestDispatcherSpec.groovy

--- test/integration/com/unifina/api/node/NodeRequestDispatcherSpec.groovy
+++
@@ -1,9 +0,0 @@
-package com.unifina.api.node
-
-import grails.test.spock.IntegrationSpec
-import spock.lang.Ignore
-
-@Ignore
-class NodeRequestDispatcherSpec extends IntegrationSpec {
-	// TODO: write tests
-}
