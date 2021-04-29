3f1adccde43a9e5d1063097d33d101b2bbaaf2a6
Kare Nuorteva <kare.nuorteva@streamr.com>
2021-04-29 11:58:20 +0300

    BACK-143: Delete streams marked as ui_channels (#966)

--- grails-app/migrations/core/2021-04-20-remove-canvas-dashboard-module.groovy
+++ grails-app/migrations/core/2021-04-20-remove-canvas-dashboard-module.groovy
@@ -15,23 +15,23 @@ databaseChangeLog = {
 		dropTable(tableName: "dashboard")
 	}
 	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-4") {
-		dropUniqueConstraint(tableName: "stream", constraintName: "FKCAD54F8052E2E25F")
-		dropUniqueConstraint(tableName: "stream", constraintName: "ui_channel_path_idx")
-		dropColumn(tableName: "stream", columnName: "ui_channel_canvas_id")
-		dropColumn(tableName: "stream", columnName: "ui_channel_path")
-		dropColumn(tableName: "stream", columnName: "ui_channel")
-	}
-	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-5") {
 		dropUniqueConstraint(tableName: "permission", constraintName: "FKE125C5CF3D649786")
 		dropUniqueConstraint(tableName: "permission", constraintName: "FKE125C5CF70E281EB")
 		dropColumn(tableName: "permission", columnName: "canvas_id")
 		dropColumn(tableName: "permission", columnName: "dashboard_id")
-	}
-	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-6") {
 		sql("delete from permission where operation like 'canvas_%'")
 		sql("delete from permission where operation like 'dashboard_%'")
 	}
-	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-7") {
+	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-5") {
+		dropUniqueConstraint(tableName: "stream", constraintName: "FKCAD54F8052E2E25F")
+		dropUniqueConstraint(tableName: "stream", constraintName: "ui_channel_path_idx")
+		sql("delete from permission where stream_id is not null and operation like 'stream_%' and stream_id in (select id from stream where ui_channel = 1)")
+		sql("delete from stream where ui_channel = 1")
+		dropColumn(tableName: "stream", columnName: "ui_channel_canvas_id")
+		dropColumn(tableName: "stream", columnName: "ui_channel_path")
+		dropColumn(tableName: "stream", columnName: "ui_channel")
+	}
+	changeSet(author: "kkn", id: "remove-canvas-dashboard-module-6") {
 		sql("delete from integration_key where service = 'ETHEREUM'")
 	}
 }
