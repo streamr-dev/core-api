8911e5830582f476b3767da9136fa440412aea6c
Kare Nuorteva <kare.nuorteva@streamr.com>
2021-05-26 17:23:20 +0300

    BACK-179: Node 16 update (#989)
    * style(e2e-test): remove unused @ts-expect-error
    
    * chore(e2e-test): Streamr Javascript client 5.4.1
    
    * chore(e2e-test): remove unused async-wait-until
    
    * chore(e2e-test): remove unused sleep-promise
    
    * style(e2e-test): move tests to src dir
    
    * chore(e2e-test): update Node 16.2
    
    Npm install/ci require an additional command line flag to prevent
    following compilation error: CXXFLAGS=-std=c++14
    
    npm ERR! In file included from /Users/kkn/.node-gyp/16.2.0/include/node/v8.h:30:
    npm ERR! /Users/kkn/.node-gyp/16.2.0/include/node/v8-internal.h:452:38: error: no template named 'remove_cv_t' in namespace 'std'; did you mean 'remove_cv'?
    npm ERR!             !std::is_same<Data, std::remove_cv_t<T>>::value>::Perform(data);
    npm ERR!                                 ~~~~~^~~~~~~~~~~
    npm ERR!                                      remove_cv
    npm ERR! /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/c++/v1/type_traits:776:50: note: 'remove_cv' declared here
    npm ERR! template <class _Tp> struct _LIBCPP_TEMPLATE_VIS remove_cv
    
    Added npm_install recipe for convenience.
    
    * chore(ci): remove old npm install

--- rest-e2e-tests/src/schema-validator.ts
+++ rest-e2e-tests/src/schema-validator.ts
@@ -1,9 +1,9 @@
 import Ajv from 'ajv'
-import categorySchema from './schemas/category.json'
-import permissionSchema from './schemas/permission.json'
-import productSchema from './schemas/product.json'
-import streamSchema from './schemas/stream.json'
-import subscriptionSchema from './schemas/subscription.json'
+import categorySchema from '../schemas/category.json'
+import permissionSchema from '../schemas/permission.json'
+import productSchema from '../schemas/product.json'
+import streamSchema from '../schemas/stream.json'
+import subscriptionSchema from '../schemas/subscription.json'
 
 export class SchemaValidator {
 
@@ -24,23 +24,23 @@ export class SchemaValidator {
 
     validateCategory(data: any) {
         // @ts-expect-error
-        return this.ajv.validate('Category', data) ? [] : [... this.ajv.errors]
+        return this.ajv.validate('Category', data) ? [] : [...this.ajv.errors]
     }
 
     validatePermission(data: any) {
-        return this.ajv.validate('Permission', data) ? [] : [... this.ajv.errors]
+        return this.ajv.validate('Permission', data) ? [] : [...this.ajv.errors]
     }
 
     validateProduct(data: any) {
-        return this.ajv.validate('Product', data) ? [] : [... this.ajv.errors]
+        return this.ajv.validate('Product', data) ? [] : [...this.ajv.errors]
     }
 
     validateStream(data: any) {
-        return this.ajv.validate('Stream', data) ? [] : [... this.ajv.errors]
+        return this.ajv.validate('Stream', data) ? [] : [...this.ajv.errors]
     }
 
     validateSubscription(data: any) {
-        return this.ajv.validate('Subscription', data) ? [] : [... this.ajv.errors]
+        return this.ajv.validate('Subscription', data) ? [] : [...this.ajv.errors]
     }
 
     toMessages(errors: any) {
--- rest-e2e-tests/src/test-utilities.ts
+++ rest-e2e-tests/src/test-utilities.ts
@@ -1,16 +1,16 @@
-import { assert } from 'chai'
+import {assert} from 'chai'
 import _ from 'lodash';
-import { StreamrClient, StreamrClientOptions } from 'streamr-client'
-import { Wallet } from '@ethersproject/wallet'
-import { EthereumAccount } from './EthereumAccount';
+import {StreamrClient, StreamrClientOptions} from 'streamr-client'
+import {Wallet} from '@ethersproject/wallet'
+import {EthereumAccount} from './EthereumAccount';
 
 export const assertResponseIsError = async (response: any, statusCode: number, programmaticCode: string, includeInMessage?: string) => {
-    const json = await response.json()
-    assert.equal(response.status, statusCode)
-    assert.equal(json.code, programmaticCode)
-    if (includeInMessage) {
-        assert.include(json.message, includeInMessage)
-    }
+	const json = await response.json()
+	assert.equal(response.status, statusCode)
+	assert.equal(json.code, programmaticCode)
+	if (includeInMessage) {
+		assert.include(json.message, includeInMessage)
+	}
 }
 
 export const assertStreamrClientResponseError = async (request: any, expectedStatusCode: number) => {
@@ -23,22 +23,22 @@ export const assertStreamrClientResponseError = async (request: any, expectedSta
 		})
 }
 
-export const assertEqualEthereumAddresses = (actual: string|undefined, expected: string) => {
-	const normalized = (address: string|undefined) => address ? address.toLowerCase : address
+export const assertEqualEthereumAddresses = (actual: string | undefined, expected: string) => {
+	const normalized = (address: string | undefined) => address ? address.toLowerCase : address
 	assert.equal(normalized(actual), normalized(expected))
 }
 
 export const getStreamrClient = (user?: EthereumAccount) => {
-	const options: StreamrClientOptions = { 
+	const options: StreamrClientOptions = {
 		url: 'ws://localhost/api/v1/ws',
 		restUrl: 'http://localhost/api/v1',
 		tokenAddress: '0xbAA81A0179015bE47Ad439566374F2Bae098686F',
 		tokenSidechainAddress: '0x73Be21733CC5D08e1a14Ea9a399fb27DB3BEf8fF',
 		dataUnion: {
-            factoryMainnetAddress: '0x4bbcBeFBEC587f6C4AF9AF9B48847caEa1Fe81dA',
-            factorySidechainAddress: '0x4A4c4759eb3b7ABee079f832850cD3D0dC48D927',
-            templateMainnetAddress: '0x7bFBAe10AE5b5eF45e2aC396E0E605F6658eF3Bc',
-            templateSidechainAddress: '0x36afc8c9283CC866b8EB6a61C6e6862a83cd6ee8',
+			factoryMainnetAddress: '0x4bbcBeFBEC587f6C4AF9AF9B48847caEa1Fe81dA',
+			factorySidechainAddress: '0x4A4c4759eb3b7ABee079f832850cD3D0dC48D927',
+			templateMainnetAddress: '0x7bFBAe10AE5b5eF45e2aC396E0E605F6658eF3Bc',
+			templateSidechainAddress: '0x36afc8c9283CC866b8EB6a61C6e6862a83cd6ee8',
 		},
 		sidechain: {
 			url: 'http://localhost:8546'
@@ -63,7 +63,6 @@ export const getSessionToken = async (user: EthereumAccount) => {
 	const privateKey = user.privateKey
 	if (cachedSessionTokens[privateKey] === undefined) {
 		const client = getStreamrClient(user)
-		// @ts-expect-error
 		const sessionToken = await client.session.getSessionToken()
 		cachedSessionTokens[privateKey] = sessionToken
 		return sessionToken
@@ -76,7 +75,7 @@ export const testUsers = _.mapValues({
 	devOpsUser: '0x628acb12df34bb30a0b2f95ec2e6a743b386c5d4f63aa9f338bec6f613160e78',     // user_id=6 in the test DB, has ROLE_DEV_OPS authority
 	tokenHolder: '0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0',    // owns tokens in dev mainchain and sidechain
 	ensDomainOwner: '0xe5af7834455b7239881b85be89d905d6881dcb4751063897f12be1b0dd546bdb'  // owns testdomain1.eth ENS domain in dev mainchain
-}, (privateKey: string) => ( { 
+}, (privateKey: string) => ({
 	privateKey,
 	address: new Wallet(privateKey).address
-} ))
+}))
