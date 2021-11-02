e2802fcf147faa79a94bbf447488130f83c79062
Kare Nuorteva <kare.nuorteva@me.com>
2021-11-02 13:55:15 +0200

    add index to permission table
    Makes stream queries faster:
    select this_.id as id1_4_0_, this_.version as version2_4_0_, this_.anonymous as anonymou3_4_0_, this_.ends_at as ends_at4_4_0_, this_.invite_id as invite_i5_4_0_, this_.operation as operatio6_4_0_, this_.parent_id as parent_i7_4_0_, this_.product_id as product_8_4_0_, this_.stream_id as stream_i9_4_0_, this_.subscription_id as subscri10_4_0_, this_.user_id as user_id11_4_0_ from permission this_ where this_.stream_id='kyqokh62Qn-a2_4jiFrbUg' and this_.operation='stream_get' and this_.anonymous=1 and (this_.ends_at is null or this_.ends_at>'2021-11-02 11:09:00.741')

--- grails-app/migrations/core/2021-11-02-unique-username-index.groovy
+++ grails-app/migrations/core/2021-11-02-unique-username-index.groovy
@@ -4,4 +4,7 @@ databaseChangeLog = {
 	changeSet(author: "kkn", id: "unique-username-index-1") {
 		sql("alter table user add unique index (username);")
 	}
+	changeSet(author: "kkn", id: "unique-username-index-2") {
+		sql("alter table `permission` add index `stream_id_operation_anonymous_idx` (`stream_id`, `operation`, `anonymous`);")
+	}
 }
