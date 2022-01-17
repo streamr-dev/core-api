45617ca00c723c38ebbe952b77a03387777c5b9e
Kare Nuorteva <kare.nuorteva@me.com>
2022-01-17 17:24:42 +0200

    docs: add javadocs to Web3jHelperService

--- grails-app/services/com/unifina/service/Web3jHelperService.groovy
+++ grails-app/services/com/unifina/service/Web3jHelperService.groovy
@@ -6,8 +6,10 @@ import org.web3j.protocol.Web3j
 import java.util.concurrent.ExecutionException
 
 class Web3jHelperService {
-
-	// wrap the static method for testing
+	/**
+	 * @param erc20address address of ERC20 or 0x0 for ETH balance
+	 * @return token balance in wei
+	 */
 	BigInteger getERC20Balance(Web3j web3j, String erc20address, String holderAddress) throws ExecutionException, InterruptedException {
 		return Web3jHelper.getERC20Balance(web3j, erc20address, holderAddress)
 	}
